e585d931c061c7f0db0a35b111adff3889a47457
Ian Berry <iberry@raxnet.net>
2002-05-28 00:41:00 +0000

    CreateList can now handle normal arrays along with SQL arrays
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@66 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/functions.php
+++ lib/functions.php
@@ -219,17 +219,27 @@ function SyncDataSourceName($data_source_id, $saved_custom_data_source_name, $sa
 }
 
 function CreateList($data,$name,$value,$prev) {
-    
-    if (sizeof($data) > 0) {
-		foreach ($data as $row) {
-		    print "	<option value='$row[$value]'";
-		    
-			if ($prev == $row[$value]) {
-				print " selected ";
-		    }
-			
-		    print ">$row[$name]</option>\n";
-		}
+    /* if this array appears to be empty, try the alternate method */
+    if (sizeof($data[$value]) == 0) {
+	foreach ($data as $row) {
+	    print "<option value='$row[$value]'";
+	    
+		if ($prev == $row[$value]) {
+			print " selected ";
+	    }
+		
+	    print ">$row[$name]</option>\n";
+	}
+    }else{
+	for ($i=0; ($i < sizeof($data[$value])); $i++) {
+	    print '<option value="' . $data[$value][$i] . '"';
+	    
+		if ($prev == $data[$value][$i]) {
+			print " selected ";
+	    }
+		
+	    print ">" . $data[$name][$i] . "</option>\n";
+	}
     }
 }
 
