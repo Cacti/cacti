b5b7ceb37ee08ca7d9b9a57965aea57d62553859
Ian Berry <iberry@raxnet.net>
2002-06-11 23:34:46 +0000

    updated look of the about page
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@117 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- about.php
+++ about.php
@@ -1,12 +1,12 @@
 <?/*
 +-------------------------------------------------------------------------+
 | Copyright (C) 2002 Ian Berry                                            |
-|																	      |
-| This program is free software; you can redistribute it and/or			  |
+|									  |								      |
+| This program is free software; you can redistribute it and/or	          |
 | modify it under the terms of the GNU General Public License             |
 | as published by the Free Software Foundation; either version 2          |
 | of the License, or (at your option) any later version.                  |
-|																		  |
+|									  |									  |
 | This program is distributed in the hope that it will be useful,         |
 | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
 | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
@@ -19,46 +19,58 @@
 | - iberry@raxnet.net                                                     |
 +-------------------------------------------------------------------------+
 | - raXnet - http://www.raxnet.net/                                       |
-+-------------------------------------------------------------------------+
-*/?>
++-------------------------------------------------------------------------+ */?>
 <?	$section = "Console Access"; include ('include/auth.php');
-#	include ('include/config.php');
-#	include ("include/database.php");
-	include_once ("include/top_header.php"); ?> 
+	include_once ("include/top_header.php");
+	$title_text = "About cacti";
+	include_once ("include/top_table_header.php");
+	?> 
 
-<table width="97%">
+
 	<tr>
-		<td bgcolor="#<?print $colors[dark_bar];?>" colspan="2">
-			<strong><font size="+0">About raXnet's cacti (Version <?print $config[cacti_version];?>)</font></strong>
+		<td bgcolor="#<?print $colors[header_panel];?>" colspan="2">
+			<strong><font color="#<?print $colors[header_text];?>">Version <?print $config[cacti_version];?></font></strong>
 		</td>
 	</tr>
 	<tr>
-		<td valign="top">
-			<p>raXnet's cacti, the complete rrdtool frontend was written by Ian Berry. This
-			product took many long hours of PHP and SQL design, so I hope you find it very
-			useful.</p>
+		<td valign="top" bgcolor="#<?print $colors[light];?>" class="textArea">
+			<a href="http://www.raxnet.net/"><img align="right" src="images/raxnet_logo.gif" border="0" alt="raXnet"></a>
 			
-			<p><strong>Thanks</strong><br>
-			- A very special thanks to <a href="http://ee-staff.ethz.ch/~oetiker/">Tobi Oetiker</a>, 
-			the creator of <a href="http://www.mrtg.org/">rrdtool</a> and the very popular
-			<a href="http://www.mrtg.org">MRTG</a>.<br>
-			- Brady Alleman, creator of NetMRG and 
-			<a href="http://www.treehousetechnologies.net">Treehouse Technolgies</a> for questions and ideas. Just
-			as a note, NetMRG is a complete Network Monitering solution also written in PHP/MySQL. His
-			product also makes use of rrdtool's graphing capabilites, I encourage you to check it out.<br>
-			- <a href="http://www.globalunderground.co.uk/">Cluboxed</a>, the creator of the popular Global 
-			Underground compilations for hours of listening enjoyment.</p>
+			raXnet's cacti is designed to be the complete frontend to rrdtool. Cacti's goal is to make the
+			network administrator's job easier by taking care of all the nessesary details to create 
+			meaningful network graphs.
 			
-			<p><strong>Ways to Contact Ian</strong><br>
+			<p>The design of cacti took many hours of SQL and PHP coding, so I hope you find it very useful.</p>
+			
+			<p><strong>Developer Thanks</strong><br>
+			<ul type="disc">
+				<li>A <em>very</em> special thanks to <a href="mailto:rob@ldg.net">Rob Sweet</a> for contributing large amounts 
+					of code to the 0.8 branch. Also thanks to <a href="http://www.cox.com/">Cox Communications</a> for paying Rob to work on
+					cacti.</li>
+			</ul>
+			</p>
+			
+			<p><strong>Thanks</a></strong><br>
+			<ul type="disc">
+				<li>A very special thanks to <a href="http://ee-staff.ethz.ch/~oetiker/">Tobi Oetiker</a>, 
+					the creator of <a href="http://www.mrtg.org/">rrdtool</a> and the very popular
+					<a href="http://www.mrtg.org">MRTG</a>.</li>
+				<li>Brady Alleman, creator of NetMRG and 
+					<a href="http://www.treehousetechnologies.net">Treehouse Technolgies</a> for questions and ideas. Just
+					as a note, NetMRG is a complete Network Monitering solution also written in PHP/MySQL. His
+					product also makes use of rrdtool's graphing capabilites, I encourage you to check it out.</li>
+				<li><a href="http://www.globalunderground.co.uk/">Cluboxed</a>, the creator of the popular Global 
+					Underground compilations for hours of listening enjoyment.</li>
+			</ul>
+			</p>
+			
+			<p><strong>Ways to Contact Ian</a></strong><br>
 			<a href="mailto:iberry@raxnet.net">iberry@raxnet.net</a><br>
-			<a href="mailto:rax@kuhncom.net">rax@kuhncom.net</a><br></p>
-			
-			<strong><a href="http://www.raxnet.net/">raXnet home</a></strong><br>
-		</td>
-		<td>
-			<img src="images/cacti.png" width="52" height="385" border="0" alt="">
+			<a href="mailto:rax@kuhncom.net">rax@kuhncom.net</a><br>
+			</p>
 		</td>
 	</tr>
-</table>
 
-<?include_once ("include/bottom_footer.php");?>
+
+<?	include_once ("include/bottom_footer.php");
+	include_once ("include/bottom_table_footer.php");?>
