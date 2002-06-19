26e0e7ba5eb0e7758e63ce3d5905045e0853c6fc
Ian Berry <iberry@raxnet.net>
2002-06-19 04:39:55 +0000

    added tabs, cleaned up settings page
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@125 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- settings.php
+++ settings.php
@@ -26,99 +26,130 @@ $section = "User Administration";
 include ('include/auth.php');
 header("Cache-control: no-cache");
 
+include_once ("include/config_settings.php");
 include_once ("include/functions.php");
 include_once ('include/form.php');
 
 switch ($action) {
  case 'save':
-    $settings = db_fetch_assoc("select Name from settings");
-    if (sizeof($settings) > 0) {
-	foreach ($settings as $setting) {
-	    db_execute("update settings set value=\"" . ${$setting[Name]} .
-			"\" where name=\"$setting[Name]\"");
-	}
-    }
-    
-    header ("Location: settings.php");
-    break;
- default:
-    include_once ('include/top_header.php');
-    
-    $settings = db_fetch_assoc("select * from settings order by name");
-    
-    $title_text = "cacti Settings";
-    print "<form method='post' action='".basename($HTTP_SERVER_VARS["SCRIPT_NAME"])."?action=save'>\n";
-    include_once ("include/top_table_header.php");
-    if (sizeof($settings) > 0) {
-	foreach ($settings as $setting) {
-	    /* split appart the 'method' on the ':' */
-	    $setting_method = split(":",$setting[Method]);
-	    
-	    /* make sure to skip group members here; only parents are allowed */
-	    if ($setting_method[1] != "group") {
-		++$i;
-		DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i);
-		/* draw the acual header and textbox on the form */
-		DrawFormItem($setting[FriendlyName],$setting[Description]);
-		
-		/* choose what kind of item this is */
-		switch ($setting_method[0]) {
-		 case 'textbox':
-		    DrawFormItemTextBox($setting[Name],$setting[Value],"","");
-		    print "</tr>\n";
-		    break;
-		 case 'checkbox':
-		    DrawFormItemCheckBox($setting[Name],$setting[Value],$setting[Description],"");
-		    print "</tr>\n";
-		    break;
-		 case 'group':
-		    print "<td><table border=0 wieth=100% cellpadding=0 cellspacing=0><tr>\n";
-		    /* use 'o' for the internal group counter...remeber we can have unlimited items
-		     under each item (hence the group). */
-		    $o = 1; 
-		    $first = " WHERE"; 
-		    $sql_where = "";
-		    
-		    /* loop through each item in the group to create the OR SQL where clause */
-		    while ($o < count($setting_method)) {
-			$sql_where .= "$first name=\"$setting_method[$o]\"";
-			$first = " OR";
-			$o++;
-		    }
-		    
-		    /* pass the SQL where clause create above to MySQL */
-		    $group_list = db_fetch_assoc("select * from settings $sql_where");
-		    
-		    /* loop through the resultset and draw each item in the group */
-		    if (sizeof($group_list) > 0) {
-			foreach ($group_list as $item) {
-			    /* once again split apart the 'method' for this group item */
-			    $setting_method_group = split(":",$item[Method]);
-			    
-			    switch ($setting_method_group[0]) {
-			     case 'textbox':
-				DrawFormItemTextBox($item[Name],$item[Value],"","");
-				break;
-			     case 'checkbox':
-				DrawFormItemCheckBox($item[Name],$item[Value],$item[Description],"");
-				break;
-			    }
-			    
+	if (sizeof($settings) > 0) {
+	foreach (array_keys($settings) as $setting) {
+		if ($settings[$setting][tab] == $form[tab]) {
+			if ($settings[$setting][method] == "group") {
+				if (sizeof($settings[$setting][items]) > 0) {
+				foreach (array_keys($settings[$setting][items]) as $item) {
+					db_execute("replace into settings (name,value) values ('$item', '$form[$item]')");
+				}
+				}
+			}else{
+				db_execute("replace into settings (name,value) values ('$setting', '$form[$setting]')");
 			}
-		    }
-		    print "\t</tr></table></td>\n</tr>\n";
-		    break;
 		}
-	    }
-	    
 	}
-    }
-    print "<tr><td colspan=2>";
-    DrawFormSaveButton();
-    print "</td></tr>\n";
+	}
 
-    include_once ("include/bottom_table_footer.php");
-    include_once ("include/bottom_footer.php");
-    break;
+	header ("Location: settings.php?tab=$form[tab]");
+	break;
+ default:
+    	include_once ('include/top_header.php');
+	
+	if (!(isset($args[tab]))) {
+		/* there is no selected tab; select the first one */
+		$current_tab = array_keys($tabs);
+		$current_tab = $current_tab[0];
+	}else{
+		$current_tab = $args[tab];
+	}
+	
+	$title_text = "cacti Settings (" . $tabs[$current_tab] . ")"; include_once ("include/top_table_header.php");
+	
+	?>
+	
+			<tr height="33">
+				<td valign="bottom" colspan="3" background="images/tab_back.gif">
+					<table border="0" cellspacing="0" cellpadding="0">
+						<tr>
+						<?
+						if (sizeof($tabs) > 0) {
+						foreach (array_keys($tabs) as $tab_short_name) {
+						?>
+							<td nowrap class="textTab" align="center" background="images/tab_middle.gif">
+								<img src="images/tab_left.gif" border="0" align="absmiddle"><a href="settings.php?tab=<?print $tab_short_name;?>"><?print $tabs[$tab_short_name];?></a><img src="images/tab_right.gif" border="0" align="absmiddle">
+							</td>
+						<?
+						}
+						}
+						?>
+					</table>
+				</td>
+			</tr>
+	
+	<?
+	
+    	print "<form method='post' action='settings.php?action=save'>\n";
+	
+	if (sizeof($settings) > 0) {
+	foreach (array_keys($settings) as $setting) {
+	    	/* make sure to skip group members here; only parents are allowed */
+		if (($settings[$setting][method] != "internal") && ($settings[$setting][tab] == $current_tab)) {
+			++$i;
+			DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i);
+			
+			/* draw the acual header and textbox on the form */
+			DrawFormItem($settings[$setting][friendly_name],$settings[$setting][description]);
+			
+			$current_value = db_fetch_cell("select value from settings where name='$setting'");
+			
+			/* choose what kind of item this is */
+			switch ($settings[$setting][method]) {
+				case 'textbox':
+					DrawFormItemTextBox($setting,$current_value,"","");
+					print "</tr>\n";
+					break;
+				case 'checkbox':
+					DrawFormItemCheckBox($setting,$current_value,$settings[$setting][friendly_name],"");
+					print "</tr>\n";
+					break;
+				case 'group':
+					print "<td>\n";
+					
+		    			/* loop through the resultset and draw each item in the group */
+					if (sizeof($settings[$setting][items]) > 0) {
+					foreach (array_keys($settings[$setting][items]) as $item) {
+						$current_value = db_fetch_cell("select value from settings where name='$item'");
+						
+			    			switch ($settings[$setting][items][$item][method]) {
+							case 'textbox':
+								DrawtrippedFormItemTextBox($item,$current_value,"","");
+								break;
+							case 'checkbox':
+								DrawStrippedFormItemCheckBox($item,$current_value,$settings[$setting][items][$item][description],"",true);
+								break;
+						}
+			    
+					}
+					}
+					
+					print "</td></tr>\n";
+					break;
+			}
+		}
+	
+	}
+	}
+	
+	?>
+		<tr bgcolor="#FFFFFF">
+			 <td colspan="2" align="right" background="images/blue_line.gif">
+				<?DrawFormSaveButton("save");?>
+			</td>
+		</tr>
+	<?
+	DrawFormItemHiddenIDField("tab",$args[tab]);
+	
+	include_once ("include/bottom_table_footer.php");
+	include_once ("include/bottom_footer.php");
+	
+	break;
 } ?>	
 			
