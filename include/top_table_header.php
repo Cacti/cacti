2f2165be9f6339cc95bb3f00f7a0e96bc0d1739a
Ian Berry <iberry@raxnet.net>
2002-06-24 07:19:26 +0000

    will not display the title bar if no text is entered
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@138 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/top_table_header.php
+++ include/top_table_header.php
@@ -1,14 +1,14 @@
 <table align="center" <?if ($style == "dialog") {?>width="60%"<?}else{?>width="98%"<?}?> cellpadding=1 cellspacing=0 border=0 bgcolor="#00438C">
-  <tr>
-    <td>
-      <table cellpadding=3 cellspacing=0 border=0 bgcolor="#E1E1E1" width="100%">
- 	<tr>
-  	  <td bgcolor="#00438C" colspan="10">
-	    <table width="100%" cellpadding="0" cellspacing="0">
-	      <tr>
-		<td bgcolor="#00438C" class="textHeaderDark"><?print $title_text;?></td>
-		   <?if (isset($add_text)) {?><td class="textHeaderDark" align="right" bgcolor="#00438C"><strong><a class="linkOverDark" href="<?print $add_text;?>">Add</a>&nbsp;</strong></td><?}?>
-	      </tr>
-	    </table>
-	  </td>
-        </tr>
+	<tr>
+		<td>
+			<table cellpadding=3 cellspacing=0 border=0 bgcolor="#E1E1E1" width="100%">
+				<?if ($title_text != "") {?><tr>
+					<td bgcolor="#00438C" colspan="10">
+						<table width="100%" cellpadding="0" cellspacing="0">
+							<tr>
+								<td bgcolor="#00438C" class="textHeaderDark"><?print $title_text;?></td>
+									<?if (isset($add_text)) {?><td class="textHeaderDark" align="right" bgcolor="#00438C"><strong><a class="linkOverDark" href="<?print $add_text;?>">Add</a>&nbsp;</strong></td><?}?>
+							</tr>
+						</table>
+					</td>
+				</tr><?}?>
