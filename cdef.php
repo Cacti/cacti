6f7b33c1fe5c9bb2bd183cd47db5e93a336d5f37
Ian Berry <iberry@raxnet.net>
2002-07-17 05:01:37 +0000

    added cdef preview, item edit screen, refined item view table
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@157 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cdef.php
+++ cdef.php
@@ -25,6 +25,8 @@
 $section = "Add/Edit Graphs"; 
 include ('include/auth.php');
 header("Cache-control: no-cache");
+include_once ("include/cdef_functions.php");
+include_once ("include/config_arrays.php");
 include_once ('include/form.php');
 
 if (isset($form[action])) { $action = $form[action]; } else { $action = $args[action]; }
@@ -32,17 +34,140 @@ if (isset($form[ID])) { $id = $form[ID]; } else { $id = $args[id]; }
 
 $current_script_name = basename($HTTP_SERVER_VARS["SCRIPT_NAME"]);
 
+function draw_cdef_preview($cdef_id) {
+	global $colors; ?>
+	<tr bgcolor="#<?print $colors[panel];?>">
+		<td>
+			<pre><?print get_cdef($cdef_id);?></pre>
+		</td>
+	</tr>	
+<?}
+
 switch ($action) {
- case 'save':
- 	$save["ID"] = $form["ID"];
-	$save["Hex"] = $form["Hex"];
+ case 'save_item':
+ 	if ($form[value_function] != "0") { $current_type = 1; $current_value = $form[value_function]; }
+	if ($form[value_operator] != "0") { $current_type = 2; $current_value = $form[value_operator]; }
+	if ($form[value_data_source] != "0") { $current_type = 3; $current_value = $form[value_data_source]; }
+	if ($form[value_special_data_source] != "0") { $current_type = 4; $current_value = $form[value_special_data_source]; }
+	if ($form[value_cdef] != "0") { $current_type = 5; $current_value = $form[value_cdef]; }
+	if ($form[value_custom] != "") { $current_type = 6; $current_value = $form[value_custom]; }
 	
-	sql_save($save, "def_colors");
+	if (!(isset($current_type))) {
+		/* YOU MUST SELECT SOMETHING */
+		header ("Location: cdef.php?action=edit&id=$form[cdef_id]"); exit;
+	}
+	
+ 	$save["id"] = $form["id"];
+	$save["cdef_id"] = $form["cdef_id"];
+	$save["sequence"] = 0;
+	$save["type"] = $current_type;
+	$save["value"] = $current_value;
+	
+	sql_save($save, "cdef_items");
+
+	header ("Location: cdef.php?action=edit&id=$form[cdef_id]");
+	break;
+ case 'remove_item':
+    	db_execute("delete from cdef_items where id=$args[id]");
+    
+    	header ("Location: cdef.php?action=edit&id=$args[cdef_id]");
+	break;
+ case 'edit_item':
+	include_once ("include/top_header.php");
+	
+	if (isset($args[id])) {
+		$cdef = db_fetch_row("select * from cdef_items where id=$args[id]");
+		$current_type = $cdef[type];
+		$values[$current_type] = $cdef[value];
+	}else{
+		unset($cdef);
+	}
+	
+	start_box("", "", "");
+	draw_cdef_preview($args[cdef_id]);
+	end_box();
+	
+	start_box("<strong>CDEF Items [edit]</strong> (" . db_fetch_cell("select name from cdef where id=$cdef_id") . ")", "", "");
+	
+	?>
+	<form method="post" action="cdef.php">
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+		<td colspan="2">
+			<font class="textHeader">Choose any one of these items:</font>
+		</td>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],1); ?>
+		<td width="50%">
+			<font class="textEditTitle">Function</font>
+		</td>
+		<?DrawFormItemDropdownFromSQL("value_function",$cdef_functions,"","",$values[1],"None","");?>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+		<td width="50%">
+			<font class="textEditTitle">Operator</font>
+		</td>
+		<?DrawFormItemDropdownFromSQL("value_operator",$cdef_operators,"","",$values[2],"None","");?>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],1); ?>
+		<td width="50%">
+			<font class="textEditTitle">Data Source</font>
+		</td>
+		<?DrawFormItemDropdownFromSQL("value_data_source",db_fetch_assoc("select descrip,item_id from polling_items"),"descrip","item_id",$values[3],"None","");?>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+		<td width="50%">
+			<font class="textEditTitle">Special Data Source</font>
+		</td>
+		<?DrawFormItemDropdownFromSQL("value_special_data_source",$custom_data_source_types,"","",$values[4],"None","");?>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],1); ?>
+		<td width="50%">
+			<font class="textEditTitle">Another CDEF</font>
+		</td>
+		<?DrawFormItemDropdownFromSQL("value_cdef",db_fetch_assoc("select name,id from cdef"),"name","id",$values[5],"None","");?>
+	</tr>
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+		<td width="50%">
+			<font class="textEditTitle">Custom String</font>
+		</td>
+		<?DrawFormItemTextBox("value_custom",$values[6],"","150", "40");?>
+	</tr>
+	
+	<?
+	DrawFormItemHiddenIDField("id",$args[id]);
+	DrawFormItemHiddenIDField("cdef_id",$cdef_id);
+	?>
+	
+	<tr bgcolor="#FFFFFF">
+		 <td colspan="2" align="right" background="images/blue_line.gif">
+			<?DrawFormSaveButton("save_item", "cdef.php?action=edit&id=$cdef_id");?>
+			</form>
+		</td>
+	</tr>
+	<?
+	
+	end_box();
+	
+	include_once ("include/bottom_footer.php");
+ 	break;
+ case 'save':
+ 	$save["id"] = $form["id"];
+	$save["name"] = $form["name"];
+	
+	sql_save($save, "cdef");
 
 	header ("Location: cdef.php");
 	break;
  case 'remove':
-    	db_execute("delete from def_colors where id=$args[id]");
+    	db_execute("delete from cdef where id=$args[id]");
+	db_execute("delete from cdef_items where cdef_id=$args[id]");
     
     	header ("Location: cdef.php");
 	break;
@@ -56,8 +181,11 @@ switch ($action) {
 	}else{
 		unset($cdef);
 	}
-    
-	DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+	
+	?>
+	<form method="post" action="cdef.php">
+	
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
 		<td width="50%">
 			<font class="textEditTitle">Name</font><br>
 			A useful name for this CDEF.
@@ -67,40 +195,48 @@ switch ($action) {
 	
 	<?
 	DrawFormItemHiddenIDField("id",$args[id]);
-	
 	?>
 	
 	<tr bgcolor="#FFFFFF">
 		 <td colspan="2" align="right" background="images/blue_line.gif">
-			<?DrawFormSaveButton("save", "color.php");?>
+			<?DrawFormSaveButton("save", "cdef.php");?>
 			</form>
 		</td>
 	</tr>
 	<?
 	
 	end_box();
+	
 	start_box("", "", "");
+	draw_cdef_preview($args[id]);
+	end_box();
+	
+	start_box("CDEF Items", "", "cdef.php?action=edit_item&cdef_id=$cdef[id]");
 	
 	print "<tr bgcolor='#$colors[header_panel]'>";
 		DrawMatrixHeaderItem("Item",$colors[header_text],1);
 		DrawMatrixHeaderItem("Item Value",$colors[header_text],1);
-		DrawMatrixHeaderItem("&nbsp;",$colors[header_text],1);
+		DrawMatrixHeaderItem("&nbsp;",$colors[header_text],2);
 	print "</tr>";
     
-	$cdef_items = db_fetch_assoc("select * from cdef_items where order by sequence");
+	$cdef_items = db_fetch_assoc("select * from cdef_items where cdef_id=$args[id] order by sequence");
 	
 	if (sizeof($cdef_items) > 0) {
 	foreach ($cdef_items as $cdef_item) {
 		DrawMatrixRowAlternateColorBegin($colors[alternate],$colors[light],$i); $i++;
 			?>
 			<td>
-				<a class="linkEditMain" href="cdef.php?action=edit_item&id=<?print $cdef_item[id];?>">Item #<?print $i;?></a>
+				<a class="linkEditMain" href="cdef.php?action=edit_item&id=<?print $cdef_item[id];?>&cdef_id=<?print $cdef[id];?>">Item #<?print $i;?></a>
 			</td>
 			<td>
-				Value
+				<em><?$cdef_item_type = $cdef_item[type]; print $cdef_item_types[$cdef_item_type];?></em>: <strong><?print get_cdef_item_name($cdef_item[id]);?></strong>
+			</td>
+			<td>
+				<a href="cdef.php?action=movedown_item&id=<?print $cdef_item[id];?>&cdef_id=<?print $cdef[id];?>"><img src="images/move_down.gif" border="0" alt="Move Down"></a>
+				<a href="cdef.php?action=moveup_item&id=<?print $cdef_item[id];?>&cdef_id=<?print $cdef[id];?>"><img src="images/move_up.gif" border="0" alt="Move Up"></a>
 			</td>
 			<td width="1%" align="right">
-				<a href="cdef.php?action=remove_item&id=<?print $cdef_item[id];?>"><img src="images/delete_icon.gif" width="10" height="10" border="0" alt="Delete"></a>&nbsp;
+				<a href="cdef.php?action=remove_item&id=<?print $cdef_item[id];?>&cdef_id=<?print $cdef[id];?>"><img src="images/delete_icon.gif" width="10" height="10" border="0" alt="Delete"></a>&nbsp;
 			</td>
 		</tr>
 	<?
