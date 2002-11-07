72c94422f3aaa4ed1e4177b484eb121ff9a94585
Ian Berry <iberry@raxnet.net>
2002-11-07 07:50:38 +0000

    impliment field ordering (in db) for data inputs
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@270 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- data_input.php
+++ data_input.php
@@ -93,7 +93,7 @@ function form_save() {
    -------------------------- */
 
 function field_remove() {
-	global $args, $config;
+	global $args, $config, $registered_cacti_names;
 	
 	if (($config["remove_verification"]["value"] == "on") && ($args[confirm] != "yes")) {
 		include ('include/top_header.php');
@@ -103,13 +103,26 @@ function field_remove() {
 	}
 	
 	if (($config["remove_verification"]["value"] == "") || ($args[confirm] == "yes")) {
+		/* get information about the field we're going to delete so we can re-order the seqs */
+		$field = db_fetch_row("select input_output,data_input_id from data_input_fields where id=$args[id]");
+		
 		db_execute("delete from data_input_fields where id=$args[id]");
 		db_execute("delete from data_input_data where data_input_field_id=$args[id]");
+		
+		/* when a field is deleted; we need to re-order the field sequences */
+		if (preg_match_all("/<([_a-zA-Z0-9]+)>/", db_fetch_cell("select " . $field["input_output"] . "put_string from data_input where id=" . $field["data_input_id"]), $matches)) {
+			$j = 0;
+			for ($i=0; ($i < count($matches[1])); $i++) {
+				if (in_array($matches[1][$i], $registered_cacti_names) == false) {
+					$j++; db_execute("update data_input_fields set sequence=$j where data_input_id=" . $field["data_input_id"] . " and input_output='" .  $field["input_output"]. "' and data_name='" . $matches[1][$i] . "'");
+				}
+			}
+		}
 	}
 }
 
 function field_save() {
-	global $form;
+	global $form, $registered_cacti_names;
 	
  	$save["id"] = $form["id"];
 	$save["data_input_id"] = $form["data_input_id"];
@@ -117,8 +130,23 @@ function field_save() {
 	$save["data_name"] = $form["data_name"];
 	$save["input_output"] = $form["input_output"];
 	$save["update_rra"] = $form["update_rra"];
+	$save["sequence"] = $form["sequence"];
+	$save["type_code"] = $form["type_code"];
 	
-	sql_save($save, "data_input_fields");	
+	$data_input_field_id = sql_save($save, "data_input_fields");
+	
+	if (!empty($data_input_field_id)) {
+		if (preg_match_all("/<([_a-zA-Z0-9]+)>/", db_fetch_cell("select " . $form["input_output"] . "put_string from data_input where id=" . $form["data_input_id"]), $matches)) {
+			$j = 0;
+			for ($i=0; ($i < count($matches[1])); $i++) {
+				if (in_array($matches[1][$i], $registered_cacti_names) == false) {
+					if ($matches[1][$i] == $form["data_name"]) {
+						$j++; db_execute("update data_input_fields set sequence=$j where data_input_id=" . $form["data_input_id"] . " and input_output='" .  $form["input_output"]. "' and data_name='" . $matches[1][$i] . "'");
+					}
+				}
+			}
+		}
+	}
 }
 
 function field_edit() {
@@ -157,7 +185,7 @@ function field_edit() {
 	?>
 	<form method="post" action="data_input.php">
 	
-	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],1); ?>
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i); $i++; ?>
 		<td width="50%">
 			<font class="textEditTitle">Field [<?print $header_name;?>]</font><br>
 			Choose the associated field from the <?print $header_name;?> field.
@@ -165,7 +193,7 @@ function field_edit() {
 		<?DrawFormItemDropdownFromSQL("data_name",$array_field_names,"","",$field[data_name],"","");?>
 	</tr>
 	
-	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],0); ?>
+	<?DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i); $i++; ?>
 		<td width="50%">
 			<font class="textEditTitle">Name</font><br>
 			Enter a meaningful name for this data input method.
@@ -175,7 +203,7 @@ function field_edit() {
 	
 	<?
 	if ($current_field_type == "out") {
-	DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],1); ?>
+	DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i); $i++; ?>
 		<td width="50%">
 			<font class="textEditTitle">Update RRD File</font><br>
 			Whether data from this output field is to be entered into the rrd file.
@@ -185,8 +213,18 @@ function field_edit() {
 	<?
 	}
 	
+	DrawMatrixRowAlternateColorBegin($colors[form_alternate1],$colors[form_alternate2],$i); $i++; ?>
+		<td width="50%">
+			<font class="textEditTitle">Special Type Code</font><br>
+			If this field should be treated specially by host templates, indicate so here. Valid keywords for this field are 'hostname', 'management_ip', 'snmp_community', 'snmp_username', and 'snmp_password'.
+		</td>
+		<?DrawFormItemTextBox("type_code",$field[type_code],"","40", "40");?>
+	</tr>
+	<?
+	
 	DrawFormItemHiddenIDField("id",$args[id]);
 	DrawFormItemHiddenTextBox("input_output",$current_field_type,"");
+	DrawFormItemHiddenTextBox("sequence",$field[sequence],"");
 	DrawFormItemHiddenTextBox("data_input_id",$args[data_input_id],$field[data_input_id]);
 	DrawFormItemHiddenTextBox("save_component_field","1","");
 	end_box();
@@ -225,7 +263,7 @@ function data_remove() {
 }
 
 function data_save() {
-	global $form;
+	global $form, $registered_cacti_names;
 	
 	$save["id"] = $form["id"];
 	$save["name"] = $form["name"];
@@ -233,6 +271,31 @@ function data_save() {
 	$save["output_string"] = $form["output_string"];
 	
 	sql_save($save, "data_input");
+	
+	/* get a list of each field so we can note their sequence of occurance in the database */
+	if (!empty($form["id"])) {
+		db_execute("update data_input_fields set sequence=0 where data_input_id=" . $form["id"]);
+		
+		if (preg_match_all("/<([_a-zA-Z0-9]+)>/", $form["input_string"], $matches)) {
+			$j = 0;
+			for ($i=0; ($i < count($matches[1])); $i++) {
+				if (in_array($matches[1][$i], $registered_cacti_names) == false) {
+					$j++; db_execute("update data_input_fields set sequence=$j where data_input_id=" . $form["id"] . " and input_output='in' and data_name='" . $matches[1][$i] . "'");
+				}
+			}
+		}
+		
+		if (preg_match_all("/<([_a-zA-Z0-9]+)>/", $form["output_string"], $matches)) {
+			$j = 0;
+			for ($i=0; ($i < count($matches[1])); $i++) {
+				if (in_array($matches[1][$i], $registered_cacti_names) == false) {
+					$j++; db_execute("update data_input_fields set sequence=$j where data_input_id=" . $form["id"] . " and input_output='out' and data_name='" . $matches[1][$i] . "'");
+				}
+			}
+		}
+	}
+	
+	
 }
 
 function data_edit() {
@@ -281,10 +344,11 @@ function data_edit() {
 		start_box("Input Fields", "", "data_input.php?action=field_edit&type=in&data_input_id=$args[id]");
 		print "<tr bgcolor='#$colors[header_panel]'>";
 			DrawMatrixHeaderItem("Name",$colors[header_text],1);
+			DrawMatrixHeaderItem("Field Order",$colors[header_text],1);
 			DrawMatrixHeaderItem("Friendly Name",$colors[header_text],2);
 		print "</tr>";
 	    
-		$fields = db_fetch_assoc("select id,data_name,name from data_input_fields where data_input_id=$args[id] and input_output='in' order by data_name");
+		$fields = db_fetch_assoc("select id,data_name,name,sequence from data_input_fields where data_input_id=$args[id] and input_output='in' order by sequence");
 		
 		if (sizeof($fields) > 0) {
 		foreach ($fields as $field) {
@@ -292,6 +356,9 @@ function data_edit() {
 				?>
 				<td>
 					<a class="linkEditMain" href="data_input.php?action=field_edit&id=<?print $field[id];?>&data_input_id=<?print $args[id];?>"><?print $field[data_name];?></a>
+				</td>
+				<td>
+					<?print $field[sequence]; if ($field[sequence] == "0") { print " (Not In Use)"; }?>
 				</td>
 				<td>
 					<?print $field[name];?>
@@ -308,11 +375,12 @@ function data_edit() {
 		start_box("Output Fields", "", "data_input.php?action=field_edit&type=out&data_input_id=$args[id]");
 		print "<tr bgcolor='#$colors[header_panel]'>";
 			DrawMatrixHeaderItem("Name",$colors[header_text],1);
+			DrawMatrixHeaderItem("Field Order",$colors[header_text],1);
 			DrawMatrixHeaderItem("Friendly Name",$colors[header_text],1);
 			DrawMatrixHeaderItem("Update RRA",$colors[header_text],2);
 		print "</tr>";
 	
-		$fields = db_fetch_assoc("select id,name,data_name,update_rra from data_input_fields where data_input_id=$args[id] and input_output='out' order by data_name");
+		$fields = db_fetch_assoc("select id,name,data_name,update_rra,sequence from data_input_fields where data_input_id=$args[id] and input_output='out' order by sequence");
 		
 		if (sizeof($fields) > 0) {
 		foreach ($fields as $field) {
@@ -320,6 +388,9 @@ function data_edit() {
 				?>
 				<td>
 					<a class="linkEditMain" href="data_input.php?action=field_edit&id=<?print $field[id];?>&data_input_id=<?print $args[id];?>"><?print $field[data_name];?></a>
+				</td>
+				<td>
+					<?print $field[sequence]; if ($field[sequence] == "0") { print " (Not In Use)"; }?>
 				</td>
 				<td>
 					<?print $field[name];?>
