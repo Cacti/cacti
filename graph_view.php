097d1ce7e12868173a84e000af6b6f776934f8e1
Ian Berry <iberry@raxnet.net>
2003-01-10 08:35:55 +0000

    re-added list/preview views and updated them to 0.8
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@361 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- graph_view.php
+++ graph_view.php
@@ -67,122 +67,104 @@ case 'tree':
 		}
 	}
 	
-	print "<table width='98%' style='background-color: #f5f5f5; border: 1px solid #bbbbbb;' align='center'>";
-	print "<tr><td><strong><a href='graph_view.php?action=tree&tree_id=" . $_SESSION["sess_view_tree_id"] . "'>[root]</a> - $tree_name</strong></td></tr>";
+	print "	<table width='98%' align='center'>
+			<tr>
+				<td>
+					<strong><a href='graph_view.php?action=tree&tree_id=" . $_SESSION["sess_view_tree_id"] . "'>[root]</a> - $tree_name</strong>
+				</td>
+			</tr>
+		</table>
+		<br>\n";
 	
 	grow_graph_tree($_SESSION["sess_view_tree_id"], $start_branch, $user_id, $tree_parameters);
 	
-	print '</tr>';
-	print "</table>";
-	
 	break;
 case 'preview':
-	if ($show_preview_view != "on") {
-		print "<strong><font size=\"+1\" color=\"FF0000\">YOU DO NOT HAVE RIGHTS FOR PREVIEW VIEW</font></strong>"; exit;
+	if ($current_user["show_preview"] == "") {
+		print "<strong><font size='+1' color='FF0000'>YOU DO NOT HAVE RIGHTS FOR PREVIEW VIEW</font></strong>"; exit;
 	}
 	
 	/* graph permissions */
-	if ($config["global_auth"]["value"] == "on") {
-		if ($config["graph_policy"]["auth"] == "1") {
-			$sql_where = "where ag.userid is null";
-		}elseif ($config["graph_policy"]["auth"] == "2") {
-			$sql_where = "where ag.userid is not null";
+	if (read_config_option("global_auth") == "on") {
+		if ($current_user["graph_policy"] == "1") {
+			$sql_where = "where user_auth_graph.user_id is null";
+		}elseif ($current_user["graph_policy"] == "2") {
+			$sql_where = "where user_auth_graph.user_id is not null";
 		}
 		
-		$sql_id = mysql_query("select 
-			g.id,g.title 
-			from rrd_graph g 
-			left join auth_graph ag on (g.id=ag.graphid and ag.userid=$user_id) 
+		$graphs = db_fetch_assoc("select 
+			graph_templates_graph.local_graph_id,
+			graph_templates_graph.title
+			from graph_templates_graph 
+			left join user_auth_graph on (graph_templates_graph.local_graph_id=user_auth_graph.local_graph_id and user_auth_graph.user_id=" . $_SESSION["sess_user_id"] . ") 
 			$sql_where
-			order by g.title",$cnn_id);
+			and graph_templates_graph.local_graph_id > 0
+			order by graph_templates_graph.title");
 	}else{
-		$sql_id = mysql_query("select 
-			id,title 
-			from rrd_graph 
-			order by title",$cnn_id);
+		$graphs = db_fetch_assoc("select 
+			local_graph_id,title 
+			from graph_templates_graph 
+			where local_graph_id > 0
+			order by title");
 	}
 	
-	$rows = mysql_num_rows($sql_id); $i = 0;
+	print "<table width='98%' align='center'><tr>";
 	
-	?><tr><td bgcolor="#<?print $color_light;?>" width="1%" rowspan="99999"></td><?
-		while ($i < $rows) {
-			?><td width="25%"><a href="graph.php?rraid=all&graphid=<?print mysql_result($sql_id, $i, "id");?>"><img src="graph_image.php?graphid=<?print mysql_result($sql_id, $i, "id");?>&rraid=<?print $array_settings["preview"]["rraid"];?>&graph_start=-<?print $array_settings["preview"]["timespan"];?>&graph_height=<?print $array_settings["preview"]["height"];?>&graph_width=<?print $array_settings["preview"]["width"];?>&graph_nolegend=true" border="0" alt="<?print mysql_result($sql_id, $i, "title");?>"></a><?
-			?></td><?
-			$i++;
-			$k++;
-			
-			if ($k==$array_settings["preview"]["columnnumber"]) {
-				$k = 0;
-				?></tr><tr height="10"><td>&nbsp;</td></tr><tr><?
-			}
-			$i_rra = 0;
+	if (sizeof($graphs) > 0) {
+	foreach ($graphs as $graph) {
+		print "<td><a href='graph.php?rraid=all&local_graph_id=" . $graph["local_graph_id"] . "'><img src='graph_image.php?local_graph_id=" . $graph["local_graph_id"] . "&rra_id=" . read_graph_config_option("default_rra_id") . "&graph_start=-" . read_graph_config_option("timespan") . "&graph_height=" . read_graph_config_option("height") . "&graph_width=" . read_graph_config_option("width") . "&graph_nolegend=true' border='0' alt='" . $graph["title"] . "'></a></td>\n";
+		
+		$i++;
+		$k++;
+		
+		if (($i == read_graph_config_option("num_columns")) && ($k < sizeof($graphs))) {
+			$i = 0;
+			print "</tr><tr height='10'><td>&nbsp;</td></tr>\n<tr>";
 		}
-	?></tr><?
+	}
+	}
+	
+	print "</tr></table>";
 	
 	break;
 case 'list':
-	if ($show_list_view != "on") {
-		print "<strong><font size=\"+1\" color=\"FF0000\">YOU DO NOT HAVE RIGHTS FOR LIST VIEW</font></strong>"; exit;
+	if ($current_user["show_list"] == "") {
+		print "<strong><font size='+1' color='FF0000'>YOU DO NOT HAVE RIGHTS FOR LIST VIEW</font></strong>"; exit;
 	}
-	
-	include_once ("include/form.php");
 	
 	/* graph permissions */
-	if ($config["global_auth"]["value"] == "on") {
-		if ($config["graph_policy"]["auth"] == "1") {
-			$sql_where = "where ag.userid is null";
-		}elseif ($config["graph_policy"]["auth"] == "2") {
-			$sql_where = "where ag.userid is not null";
+	if (read_config_option("global_auth") == "on") {
+		if ($current_user["graph_policy"] == "1") {
+			$sql_where = "where user_auth_graph.user_id is null";
+		}elseif ($current_user["graph_policy"] == "2") {
+			$sql_where = "where user_auth_graph.user_id is not null";
 		}
 		
-		$sql_id = mysql_query("select 
-			g.id,g.title 
-			from rrd_graph g 
-			left join auth_graph ag on (g.id=ag.graphid and ag.userid=$user_id) 
+		$graphs = db_fetch_assoc("select 
+			graph_templates_graph.local_graph_id,
+			graph_templates_graph.title
+			from graph_templates_graph 
+			left join user_auth_graph on (graph_templates_graph.local_graph_id=user_auth_graph.local_graph_id and user_auth_graph.user_id=" . $_SESSION["sess_user_id"] . ") 
 			$sql_where
-			order by g.title",$cnn_id);
+			and graph_templates_graph.local_graph_id > 0
+			order by graph_templates_graph.title");
 	}else{
-		$sql_id = mysql_query("select 
-			id,title 
-			from rrd_graph 
-			order by title",$cnn_id);
+		$graphs = db_fetch_assoc("select 
+			local_graph_id,title 
+			from graph_templates_graph 
+			where local_graph_id > 0
+			order by title");
 	}
 	
-	$rows = mysql_num_rows($sql_id); $i = 0;
+	print "<table width='98%' align='center'>";
 	
-	$sql_id_rra = mysql_query("select id,name from rrd_rra order by id",$cnn_id);
-	$rows_rra = mysql_num_rows($sql_id_rra); $i_rra = 0;
-	
-	print "<table width=\"97%\">";
-	print "<tr>";
-	
-	while ($i < $rows) {
-		switch ($array_settings["list"]["listviewtype"]) {
-			case "1":
-				?><td width="25%"><strong><?print mysql_result($sql_id, $i, "title");?> [<a href="graph.php?graphid=<?print mysql_result($sql_id, $i, "id");?>&rraid=all">all</a>]</strong><br><?
-				while ($i_rra < $rows_rra) {
-					?><a href="graph.php?graphid=<?print mysql_result($sql_id, $i, "id");?>&rraid=<?print mysql_result($sql_id_rra, $i_rra, "id");?>"><?print mysql_result($sql_id_rra, $i_rra, "name");?></a><br><?
-					$i_rra++;
-				}
-				?></td><?
-				$i++;
-				$k++;
-				
-				if ($k==4) {
-					$k = 0;
-					?></tr><tr height="10"><td>&nbsp;</td></tr><tr><?
-				}
-				$i_rra = 0;
-				
-				break;
-			case "2":
-				?><td><strong><a href="graph.php?graphid=<?print mysql_result($sql_id, $i, "id");?>&rraid=all"><?print mysql_result($sql_id, $i, "title");?></a></strong></td></tr><tr><?
-				$i++;
-				
-				break;
-		}
+	if (sizeof($graphs) > 0) {
+	foreach ($graphs as $graph) {
+		print "<tr><td><strong><a href='graph.php?local_graph_id=" . $graph["local_graph_id"] . "&rra_id=all'>" . $graph["title"] . "</a></strong></td></tr>\n";
 	}
-		?></tr></table><?
+	}
+	
+	print "</table>";
 	
 	break;
 }
