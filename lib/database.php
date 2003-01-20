148e87e5f88c1df8704302ba211acda3568f0125
Ian Berry <iberry@raxnet.net>
2003-01-20 06:34:54 +0000

    code formatting/cleanup
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@414 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/database.php
+++ lib/database.php
@@ -1,11 +1,24 @@
-<?/*
+<?/* 
 +-------------------------------------------------------------------------+
-| raXnet Database Wrappers                                                |
+| Copyright (C) 2002 Ian Berry                                            |
+|                                                                         |
+| This program is free software; you can redistribute it and/or           |
+| modify it under the terms of the GNU General Public License             |
+| as published by the Free Software Foundation; either version 2          |
+| of the License, or (at your option) any later version.                  |
+|                                                                         |
+| This program is distributed in the hope that it will be useful,         |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+| GNU General Public License for more details.                            |
 +-------------------------------------------------------------------------+
-| This code was crafted by Ian Berry, make sure any questions             |
-| about the structure or integrity of this code be directed to:           |
-| - rax@kuhncom.net                                                       |
-| - iberry@onion.dyndns.org                                                 |
+| cacti: the rrdtool frontend [php-auth, php-tree, php-form]              |
++-------------------------------------------------------------------------+
+| This code is currently maintained and debugged by Ian Berry, any        |
+| questions or comments regarding this code should be directed to:        |
+| - iberry@raxnet.net                                                     |
++-------------------------------------------------------------------------+
+| - raXnet - http://www.raxnet.net/                                       |
 +-------------------------------------------------------------------------+
 */?>
 <?
@@ -15,250 +28,101 @@ if ($database == "") { $database = $database_default; }
 db_connect();
 
 function db_connect() {
-    global $database_hostname,$database_username,$database_password,$database;
-    db_connect_real($database_hostname,$database_username,$database_password,$database); 
+	global $database_hostname,$database_username,$database_password,$database;
+	db_connect_real($database_hostname,$database_username,$database_password,$database); 
 }
 
 function db_connect_real($host,$user,$pass,$db_name) {
-    global $cnn_id;
-    
-    $cnn_id = mysql_connect($host,$user,$pass);
+	global $cnn_id;
 	
-    if ($cnn_id) {
+	$cnn_id = mysql_connect($host,$user,$pass);
+	
+	if ($cnn_id) {
 		if (mysql_selectdb($db_name)) {
-		    return(1);
+			return(1);
 		}else{
-	    	die("Cannot find the database $database");
-	    	return(0);
+			die("Cannot find the database $database");
+			return(0);
 		}
-    }else{
+	}else{
 		die("Cannot connect to MySQL server on $database_hostname");
 		return(0);
-    }
+	}
 }
 
 
 function db_execute($sql) {
-    global $cnn_id;
+	global $cnn_id;
 	
 	if (!$cnn_id) { db_connect(); }
 	
 	$query = mysql_query($sql);
-   	
+	
 	if ($query) {
 		return(1); 
-   	}else{
+	}else{
 		return(0);
-   	}
+	}
 }
 
 
 function db_fetch_cell($sql,$col_name = '') {
-    global $cnn_id;
+	global $cnn_id;
 	
 	if (!$cnn_id) { db_connect(); }
-    
-	$row = array();
-    $query = mysql_query($sql);
 	
-    if ($query) {
+	$row = array();
+	$query = mysql_query($sql);
+	
+	if ($query) {
 		$rows = mysql_numrows($query);
+		
 		if ($rows > 0) {
-	    	if ($col_name != '') {
+			if ($col_name != '') {
 				$row = mysql_fetch_assoc($query);
 				return($row[$col_name]);
-	    	}else{
+			}else{
 				return(mysql_result($query,0,0));
-	    	}
+			}
 		}
-    }
+	}
 }
 
 function db_fetch_row($sql) {
-    global $cnn_id;
-    
+	global $cnn_id;
+	
 	if (!$cnn_id) { db_connect(); }
-    
+	
 	$row = array();
-    $query = mysql_query($sql);
-    
+	$query = mysql_query($sql);
+	
 	if ($query) {
 		$rows = mysql_numrows($query);
+		
 		if ($rows > 0) {
-	    	return(mysql_fetch_assoc($query));
+			return(mysql_fetch_assoc($query));
 		}
-    }
+	}
 }
 
 function db_fetch_assoc($sql) {
-    global $cnn_id;
-    
+	global $cnn_id;
+	
 	if (!$cnn_id) { db_connect(); }
-    
+	
 	$data = array();
-    $query = mysql_query($sql);
-    
+	$query = mysql_query($sql);
+	
 	if ($query) {
 		$rows = mysql_numrows($query);
-			if ($rows > 0) {
-	    		while($row = mysql_fetch_assoc($query)) {
-				$data[sizeof($data)] = $row;
-	    	}
-	    	return($data);
+		
+		if ($rows > 0) {
+			while($row = mysql_fetch_assoc($query)) {
+				$data{sizeof($data)} = $row;
+			}
+			
+			return($data);
 		}
-    }
-}
-
-function get_table_fields($table) {
-    global $cnn_id;
-    if (!$cnn_id) { db_connect(); }
-  	static $cols;
-	
-	if (! is_array($cols[$table])) {
-		$sql2="DESC $table";
-	    $columnquery=MYSQL_QUERY($sql2);
-		
-	    # determines weather or not to put the value in ""'s
-		
-	    while ($col = MYSQL_FETCH_ARRAY($columnquery)){
-	      	$cols[$table][$col[Field]] = $col;
-			if (strstr($col[Type],"int")      ||
-				strstr($col[Type],"bigint")   ||
-				strstr($col[Type],"tinyint")  ||
-				strstr($col[Type],"float")    ||
-				strstr($col[Type],"decimal")  ||
-				strstr($col[Type],"double")   ||
-				strstr($col[Type],"real")     ||
-				strstr($col[Type],"numeric")  ){
-	       		$cols[$table][$col[Field]][quoted] = 0;
-	     	}else{
-	       		$cols[$table][$col[Field]][quoted] = 1;
-	     	}
-	    }
-	}
-	
-	return($cols[$table]);
-}
-
-
-##  This function adds records to the specified table based on the data supplied in $newdata.
-
-##  NOTE:  a field value that starts with &mysql- denotes a mysql function to be used.
-##  The '&mysql-' will be stripped and the resulting string WILL NOT be quoted.
-
-function addrec ($newdata, $table) {
-    global $cnn_id,$defaults,$fieldlist,$fields;
-    if (!$cnn_id) { db_connect(); }
-   	
-	$sql="INSERT INTO $table VALUES(";
-   
-  	$cols = get_table_fields($table);
-	
-  	foreach (array_keys($cols) as $key) {
-	    if (! isset($newdata[$key])) { 
-	      	$newdata[$key] = $cols[$key]["Default"];
-	    }
-	    
-		if ($cols[$key][quoted]) {
-	      	if (! isset($newdata[$key]) || $newdata[$key] == '') { 
-	        	$sql .= "NULL,";
-	      	}else{
-	        	if (preg_match("/^\&mysql-/",$newdata[$key])) {
-		  			$newdata[$key] = preg_replace("/^&mysql-/","",$newdata[$key]);
-	          		$sql .= "$newdata[$key],";
-				}else{
-	          		$sql .= "\"$newdata[$key]\",";
-				}
-	      	}
-	    }else{
-	      	if (! isset($newdata[$key]) || $newdata[$key] == '') { $newdata[$key] = "0"; }
-	      	$sql .= "$newdata[$key],";
-	    }
-	}
-	
-	# this is to replace the last , with nothing
-	$sql = ereg_replace(",$","",$sql).")";
-	
-	##  Run the query.  If it fails, return -1.  If it works, either return the value it
-	##  used for an AUTO_INCREMENT field (if there was one) or just 0.  I'd return a '1' 
-	##  but that's a valid id number to return so '0' it must be.
-	if ($addquery = MYSQL_QUERY($sql)) {
-		$sql = "SELECT LAST_INSERT_ID()";
-	    $lastquery = mysql_query($sql);
-	    $res_tmp = mysql_fetch_array($lastquery);
-	    $result = $res_tmp[0];
-	    
-		if (! $result) { $result =  0; }
-	    return $result;
-	}else{
-		return -1;
-	}
-}
-
-
-##  This function is very similar to addrec but instead of just adding $newdata to the
-##  specified table, it compares the data in $newdata to the data in $olddata and selects
-##  which fields to change.  Also an argument is the WHERE clause to use in the query with 
-##  the word 'WHERE' (i.e.:  'id = 45', for example).  We also automatically decide which 
-##  fields get quotes by what kind of field it is.
-
-##  NOTE:  a field value that starts with &mysql- denotes a mysql function to be used.
-##  The '&mysql-' will be stripped and the resulting string WILL NOT be quoted.
-
-function modrec ($olddata, $newdata, $table, $where) {
-	global $cnn_id,$defaults,$fieldlist,$fields;
-    if (!$cnn_id) {   db_connect(); }
-  	$cols = get_table_fields($table);
-  	
-  	$sql="UPDATE $table SET ";
-  	
-	foreach(array_keys($cols) as $key){
-    	if ($olddata[$key] != $newdata[$key]) {
-      		++$fieldchange;
-      		if ($cols[$key][quoted]) {
-        		if (preg_match("/^\&mysql-/",$newdata[$key])) {
-	  				$newdata[$key] = preg_replace("/^&mysql-/","",$newdata[$key]);
-          			$update .= "$key = $newdata[$key],";
-				}else{
-          			$update .= "$key = \"$newdata[$key]\",";
-				}
-      		}else{
-        		$update .= "$key = $newdata[$key],";
-      		}
-    	}
-  	}
-	
-  	if ($update) {
-	    $sql .= $update;
-		
-	    # this is to replace the last , with nothing
-	    $sql = ereg_replace(",$","",$sql)." WHERE $where";
-		
-	    ##  Run the query.  Return 0 if there were no changes to make, -1 for a failed query, 
-	    ##  or a number representing the number of fields that were changed.
-	    if ($modquery = MYSQL_QUERY($sql)) {
-			return $fieldchange;
-	    }else{
-			return -1;
-	    }
-		
-	}else{
-		return 0;
-	}
-}
-
-##  This function is delete records from $table based on the passed WHERE clause ($where).
-function delrec ($table, $where) {
-    global $cnn_id;
-    if (!$cnn_id) { db_connect(); }
-	$sql = "DELETE FROM $table WHERE $where";
-	
-	##  Run the query.  Return 0 if there were no changes to make, -1 for a failed query, 
-	##  or a number representing the number of fields that were changed.
-	if ($delquery = MYSQL_QUERY($sql)) {
-		return 1;
-	}else{
-		return -1;
 	}
 }
 
@@ -307,9 +171,6 @@ function sql_save($array_items, $table_name) {
 	/* form the SQL string */
 	$sql_save = "replace into $table_name ($sql_save_fields) values ($sql_save_values)";
 	
-	/* replace all blank entries with NULLs */
-	//$sql_save = str_replace('""', "NULL", $sql_save);
-	//print $array_items
 	//print $sql_save . "<br>";
 	if (!db_execute($sql_save)) { return 0; }
 	
