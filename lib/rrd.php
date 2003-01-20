44e5a8e80f1937e249cc5018a0097d7299a974c8
Ian Berry <iberry@raxnet.net>
2003-01-20 06:36:03 +0000

    lots of graph generation stuff including cdef support
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@416 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/rrd.php
+++ lib/rrd.php
@@ -193,9 +193,9 @@ function rrdtool_function_tune($rrd_tune_array) {
 	include_once ('functions.php');
 	include ('config_arrays.php');
 	
-	$data_source_name = GetDataSourceName($rrd_tune_array["data_source_id"]);
+	$data_source_name = get_data_source_name($rrd_tune_array["data_source_id"]);
 	$data_source_type = $data_source_types{$rrd_tune_array["data-source-type"]};
-	$data_source_path = GetDataSourcePath($rrd_tune_array["data_source_id"], true);
+	$data_source_path = get_data_source_path($rrd_tune_array["data_source_id"], true);
 	
 	if ($rrd_tune_array["heartbeat"] != "") {
 		$rrd_tune .= " --heartbeat $data_source_name:" . $rrd_tune_array["heartbeat"];
@@ -229,6 +229,7 @@ function rrdtool_function_tune($rrd_tune_array) {
 
 function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 	include_once ("functions.php");
+	include_once ("cdef_functions.php");
 	include ("config_arrays.php");
 	
 	/* before we do anything; make sure the user has permission to view this graph,
@@ -247,22 +248,9 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 		}
 	}
 	
-	//$graph = db_fetch_row("select g.*, i.Name from rrd_graph g left join def_image_type i 
-	//	on g.imageformatid=i.id where g.id=$graphid");
-	
 	/* define the time span, which decides which rra to use */
 	$rra = db_fetch_row("select rows,steps from rra where id=$rra_id");
 	$timespan = -($rra["rows"] * $rra["steps"] * 144);
-	
-	/* this is so we do not show the data for MIN/MAX data on daily graphs (steps <= 1),
-	this code is a little hacked at the moment as GPRINT's are not covered. some changes will have
-	to be made for this to be included also */
-	/*if ($rra["steps"] > 1) {
-		$sql_order_by = "";
-	}else{
-		$sql_order_by = "and !(cf.id != 1 and (t.name='LINE1' or t.name='AREA' or t.name='STACK'
-			or t.name='LINE2' or t.name='LINE3'))";
-	}*/
     	
 	$graph = db_fetch_row("select
 		graph_templates_graph.title,
@@ -381,47 +369,12 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 		"$scale" .
 		"$graph_legend" .
 		"--vertical-label='" . $graph["vertical_label"] . "'" . RRD_NL;
-    
-    	/* a note about different CF's on a graph. for now we are only going to display MAX/MIN CF
-	data when viewing anything greater than daily graphs. From what I know, rrdtool does not
-	store AVERAGE MAX/MIN data for any RRA with less than 1 step */
-	/*if ($rra[teps] > 1) {
-	$sql_group_by = "group by d.id,i.consolidationfunction";
-	}else{
-	$sql_group_by = "and c.name=\"AVERAGE\" group by d.id";
-	}*/
-    	
-    	/* +++++++++++++++++++++++ GRAPH DEFS +++++++++++++++++++++++ */
-    
-    	/* define the datasources used; only once */
-	/*$defs = db_fetch_assoc("select 
-		i.id as IID, i.CDEFID, i.ConsolidationFunction,
-		d.ID, d.Name, d.DSName, d.DSPath,
-		c.Name as CName
-		from rrd_graph_item i 
-		left join rrd_ds d 
-		on i.dsid=d.id 
-		left join def_cf c 
-		on i.consolidationfunction=c.id
-		left join def_graph_type t 
-		on i.graphtypeid=t.id
-		where i.graphid=$graphid 
-		and (t.name=\"AREA\" or t.name=\"STACK\" 
-		or t.name=\"LINE1\" or t.name=\"LINE2\" 
-		or t.name=\"LINE3\") 
-		and d.dsname is not null
-		$sql_group_by");
-		$rows_defs = sizeof($defs);*/
     	
 	$i = 0;
     	if (sizeof($graph_items > 0)) {
 	foreach ($graph_items as $graph_item) {
 		if (ereg("(AREA|STACK|LINE[123])", $graph_item_types{$graph_item["graph_type_id"]})) {
-			if (empty($graph_item["data_source_name"])) {
-				//$data_source_name = generate_data_source_name();
-			}else{
-				$data_source_name = $graph_item["data_source_name"];
-			}
+			$data_source_name = $graph_item["data_source_name"];
 			
 			/* use a user-specified ds path if one is entered */
 			$data_source_path = get_data_source_path($graph_item["local_data_id"], true);
@@ -433,11 +386,9 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 			to a function that matches the digits with letters. rrdtool likes letters instead
 			of numbers in DEF names; especially with CDEF's. cdef's are created
 			the same way, except a 'cdef' is put on the beginning of the hash */
-			
 			$graph_defs .= "DEF:" . generate_graph_def_name(("$i")) . "=\"$data_source_path\":$data_source_name:" . $consolidation_functions{$graph_item["consolidation_function_id"]} . RRD_NL;
 			
 			$cf_ds_cache{$graph_item["data_template_rrd_id"]}{$graph_item["consolidation_function_id"]} = "$i";
-			//$graph_group_cache[$def[IID]] = "$i";
 			
 			$i++;
 		}
@@ -469,22 +420,6 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 			}else{
 				$hardreturn[$graph_item_id] = "";
 			}
-			
-			if ($graph_item["graph_templates_item_id"] != "") {
-				/*$fields = db_fetch_assoc("select d.FieldID, d.DSID, d.Value, 
-					f.SrcID, f.DataName
-					from src_data d
-					left join src_fields f
-					on d.fieldid=f.id
-					where d.dsid=$graph_item[graph_templates_item_id]");
-			
-				if (sizeof($fields) > 0) {
-				foreach ($fields as $field) {
-					$text_format[$i] = ereg_replace ("<$field[DataName]>", $field[Value],$text_format[$i]);
-					$value_format[$i] = ereg_replace ("<$field[DataName]>", $field[Value],$value_format[$i]);
-				}
-				}*/
-			}
 	    		
 			/* +++++++++++++++++++++++ LEGEND: AUTO PADDING (<>'s) +++++++++++++++++++++++ */
 			
@@ -502,13 +437,10 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 					}
 				}
 			}
-	    		
-	    		//$i++;
 		}
 		}
     	}
 	
-    	
     	/* +++++++++++++++++++++++ GRAPH ITEMS: CDEF's +++++++++++++++++++++++ */
     	
 	$i = 0;
@@ -516,68 +448,30 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 		
 	if (sizeof($graph_items) > 0) {
 	foreach ($graph_items as $graph_item) {
+		/* CF rules: if we are using a CF because it's defined in the AREA, STACK, LINE[1-3] then
+		it is ok to use it elsewhere on the graph. But it is not ok to use a CF DEF because
+		its used in a GPRINT; so check that here */
+		if ((isset($cf_ds_cache{$graph_item["data_template_rrd_id"]}{$graph_item["consolidation_function_id"]})) && ($graph_item_types{$graph_item["graph_type_id"]} != "GPRINT")) {
+			$cf_id = $graph_item["consolidation_function_id"];
+		}else{
+			$cf_id = 1; /* CF: AVERAGE */
+		}
+		
 		/* make cdef string here; a note about CDEF's in cacti. A CDEF is neither unique to a 
 		data source of global cdef, but is unique when those two variables combine. */
 		$cdef_graph_defs = "";
 		
-		if (($graph_item["cdef_id"] != "0") && (isset($cdef_cache[$graph_item["cdef_id"]][$graph_item["graph_templates_item_id"]]) == false)) {
-		/* pull out what kind of cdef type this is */
-		$cdef_type = db_fetch_cell("select Type from rrd_ds_cdef where id=" . $graph_item["cdef_id"]);
+		if ((!empty($graph_item["cdef_id"])) && (isset($cdef_cache{$graph_item["cdef_id"]}{$graph_item["data_template_rrd_id"]}) == false)) {
+			$cdef_string = get_cdef($graph_item["cdef_id"]);
+			$cdef_string = str_replace("CURRENT_DATA_SOURCE", generate_graph_def_name($cf_ds_cache{$graph_item["data_template_rrd_id"]}[$cf_id]), $cdef_string);
+			
+			/* make the initial "virtual" cdef name: 'cdef' + md5(dsid) */
+			$cdef_graph_defs .= "CDEF:cdef" . generate_graph_def_name("$i") . "=";
+			$cdef_graph_defs .= $cdef_string;
+			$cdef_graph_defs .= " \\\n";
 		
-		/* get all of the items for this cdef */
-		$cdef_items = db_fetch_assoc("select case
-		when ci.type=\"CDEF Function\" then cf.name
-		when ci.type=\"Data Source\" then ds.name
-		when ci.type=\"Custom Entry\" then ci.custom
-		end 'CDEF',
-		ci.Type,ci.DSID,ci.CurrentDS
-		from rrd_ds_cdef_item ci left join def_cdef cf on cf.id=ci.cdeffunctionid left 
-		join rrd_ds ds on ds.id=ci.dsid
-		where ci.cdefid=$item[CDEFID]
-		order by ci.sequence");
-		
-		/* CF rules: if we are using a CF because it's defined in the AREA, STACK, LINE[1-3] then
-		it is ok to use it elsewhere on the graph. But it is not ok to use a CF DEF because
-		its used in a GPRINT; so check that here */
-		if (isset($cf_ds_cache[$item[ID]][$item[ConsolidationFunction]]) == true) {
-		$cf_id = $item[ConsolidationFunction];
-		}else{
-		$cf_id = 1; /* CF: AVERAGE */
-		}
-		
-		/* make the initial "virtual" cdef name: 'cdef' + md5(dsid) */
-		$cdef_graph_defs .= "CDEF:cdef" . generate_graph_def_name("$i") . "=";
-		$i_cdef = 0;
-		
-		/* form the cdef string by looping through each item. Additionally MD5 hash each
-		data source that we come across so this works right */
-		switch ($cdef_type) {
-		case "1": /* normal */
-		if (sizeof($cdef_items) > 0) {
-		foreach ($cdef_items as $cdef_item) {
-		if ($cdef_item[Type] == "Data Source") {
-		if ($cdef_item[CurrentDS] == "on") {
-		$cdef_current_item = generate_graph_def_name($cf_ds_cache[$item[ID]][$cf_id]);
-		}else{
-		$cdef_current_item = generate_graph_def_name($cf_ds_cache[$cdef_item[DSID]][$cf_id]);
-		}
-		}else{
-		$cdef_current_item = $cdef_item[CDEF];
-		}
-		
-		if ($i_cdef == 0) { $delimeter = ""; }else{ $delimeter = ","; }
-		$cdef_graph_defs .= "$delimeter$cdef_current_item";
-		
-		$i_cdef++;
-		}
-		}
-		break;
-		}
-		
-		$cdef_graph_defs .= " \\\n";
-		
-		/* the CDEF cache is so we do not create duplicate CDEF's on a graph */
-		$cdef_cache[$item[CDEFID]][$item[ID]] = "$i";
+			/* the CDEF cache is so we do not create duplicate CDEF's on a graph */
+			$cdef_cache{$graph_item["cdef_id"]}{$graph_item["data_template_rrd_id"]} = "$i";
 		}
 		
 		/* add the cdef string to the end of the def string */
@@ -610,23 +504,6 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 		if we want to skip it, like below */
 		$current_graph_item_type = $graph_item_types{$graph_item["graph_type_id"]};
 		
-		/* CF rules: if we are using a CF because it's defined in the AREA, STACK, LINE[1-3] then
-		it is ok to use it elsewhere on the graph. But it is not ok to use a CF DEF because
-		its used in a GPRINT; so check that here */
-		if ((isset($cf_ds_cache{$graph_item["data_template_rrd_id"]}{$graph_item["consolidation_function_id"]})) && ($graph_item_types{$graph_item["graph_type_id"]} != "GPRINT")) {
-			$cf_id = $graph_item["consolidation_function_id"];
-		}else{
-			$cf_id = 1; /* CF: AVERAGE */
-		}
-		
-		/* make sure grouping is on, before we make decisions based on the group */
-		//if ($graph[Grouping] == "on") {
-		/* if this item belongs to a graph group that has a parent that does not exist, do
-		not show the child item. this happens with MAX/MIN items on daily graphs mostly. */
-		//if ((isset($graph_group_cache[$item[Parent]]) == false) && ($item[TName] == "GPRINT")) {
-		//$current_graph_item_type = "SKIP";
-		//}
-		//}
 		/* use cdef if one if specified */
 		if ($graph_item["cdef_id"] == "0") {
 			$data_source_name = generate_graph_def_name($cf_ds_cache{$graph_item["data_template_rrd_id"]}[$cf_id]);
@@ -641,35 +518,35 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 		
 		switch ($graph_item_types{$graph_item["graph_type_id"]}) {
 		case 'AREA':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" . 
 			$data_source_name . "#" . 
 			$graph_item["hex"] . ":" . 
 			"\"$text_format[$graph_item_id]$hardreturn[$graph_item_id]\" ";
 			break;
 		case 'STACK':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" . 
 			$data_source_name . "#" . 
 			$graph_item["hex"] . ":" .
 			"\"$text_format[$graph_item_id]$hardreturn[$graph_item_id]\" ";
 			break;
 		case 'LINE1':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" . 
 			$data_source_name . "#" . 
 			$graph_item["hex"] . ":" . 
 			"\"$text_format[$graph_item_id]$hardreturn[$graph_item_id]\" ";
 			break;
 		case 'LINE2':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" . 
 			$data_source_name . "#" . 
 			$graph_item["hex"] . ":" . 
 			"\"$text_format[$graph_item_id]$hardreturn[$graph_item_id]\" ";
 			break;
 		case 'LINE3':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" . 
 			$data_source_name . "#" . 
 			$graph_item["hex"] . ":" . 
@@ -683,7 +560,7 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 			if ($graph_data_array["graph_nolegend"] != true) {
 				$gprint_text = $graph_item["gprint_text"];
 				
-				$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+				$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 				$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" .
 				$data_source_name . ":" . $consolidation_functions{$graph_item["consolidation_function_id"]} .
 				":\"$text_padding$text_format[$graph_item_id]$gprint_text$hardreturn[$graph_item_id]\" ";
@@ -691,12 +568,12 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 			
 			break;
 		case 'HRULE':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			
 			if ($graph_data_array["graph_nolegend"] == true) {
 				$value_format[$graph_item_id] = "0";
 			}else{
-				$value_format[$graph_item_id] = ereg_replace (":", "\:" ,$value_format[$graph_item_id]); /* escape colons */
+				$value_format[$graph_item_id] = str_replace(":", "\:" ,$value_format[$graph_item_id]); /* escape colons */
 			}
 			
 			$txt_graph_items .= $graph_item_types{$graph_item["graph_type_id"]} . ":" .
@@ -704,7 +581,7 @@ function rrdtool_function_graph($local_graph_id, $rra_id, $graph_data_array) {
 			"$text_format[$graph_item_id]$hardreturn[$graph_item_id]\" ";
 			break;
 		case 'VRULE':
-			$text_format[$graph_item_id] = ereg_replace (":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
+			$text_format[$graph_item_id] = str_replace(":", "\:" ,$text_format[$graph_item_id]); /* escape colons */
 			
 			$value_array = explode(":", $graph_item["value"]);
 			$value = date("U", mktime($value_array[0],$value_array[1],0));
