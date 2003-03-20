63e02aae4d20232ab88537d3a2a05fd5ef13eea4
Ian Berry <iberry@raxnet.net>
2003-03-20 07:48:05 +0000

    tcp_custom.pl
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@725 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/tcp_custom.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-#gets inbound and outbound tcp connections
-$conn = `netstat -n | grep -c \"$ARGV[0]\"`;
-
-chomp $conn;
-print $conn;
--- scripts/tcp_custom_snmp.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-#gets inbound and outbound tcp connections
-$conn = `snmpnetstat -c \"$ARGV[1]\" -n \"$ARGV[0]\" | grep -c \"$ARGV[2]\"`;
-
-print $conn;
