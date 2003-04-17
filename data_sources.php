308be27576cdd378e6344cd6e9843dbde8c77f3d
Ian Berry <iberry@raxnet.net>
2003-04-17 05:27:54 +0000

    missing arg to duplicate_data_source()
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@859 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- data_sources.php
+++ data_sources.php
@@ -313,7 +313,7 @@ function form_actions() {
 			}
 		}elseif ($_POST["drp_action"] == "4") { /* duplicate */
 			for ($i=0;($i<count($selected_items));$i++) {
-				duplicate_data_source($selected_items[$i], $_POST["title_format"]);
+				duplicate_data_source($selected_items[$i], 0, $_POST["title_format"]);
 			}
 		}elseif ($_POST["drp_action"] == "5") { /* data source -> data template */
 			for ($i=0;($i<count($selected_items));$i++) {
