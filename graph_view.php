3d0b3a7d9f51081933cec04179abaea4b5e40b45
Ian Berry <iberry@raxnet.net>
2003-05-19 16:41:38 +0000

    fixed parse error
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@943 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- graph_view.php
+++ graph_view.php
@@ -94,7 +94,7 @@ case 'tree':
 	
 	break;
 case 'preview':
-	define("ROWS_PER_PAGE", read_graph_config_option("num_graphs_per_page");
+	define("ROWS_PER_PAGE", read_graph_config_option("num_graphs_per_page"));
 	
 	$sql_or = ""; $sql_where = ""; $sql_join = "";
 	
