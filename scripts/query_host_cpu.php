24ba7f0480d90fe6e1e9d061152febd55efd14c0
Ian Berry <iberry@raxnet.net>
2003-05-23 07:13:58 +0000

    two new script queries for win32
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@950 860744bd-22fc-0310-8c96-e9fe5004b5ca

---
+++ scripts/query_host_cpu.php
@@ -0,0 +1,86 @@
+<?php
+
+include("../include/config.php");
+include("../include/snmp_functions.php");
+
+$oids = array(
+	"index" => ".1.3.6.1.2.1.25.3.3.1",
+	"usage" => ".1.3.6.1.2.1.25.3.3.1"
+	);
+
+$hostname = $_SERVER["argv"][1];
+$snmp_community = $_SERVER["argv"][2];
+$cmd = $_SERVER["argv"][3];
+
+if ($cmd == "index") {
+	$arr_index = get_indexes($hostname, $snmp_community);
+	
+	for ($i=0;($i<sizeof($arr_index));$i++) {
+		print $arr_index[$i] . "\n";
+	}
+}elseif ($cmd == "query") {
+	$arg = $_SERVER["argv"][4];
+	
+	$arr_index = get_indexes($hostname, $snmp_community);
+	$arr = get_cpu_usage($hostname, $snmp_community);
+	
+	for ($i=0;($i<sizeof($arr_index));$i++) {
+		if ($arg == "usage") {
+			print $arr_index[$i] . "!" . $arr[$i] . "\n";
+		}elseif ($arg == "index") {
+			print $arr_index[$i] . "!" . $arr_index[$i] . "\n";
+		}
+	}
+}elseif ($cmd == "get") {
+	$arg = $_SERVER["argv"][4];
+	$index = $_SERVER["argv"][5];
+	
+	$arr_index = get_indexes($hostname, $snmp_community);
+	$arr = get_cpu_usage($hostname, $snmp_community);
+	
+	if (isset($arr_index[$index])) {
+		print $arr[$index];
+	}
+}
+
+function get_cpu_usage($hostname, $snmp_community) {
+	$arr = reindex(cacti_snmp_walk($hostname, $snmp_community, ".1.3.6.1.2.1.25.3.3.1", "1", "", ""));
+	$return_arr = array();
+	
+	$j = 0;
+	
+	for ($i=0;($i<sizeof($arr));$i++) {
+		if (ereg("^[0-9]+$", $arr[$i])) {
+			$return_arr[$j] = $arr[$i];
+		}
+	}
+	
+	return $return_arr;
+}
+
+function get_indexes($hostname, $snmp_community) {
+	$arr = reindex(cacti_snmp_walk($hostname, $snmp_community, ".1.3.6.1.2.1.25.3.3.1", "1", "", ""));
+	$return_arr = array();
+	
+	$j = 0;
+	
+	for ($i=0;($i<sizeof($arr));$i++) {
+		if (ereg("^[0-9]+$", $arr[$i])) {
+			$return_arr[$j] = $j;
+		}
+	}
+	
+	return $return_arr;
+}
+
+function reindex($arr) {
+	$return_arr = array();
+	
+	for ($i=0;($i<sizeof($arr));$i++) {
+		$return_arr[$i] = $arr[$i]["value"];
+	}
+	
+	return $return_arr;
+}
+
+?>
---
+++ scripts/query_host_partitions.php
@@ -0,0 +1,55 @@
+<?php
+
+include("../include/config.php");
+include("../include/snmp_functions.php");
+
+$oids = array(
+	"total" => ".1.3.6.1.2.1.25.2.3.1.5",
+	"used" => ".1.3.6.1.2.1.25.2.3.1.6",
+	"failures" => ".1.3.6.1.2.1.25.2.3.1.7",
+	"index" => ".1.3.6.1.2.1.25.2.3.1.1",
+	"description" => ".1.3.6.1.2.1.25.2.3.1.3",
+	"sau" => ".1.3.6.1.2.1.25.2.3.1.4"
+	);
+
+$hostname = $_SERVER["argv"][1];
+$snmp_community = $_SERVER["argv"][2];
+$cmd = $_SERVER["argv"][3];
+
+if ($cmd == "index") {
+	$return_arr = reindex(cacti_snmp_walk($hostname, $snmp_community, $oids["index"], "1", "", ""));
+	
+	for ($i=0;($i<sizeof($return_arr));$i++) {
+		print $return_arr[$i]["value"] . "\n";
+	}
+}elseif ($cmd == "query") {
+	$arg = $_SERVER["argv"][4];
+	
+	$arr_index = reindex(cacti_snmp_walk($hostname, $snmp_community, $oids["index"], "1", "", ""));
+	$arr = reindex(cacti_snmp_walk($hostname, $snmp_community, $oids[$arg], "1", "", ""));
+	
+	for ($i=0;($i<sizeof($arr_index));$i++) {
+		print $arr_index[$i] . "!" . $arr[$i] . "\n";
+	}
+}elseif ($cmd == "get") {
+	$arg = $_SERVER["argv"][4];
+	$index = $_SERVER["argv"][5];
+	
+	if (($arg == "total") || ($arg == "used")) {
+		print (cacti_snmp_get($hostname, $snmp_community, $oids[$arg] . ".$index", "1", "", "") * cacti_snmp_get($hostname, $snmp_community, $oids["sau"] . ".$index", "1", "", ""));
+	}else{
+		print (cacti_snmp_get($hostname, $snmp_community, $oids[$arg] . ".$index", "1", "", ""));
+	}
+}
+
+function reindex($arr) {
+	$return_arr = array();
+	
+	for ($i=0;($i<sizeof($arr));$i++) {
+		$return_arr[$i] = $arr[$i]["value"];
+	}
+	
+	return $return_arr;
+}
+
+?>
