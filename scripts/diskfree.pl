d5ad93b9fa0e09aceac7c14e4e461890bfba46fc
Ian Berry <iberry@raxnet.net>
2003-07-09 19:34:35 +0000

    long mount point bug (bug#67)
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1094 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/diskfree.pl
+++ scripts/diskfree.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-$ret = `df --block-size=1024 $ARGV[0] | grep -v Filesystem`;
+$ret = `df --block-size=1024 -P $ARGV[0] | grep -v Filesystem`;
 $ret =~ s/($ARGV[0])(.* )(.*[0-9])(.* )(.*[0-9])(.* )(.*[0-9])(.* )(.*[0-9])%(.* )//;
 
 print "megabytes:$7 percent:$9";
