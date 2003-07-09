6046691ac2d6f51b9769266a645d1b0703fbdf17
Ian Berry <iberry@raxnet.net>
2003-07-09 19:26:58 +0000

    add 0.8.1->0.8.2 host table modifications
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1093 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/index.php
+++ install/index.php
@@ -245,6 +245,7 @@ if ($_REQUEST["step"] == "4") {
 			}
 		}elseif ($cacti_versions[$i] == "0.8.2") {
 			db_execute("ALTER TABLE `data_input_data_cache` ADD `host_id` MEDIUMINT( 8 ) NOT NULL AFTER `local_data_id`");
+			db_execute("ALTER TABLE `host` ADD `disabled` CHAR( 2 ) , ADD `status` TINYINT( 2 ) NOT NULL");
 			db_execute("update host_snmp_cache set field_name='ifName' where field_name='ifAlias' and snmp_query_id=1");
 			db_execute("update snmp_query_graph_rrd_sv set text=REPLACE(text,'ifAlias','ifName') where (snmp_query_graph_id=1 or snmp_query_graph_id=13 or snmp_query_graph_id=14 or snmp_query_graph_id=16 or snmp_query_graph_id=9 or snmp_query_graph_id=2 or snmp_query_graph_id=3 or snmp_query_graph_id=4)");
 			db_execute("update snmp_query_graph_sv set text=REPLACE(text,'ifAlias','ifName') where (snmp_query_graph_id=1 or snmp_query_graph_id=13 or snmp_query_graph_id=14 or snmp_query_graph_id=16 or snmp_query_graph_id=9 or snmp_query_graph_id=2 or snmp_query_graph_id=3 or snmp_query_graph_id=4)");
