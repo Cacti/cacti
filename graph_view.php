c5c2e593af1195384afa64f1e5cb39be893838df
Ian Berry <iberry@raxnet.net>
2003-08-17 06:36:39 +0000

    take host permissions into account when on preview mode host dropdown
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1143 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- graph_view.php
+++ graph_view.php
@@ -200,7 +200,7 @@ case 'preview':
 							<option value="graph_view.php?action=preview&host_id=0&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["host_id"] == "0") {?> selected<?php }?>>None</option>
 							
 							<?php
-							$hosts = db_fetch_assoc("select id,CONCAT_WS('',description,' (',hostname,')') as name from host order by description,hostname");
+							$hosts = get_host_array();
 							
 							if (sizeof($hosts) > 0) {
 							foreach ($hosts as $host) {
@@ -285,6 +285,9 @@ case 'list':
 	
 	/* graph permissions */
 	if (read_config_option("global_auth") == "on") {
+		/* get policy information for the sql where clause */
+		$sql_where = get_graph_permissions_sql($current_user["policy_graphs"], $current_user["policy_hosts"], $current_user["policy_graph_templates"]);
+		
 		$graphs = db_fetch_assoc("select
 			graph_templates_graph.local_graph_id,
 			graph_templates_graph.title_cache,
@@ -296,7 +299,7 @@ case 'list':
 			left join user_auth_perms on ((graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1) OR (host.id=user_auth_perms.item_id and user_auth_perms.type=3) OR (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4) and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ")
 			where graph_templates_graph.local_graph_id=graph_local.id
 			and graph_templates_graph.local_graph_id>0
-			and " . get_graph_permissions_sql($current_user["policy_graphs"], $current_user["policy_hosts"], $current_user["policy_graph_templates"]) . "
+			" . (empty($sql_where) ? "" : "and $sql_where") . "
 			group by graph_templates_graph.local_graph_id
 			order by graph_templates_graph.title_cache");
 	}else{
