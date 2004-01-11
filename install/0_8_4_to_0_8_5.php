b499de2c9f42c226188fc4c8a661e9368d582ae1
Ian Berry <iberry@cacti.net>
2004-01-11 01:54:42 +0000

    fix rrd_step overflow issue
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1451 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/0_8_4_to_0_8_5.php
+++ install/0_8_4_to_0_8_5.php
@@ -61,4 +61,7 @@ function upgrade_to_0_8_5() {
 	/* allow 'Unit Exponent Value' = 0 */
 	db_install_execute("0.8.5", "ALTER TABLE `graph_templates_graph` CHANGE `unit_exponent_value` `unit_exponent_value` VARCHAR( 5 ) NOT NULL;");
 	db_install_execute("0.8.5", "UPDATE graph_templates_graph set unit_exponent_value='' where unit_exponent_value='0';");
+	
+	/* allow larger rrd steps */
+	db_install_execute("0.8.5", "ALTER TABLE `data_template_data` CHANGE `rrd_step` `rrd_step` SMALLINT( 8 ) UNSIGNED DEFAULT '0' NOT NULL;");
 }
