9014054837e01050ea67f25aa89123133af1b007
Ian Berry <iberry@cacti.net>
2004-01-19 06:05:14 +0000

    updated the copyright to 2004
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1469 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- rra.php
+++ rra.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2003 Ian Berry                                            |
+ | Copyright (C) 2004 Ian Berry                                            |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
