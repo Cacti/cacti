55782be4bbf0958bb4d512ef8b7bc39457c7567e
Ian Berry <iberry@cacti.net>
2004-01-30 04:28:09 +0000

    include 'snmp_timeout' and 'snmp_version' as a supported field for data input method field type codes
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1493 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/config_form.php
+++ include/config_form.php
@@ -158,7 +158,7 @@ $fields_data_input_field_edit = array(
 	"type_code" => array(
 		"method" => "textbox",
 		"friendly_name" => "Special Type Code",
-		"description" => "If this field should be treated specially by host templates, indicate so here. Valid keywords for this field are 'hostname', 'snmp_community', 'snmp_username', 'snmp_password', and 'snmp_version'.",
+		"description" => "If this field should be treated specially by host templates, indicate so here. Valid keywords for this field are 'hostname', 'snmp_community', 'snmp_username', 'snmp_password', 'snmp_port', 'snmp_timeout', and 'snmp_version'.",
 		"value" => "|arg1:type_code|",
 		"max_length" => "40"
 		),
