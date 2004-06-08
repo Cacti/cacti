48d771e8db682d9d063d844e6506acb96c9846c8
Ian Berry <iberry@cacti.net>
2004-06-08 18:41:19 +0000

    moving all global constant declarations to include/config_constants.php
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1575 860744bd-22fc-0310-8c96-e9fe5004b5ca

---
+++ include/config_constants.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004 Ian Berry                                            |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | cacti: a php-based graphing solution                                    |
+ +-------------------------------------------------------------------------+
+ | Most of this code has been designed, written and is maintained by       |
+ | Ian Berry. See about.php for specific developer credit. Any questions   |
+ | or comments regarding this code should be directed to:                  |
+ | - iberry@raxnet.net                                                     |
+ +-------------------------------------------------------------------------+
+ | - raXnet - http://www.raxnet.net/                                       |
+ +-------------------------------------------------------------------------+
+*/
+
+define("HOST_GROUPING_GRAPH_TEMPLATE", 1);
+define("HOST_GROUPING_DATA_QUERY_INDEX", 2);
+
+define("TREE_ORDERING_NONE", 1);
+define("TREE_ORDERING_ALPHABETIC", 2);
+define("TREE_ORDERING_NUMERIC", 3);
+
+?>
