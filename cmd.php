47c1144d80785bf215f1bcdcbddb594bf8a6f48e
Larry Adams <thewitness@cacti.net>
2004-06-09 02:52:18 +0000

    multiprocess, poller start/stop, snmp host down status and others.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1583 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cmd.php
+++ cmd.php
@@ -34,76 +34,193 @@ include_once($config["base_path"] . "/lib/rrd.php");
 include_once($config["base_path"] . "/lib/functions.php");
 include_once($config["base_path"] . "/lib/graph_export.php");
 
-$polling_items = db_fetch_assoc("select * from data_input_data_cache");
+print $_SERVER["argc"] . "\n";
+print $_SERVER["argv"][1] . "\n";
+print $_SERVER["argv"][2] . "\n";
 
-if (sizeof($polling_items) > 0) {
-foreach ($polling_items as $item) {
-	switch ($item["action"]) {
-	case '0': /* snmp */
-		$output = cacti_snmp_get($item["hostname"], $item["snmp_community"], $item["arg1"], $item["snmp_version"], $item["snmp_username"], $item["snmp_password"], $item["snmp_port"], $item["snmp_timeout"]);
-		print "snmp: " . $item["hostname"] . ":" . $item["snmp_port"] . ", dsname: " . $item["rrd_name"]. ", oid: " . $item["arg1"] . ", value: $output\n";
-		
-		break;
-	case '1': /* one output script */
-		$command = $item["command"];
-		$output = `$command`;
-		print "command: $command, output: $output\n";
-		
-		$data_input_field = db_fetch_row("select id,update_rra from data_input_fields where data_input_id=" . $item["data_input_id"] . " and input_output='out'");
-		
-		if ($data_input_field["update_rra"] == "") {
-			/* DO NOT write data to rrd; put it in the db instead */
-			db_execute("insert into data_input_data (data_input_field_id,data_template_data_id,value)
-				values (" . $data_input_field["id"] . "," . db_fetch_cell("select id from data_template_data 
-				where local_data_id=" . $item["local_data_id"]) . ",'$output')");
-			$item["rrd_name"] = ""; /* no rrd action here */
-		}
-		
-		break;
-	case '2': /* multi output script */
-		$command = $item["command"];
-		$output = `$command`;
-		print "MULTI command: $command, output: $output\n";
-		
-		$output_array = split(" ", $output);
-		
-		for ($i=0;($i<count($output_array));$i++) {
-			$data_input_field = db_fetch_row("select id,update_rra from data_input_fields where data_name='" . ereg_replace("^([a-zA-Z0-9_-]+):.*$", "\\1", $output_array[$i]) . "' and data_input_id=" . $item["data_input_id"] . " and input_output='out'");
-			$rrd_name = db_fetch_cell("select data_source_name from data_template_rrd where local_data_id=" . $item["local_data_id"] . " and data_input_field_id=" . $data_input_field["id"]);
-			
-			if ($data_input_field["update_rra"] == "on") {
-				print "MULTI expansion: found fieldid: " . $data_input_field["id"] . ", found rrdname: $rrd_name, value: " . trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1", $output_array[$i])) . "\n";
-				$update_cache_array{$item["local_data_id"]}{$rrd_name} = trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1", $output_array[$i]));
-			}else{
-				/* DO NOT write data to rrd; put it in the db instead */
-				db_execute("insert into data_input_data (data_input_field_id,data_template_data_id,value)
-					values (" . $data_input_field["id"] . "," . db_fetch_cell("select id from data_template_data 
-					where local_data_id=" . $item["local_data_id"]) . ",'" . trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1", $output_array[$i])) . "')");
-			}
-		}
-		
-		break;
-	}
-	
-	
-	if (!empty($item["rrd_name"])) {
-		$update_cache_array{$item["local_data_id"]}{$item["rrd_name"]} = trim($output);
-	}
-	
-	rrdtool_function_create($item["local_data_id"], false);
+if ( $_SERVER["argc"] == 1 )
+{
+    $polling_items = db_fetch_assoc("SELECT * from data_input_data_cache ORDER by host_id");
 }
+else
+{
+    if ( $_SERVER["argc"] == "3" )
+    {
+         if ( $_SERVER["argv"][1] <= $_SERVER["argv"][2] )
+              $polling_items = db_fetch_assoc("SELECT * from data_input_data_cache " .
+                       "WHERE (host_id >= " .
+                       $_SERVER["argv"][1] .
+                       " and host_id <= " .
+                       $_SERVER["argv"][2] . ") ORDER by host_id");
+         else
+         {
+             print "ERROR: Invalid Arguments.  The first argument must be less than or equal to the first.\n";
+             print "USAGE: CMD.PHP [[first_host] [second_host]]\n";
+             if ( read_config_option("log_perror") == "on" )
+                 log_data("ERROR: Invalid Arguments.  This rist argument must be less than or equal to the first.");
+         }
+    }
+    else
+    {
+        print "ERROR: Invalid Number of Arguments.  You must specify 0 or 2 arguments.\n";
+        if ( read_config_option("log_perror") == "on" )
+            log_data("ERROR: Invalid Number of Arguments.  You must specify 0 or 2 arguments.");
+    }
 }
 
-if (isset($update_cache_array)) {
-	rrdtool_function_update($update_cache_array);
-}else{
-	print "There are no items in your poller cache. Make sure you have at least one data source created. If you do, go to 'Utilities', and select 'Clear Poller Cache'.\n";
+if ((sizeof($polling_items) > 0) && (read_config_option("poller_enabled") == "on"))
+{
+     $host_down = False;
+     $new_host  = True;
+     $last_host = $current_host = "";
+
+     print sizeof($polling_items);
+
+     foreach ($polling_items as $item)
+     {
+        $current_host = $item["hostname"];
+        if ($current_host != $last_host)
+        {
+            $new_host = True;
+            $host_down = False;
+        }
+
+        if ($new_host)
+        {
+            /* Perform an SNMP test to validate the host is alive */
+            /* Wanted to do PING, but will have to wait.          */
+            $last_host = $current_host;
+            $output = cacti_snmp_get($item["hostname"],
+                          $item["snmp_community"],
+                          ".1.3.6.1.2.1.1.5.0" ,
+                          $item["snmp_version"],
+                          $item["snmp_username"],
+                          $item["snmp_password"],
+                          $item["snmp_port"],
+                          $item["snmp_timeout"]);
+            if ((substr_count($output, "ERROR") != 0) || ($output == ""))
+            {
+                $host_down = True;
+                print "ERROR: Host is not respoding to SNMP query.\n";
+                if ( read_config_option("log_perror") == "on" )
+                    log_data(sprintf("ERROR: host '%s' is not responding to SNMP query, assumed down.", $current_host));
+            }
+            $new_host = False;
+        }
+
+        if (!$host_down)
+        {
+            switch ($item["action"]) {
+            case '0': /* snmp */
+                $output = cacti_snmp_get($item["hostname"],
+                       $item["snmp_community"],
+                       $item["arg1"],
+                       $item["snmp_version"],
+                       $item["snmp_username"],
+                       $item["snmp_password"],
+                       $item["snmp_port"],
+                       $item["snmp_timeout"]);
+                print "SNMP: " .
+                       $item["hostname"] . ":" .
+                       $item["snmp_port"] .
+                       ", dsname: " .
+                       $item["rrd_name"] .
+                       ", oid: " .
+                       $item["arg1"] .
+                       ", value: $output\n";
+
+                break;
+            case '1': /* one output script */
+                $command = $item["command"];
+                $output = `$command`;
+                print "CMD: $command, output: $output\n";
+
+                $data_input_field = db_fetch_row("select id,update_rra from data_input_fields where data_input_id=" . $item["data_input_id"] . " and input_output='out'");
+
+                if ($data_input_field["update_rra"] == "")
+                {
+                    /* DO NOT write data to rrd; put it in the db instead */
+                    db_execute("insert into data_input_data (data_input_field_id,data_template_data_id,value)
+                                values (" . $data_input_field["id"] .
+                                "," . db_fetch_cell("SELECT id from data_template_data" .
+                                "where local_data_id=" .
+                                $item["local_data_id"]) .
+                                ",'$output')");
+                    $item["rrd_name"] = ""; /* no rrd action here */
+                }
+
+                break;
+            case '2': /* multi output script */
+                $command = $item["command"];
+                $output = `$command`;
+                print "CMD: MULTI command: $command, output: $output\n";
+
+                $output_array = split(" ", $output);
+
+                for ($i=0;($i<count($output_array));$i++)
+                {
+                    $data_input_field = db_fetch_row("select id,update_rra from data_input_fields" .
+                                 "where data_name='" .
+                                 ereg_replace("^([a-zA-Z0-9_-]+):.*$", "\\1",
+                                 $output_array[$i]) .
+                                 "' and data_input_id=" .
+                                 $item["data_input_id"] .
+                                 " and input_output='out'");
+                    $rrd_name = db_fetch_cell("select data_source_name " .
+                                 "from data_template_rrd where local_data_id=" .
+                                 $item["local_data_id"] .
+                                 " and data_input_field_id=" .
+                                 $data_input_field["id"]);
+
+                    if ($data_input_field["update_rra"] == "on")
+                    {
+                        print "MULTI expansion: found fieldid: " .
+                              $data_input_field["id"] .
+                              ", found rrdname: $rrd_name, value: " .
+                              trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1",
+                              $output_array[$i])) . "\n";
+                        $update_cache_array{$item["local_data_id"]}{$rrd_name} =
+                              trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1", $output_array[$i]));
+                    }
+                    else
+                    {
+                        /* DO NOT write data to rrd; put it in the db instead */
+                        db_execute("insert into data_input_data " .
+                                   "(data_input_field_id,data_template_data_id,value)
+                                   values (" . $data_input_field["id"] . "," .
+                                   db_fetch_cell("select id from data_template_data
+                                   where local_data_id=" . $item["local_data_id"]) .
+                                   ",'" . trim(ereg_replace("^[a-zA-Z0-9_-]+:(.*)$", "\\1",
+                                   $output_array[$i])) . "')");
+                    }
+                }
+
+                break;
+            }
+            /* End Switch */
+
+            if (!empty($item["rrd_name"]))
+            {
+                $update_cache_array{$item["local_data_id"]}{$item["rrd_name"]} = trim($output);
+            }
+
+            rrdtool_function_create($item["local_data_id"], false);
+
+        } /* Next Cache Item */
+     } /* End foreach */
+
+     if (isset($update_cache_array))
+     {
+         rrdtool_function_update($update_cache_array);
+     }
+}
+else
+{
+    print "Either there are no items in the cache or polling is disabled\n";
 }
 
 /* insert the current date/time for graphs */
 db_execute("replace into settings (name,value) values ('date',NOW())");
-print "time: " . (time()-$start) . "\n";
-//print_r($update_cache_array);
 
 /* dump static images/html file if user wants it */
 graph_export();
---
+++ poller.php
@@ -0,0 +1,218 @@
+#!/usr/bin/php -q
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004 Ian Berry                                            |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | cacti: a php-based graphing solution                                    |
+ +-------------------------------------------------------------------------+
+ | Most of this code has been designed, written and is maintained by       |
+ | Ian Berry. See about.php for specific developer credit. Any questions   |
+ | or comments regarding this code should be directed to:                  |
+ | - iberry@raxnet.net                                                     |
+ +-------------------------------------------------------------------------+
+ | - raXnet - http://www.raxnet.net/                                       |
+ +-------------------------------------------------------------------------+
+*/
+
+// Start Initialization Section
+include(dirname(__FILE__) . "/include/config.php");
+include_once($config["base_path"] . "/lib/functions.php");
+include_once($config["base_path"] . "/lib/rrd.php");
+
+// We are not talking to the browser
+$no_http_headers = true;
+
+// Record Start Time
+list($micro,$seconds) = split(" ", microtime());
+$start = $seconds + $micro;
+
+// Let PHP Run Just as Long as It Has To
+ini_set("max_execution_time", "0");
+
+// Get number of polling items from the database
+$polling_items = db_fetch_assoc("select distinct id from host where disabled = '' order by id");
+
+// Retreive the number of concurrent process settings
+$concurrent_processes = read_config_option("concurrent_processes");
+
+// Initialize counters for script file handling
+$host_count = 1;
+
+// Initialize file creation flags
+$change_files = False;
+
+// Initialize file and host count pointers
+$process_file_number = 0;
+$first_host = 0;
+$last_host = 0;
+
+// Obtain some defaults from the database
+$ucactid = read_config_option("using_cactid");
+$max_threads = read_config_option("max_threads");
+// End Initialization Section
+
+// Enter Mainline Processing
+if ((sizeof($polling_items) > 0) and (read_config_option("poller_enabled") == "on"))
+{
+    /* Determine the number of hosts to process per file */
+    $hosts_per_file = ceil(sizeof($polling_items) / $concurrent_processes );
+
+    /* Empty the polling autoexecution directory */
+    if (($config["cacti_server_os"] == "unix") or ($ucactid == "on"))
+    {
+        pclose(popen("rm -r -f " . read_config_option("path_scriptdir") . "/*", "r"));
+    }
+    else
+    {
+        pclose(popen("del " . stri_replace("/","\\",read_config_option("path_scriptdir")) . "\\*.* /Q", "rb"));
+    }
+    /* End Empty polling autoexecution Directory */
+
+    /* Determine Command Name */
+    if (($config["cacti_server_os"] == "unix") and ($ucactid == "on"))
+    {
+        $blast_string = "cactiplus";
+        $command_string = "cactid";
+    }
+    else if ($config["cacti_server_os"] == "unix")
+    {
+        $blast_string = "cactiplus";
+        $command_string = "cmd.php";
+    }
+    else if ($ucactid == "on")
+    {
+        $blast_string = "cactiplus.cmd";
+        $command_string = "cactid.exe";
+    }
+    else
+    {
+        $blast_string = "cactiplus.cmd";
+        $command_string = "cmd.php";
+    }
+
+    /* Initialize file for autoexecution */
+    touch(read_config_option("path_scriptdir") . "/" . $blast_string);
+    $process_exec_file = fopen(read_config_option("path_scriptdir") . "/" . $blast_string, "w");
+
+    /* Populate each execution file with appropriate information */
+    foreach ($polling_items as $item)
+    {
+        if ($host_count == 1)
+        {
+            $first_host = $item["id"];
+        }
+        if ($host_count == $hosts_per_file)
+        {
+            $last_host = $item["id"];
+            $change_files = True;
+        }
+        $host_count ++;
+        if ($change_files)
+        {
+            write_poller_file_item( $config["base_path"], $config["cacti_server_os"], $process_exec_file, $first_host, $last_host, $command_string );
+            $host_count = 1;
+            $change_files = False;
+            $first_host = 0;
+            $last_host = 0;
+        } /* End change_files */
+    } /* End For Each */
+
+    if ($host_count > 1)
+    {
+        $last_host = $item["id"];
+        write_poller_file_item( $config["base_path"], $config["cacti_server_os"], $process_exec_file, $first_host, $last_host, $command_string );
+    }
+
+    /* Close File - Ready to Blast Off!!! */
+    fclose( $process_exec_file );
+
+    /* Execute script file */
+    if ($config["cacti_server_os"] == "unix")
+    {
+        chmod(read_config_option("path_scriptdir") . "/" . $blast_string, "777");
+        exec(read_config_option("path_scriptdir") . "/" . $blast_string);
+    }
+    else
+    {
+        exec(read_config_option("path_scriptdir") . "\\" . $blast_string);
+    }
+    /* End if execute script file */
+
+    /* insert the current date/time for graphs */
+    db_execute("replace into settings (name,value) values ('date',NOW())");
+
+    /* take time and log performance data */
+    list($micro,$seconds) = split(" ", microtime());
+    $end = $seconds + $micro;
+
+    if(read_config_option("log_pstats") == "on")
+        log_data(sprintf("STATS: " .
+                          "Execution Time: %01.4f s, " .
+                          "Method: %s, " .
+                          "Max Processes: %s, " .
+                          "Max Threads/Process: %s, " .
+                          "Polled Hosts: %s, " .
+                          "Hosts/Process: %s",
+                          round($end-$start,4),
+                          $command_string,
+                          $concurrent_processes,
+                          $max_threads,
+                          sizeof($polling_items),
+                          $hosts_per_file));
+}
+else
+{
+    print "There are no items in your poller cache or polling is disabled. Make sure you have at least one data source created. If you do, go to 'Utilities', and select 'Clear Poller Cache'.\n";
+}
+// End Mainline Processing
+
+
+function write_poller_file_item( $base_path, $cacti_server_os, $process_exec_file, $first_host, $last_host, $command_string )
+{
+    if (($cacti_server_os == "unix") and (read_config_option("using_cactid") == "on"))
+    {
+        fputs($process_exec_file,
+             read_config_option("path_cactid") . " " .
+             $first_host . " " .
+             $last_host . " &\r\n");
+    }
+    else if ($cacti_server_os == "unix")
+    {
+        fputs($process_exec_file,
+             read_config_option("path_php_binary") . " " .
+             $base_path . "/" .
+             $command_string . " " .
+             $first_host . " " .
+             $last_host . " &\r\n");
+    }
+    else if (read_config_option("using_cactid") != "on")
+    {
+        fputs($process_exec_file,
+             "start \"Cactiplus\" /I /B " .
+             read_config_option("path_php_binary") . " " .
+             $base_path . "\\cmd.php " .
+             $first_host . " " .
+             $last_host . "\r\n");
+    }
+    else
+    {
+        fputs($process_exec_file,
+             "start \"Cactiplus\" /I /B /SEPARATE " .
+             read_config_option("path_cactid") . " " .
+             $first_host . " " .
+             $last_host . "\r\n");
+    }
+}
+
+?>
