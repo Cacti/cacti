19c91008fb1b92cf58685721d19aa000283f1eea
Larry Adams <thewitness@cacti.net>
2004-06-28 03:20:57 +0000

    Adding pipe php functionality to Cacti
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1613 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- script_server.php
+++ script_server.php
@@ -23,40 +23,46 @@
  | - raXnet - http://www.raxnet.net/                                       |
  +-------------------------------------------------------------------------+
 */
-print "Cacti PHP Script Handler Service Starting\n";
+// print "Cacti PHP Script Handler Service Starting\n";
 
 // load script information to handle script request
 include(dirname(__FILE__) . "/scripts/script_functions.php" );
 
 // process waits for input and then calls functions as required
-print "Ready - \"quit\" command exists.\n";
 while (1) {
-	fputs(STDOUT, ">");
 	$in_string = fgets(STDIN,255);
 	if (strlen($in_string)>0) {
 		if (($in_string != "quit\r\n") && ($in_string != "\r\n")) {
 			// parse function from command
-			$cmd1 = substr($in_string,0,strpos($in_string," "));
+			$in_string = strtr($in_string,"\r","\0");
+			$in_string = strtr($in_string,"\n","\0");
+
+			$cmd = substr($in_string,0,strpos($in_string," "));
 
 			// parse parameters from remainder of command
-			$preparm1 = substr($in_string,strpos($in_string," ")+1);
-			$preparm1 = substr($preparm1,0,strlen($preparm1)-2);
-			$parm1 = explode(" ",$preparm1);
+			$preparm = substr($in_string,strpos($in_string," ")+1);
+//			$preparm = substr($preparm,0,strlen($preparm)-2);
+			$parm = explode(" ",$preparm);
 
 			// check for existance of function.  If exists call it
-			if (function_exists($cmd1)) {
+			if (function_exists($cmd)) {
 				// print output of function arguments
 
-				$result0 = call_user_func_array($cmd1, $parm1);
-				fputs(STDOUT, $result0);
+				$result = call_user_func_array($cmd, $parm);
+				if (strpos($result,"\n") != 0) {
+					fputs(STDOUT, $result);
+				} else {
+					fputs(STDOUT, $result . "\n");
+				}
 			} else {
-				fputs(STDERR, "ERROR: Function does not exist\n");
+				fputs(STDOUT, "ERROR: Function does not exist\n");
 			}
-		}
-		elseif ($in_string == "quit\r\n") {
+		}elseif ($in_string == "quit\r\n") {
+			break;
+		}else {
+			fputs(STDOUT, "ERROR: Problems with input\n");
 			break;
 		}
 	}
 }
-print "Cactid PHP Script Handler Service Exiting";
 ?>
