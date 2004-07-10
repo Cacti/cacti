a57830028971d206dca8a29854ab1da538be39fb
Ian Berry <iberry@cacti.net>
2004-07-10 04:24:42 +0000

    adding 0.8.6 upgrade code
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1665 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/0_8_5a_to_0_8_6.php
+++ install/0_8_5a_to_0_8_6.php
@@ -24,78 +24,87 @@
  +-------------------------------------------------------------------------+
 */
 
-function upgrade_to_0_8_5() {
-	/*
-	ALTER TABLE `graph_tree_items` ADD `host_grouping_type` TINYINT( 3 ) UNSIGNED DEFAULT '1' NOT NULL ,
-ADD `sort_children_type` TINYINT( 3 ) UNSIGNED DEFAULT '1' NOT NULL;
-	UPDATE snmp_query_graph_rrd_sv set text = REPLACE(text,' (In)','') where snmp_query_graph_id = 2;
-	ALTER TABLE `host_snmp_query` ADD `sort_field` VARCHAR( 50 ) NOT NULL ,
-ADD `title_format` VARCHAR( 50 ) NOT NULL ;
-	DROP TABLE `snmp_query_field`;
-	ALTER TABLE `graph_tree_items` CHANGE `order_key` `order_key` VARCHAR( 100 ) DEFAULT '0' NOT NULL;
-	ALTER TABLE `host_snmp_query` ADD `reindex_method` TINYINT( 3 ) UNSIGNED NOT NULL;
+function upgrade_to_0_8_6() {
+	include("../lib/data_query.php");
 
+	db_install_execute("0.8.6", "DROP TABLE `snmp_query_field`;");
+	db_install_execute("0.8.6", "DROP TABLE `data_input_data_cache`;");
+	db_install_execute("0.8.6", "DROP TABLE `data_input_data_fcache`;");
 
-CREATE TABLE `poller` (
-  `id` smallint(5) unsigned NOT NULL auto_increment,
-  `hostname` varchar(250) NOT NULL default '',
-  `ip_address` int(11) unsigned NOT NULL default '0',
-  `last_update` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM;
-CREATE TABLE `poller_field` (
-  `local_data_id` mediumint(8) unsigned NOT NULL default '0',
-  `data_input_field_name` varchar(100) NOT NULL default '',
-  `rrd_data_source_name` varchar(19) NOT NULL default '',
-  `output` text NOT NULL,
-  PRIMARY KEY  (`local_data_id`,`rrd_data_source_name`)
-) TYPE=MyISAM;
-CREATE TABLE `poller_item` (
-  `local_data_id` mediumint(8) unsigned NOT NULL default '0',
-  `poller_id` smallint(5) unsigned NOT NULL default '0',
-  `host_id` mediumint(8) NOT NULL default '0',
-  `action` tinyint(2) unsigned NOT NULL default '1',
-  `hostname` varchar(250) NOT NULL default '',
-  `snmp_community` varchar(100) NOT NULL default '',
-  `snmp_version` tinyint(1) unsigned NOT NULL default '0',
-  `snmp_username` varchar(50) NOT NULL default '',
-  `snmp_password` varchar(50) NOT NULL default '',
-  `snmp_port` mediumint(5) unsigned NOT NULL default '161',
-  `snmp_timeout` mediumint(8) unsigned NOT NULL default '0',
-  `rrd_name` varchar(19) NOT NULL default '',
-  `rrd_path` varchar(255) NOT NULL default '',
-  `rrd_num` tinyint(2) unsigned NOT NULL default '0',
-  `arg1` varchar(255) default NULL,
-  `arg2` varchar(255) default NULL,
-  `arg3` varchar(255) default NULL,
-  PRIMARY KEY  (`local_data_id`,`rrd_name`),
-  KEY `local_data_id` (`local_data_id`)
-) TYPE=MyISAM;
-CREATE TABLE `poller_output` (
-  `local_data_id` mediumint(8) unsigned NOT NULL default '0',
-  `rrd_name` varchar(19) NOT NULL default '',
-  `time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `output` text NOT NULL,
-  PRIMARY KEY  (`local_data_id`,`rrd_name`,`time`)
-) TYPE=MyISAM;
-CREATE TABLE `poller_time` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `poller_id` smallint(5) unsigned NOT NULL default '0',
-  `start_time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `end_time` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM;
-CREATE TABLE `poller_command` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `poller_id` smallint(5) unsigned NOT NULL default '0',
-  `time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `action` tinyint(3) unsigned NOT NULL default '0',
-  `command` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
+	db_install_execute("0.8.6", "CREATE TABLE `poller` (`id` smallint(5) unsigned NOT NULL auto_increment, `hostname` varchar(250) NOT NULL default '', `ip_address` int(11) unsigned NOT NULL default '0', `last_update` datetime NOT NULL default '0000-00-00 00:00:00', PRIMARY KEY  (`id`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_command` (`poller_id` smallint(5) unsigned NOT NULL default '0', `time` datetime NOT NULL default '0000-00-00 00:00:00', `action` tinyint(3) unsigned NOT NULL default '0', `command` varchar(200) NOT NULL default '', PRIMARY KEY  (`poller_id`,`action`,`command`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_field` (`local_data_id` mediumint(8) unsigned NOT NULL default '0', `data_input_field_name` varchar(100) NOT NULL default '', `rrd_data_source_name` varchar(19) NOT NULL default '', `output` text NOT NULL, PRIMARY KEY  (`local_data_id`,`rrd_data_source_name`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_item` (`local_data_id` mediumint(8) unsigned NOT NULL default '0', `poller_id` smallint(5) unsigned NOT NULL default '0', `host_id` mediumint(8) NOT NULL default '0', `action` tinyint(2) unsigned NOT NULL default '1', `hostname` varchar(250) NOT NULL default '', `snmp_community` varchar(100) NOT NULL default '', `snmp_version` tinyint(1) unsigned NOT NULL default '0', `snmp_username` varchar(50) NOT NULL default '', `snmp_password` varchar(50) NOT NULL default '', `snmp_port` mediumint(5) unsigned NOT NULL default '161', `snmp_timeout` mediumint(8) unsigned NOT NULL default '0', `rrd_name` varchar(19) NOT NULL default '', `rrd_path` varchar(255) NOT NULL default '', `rrd_num` tinyint(2) unsigned NOT NULL default '0', `arg1` varchar(255) default NULL, `arg2` varchar(255) default NULL, `arg3` varchar(255) default NULL, PRIMARY KEY  (`local_data_id`,`rrd_name`), KEY `local_data_id` (`local_data_id`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_output` (`local_data_id` mediumint(8) unsigned NOT NULL default '0', `rrd_name` varchar(19) NOT NULL default '', `time` datetime NOT NULL default '0000-00-00 00:00:00', `output` text NOT NULL, PRIMARY KEY  (`local_data_id`,`rrd_name`,`time`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_reindex` (`host_id` mediumint(8) unsigned NOT NULL default '0', `data_query_id` mediumint(8) unsigned NOT NULL default '0', `action` tinyint(3) unsigned NOT NULL default '0', `op` char(1) NOT NULL default '', `assert_value` varchar(100) NOT NULL default '', `arg1` varchar(100) NOT NULL default '', PRIMARY KEY  (`host_id`,`data_query_id`,`arg1`)) TYPE=MyISAM;");
+	db_install_execute("0.8.6", "CREATE TABLE `poller_time` (`id` mediumint(8) unsigned NOT NULL auto_increment, `poller_id` smallint(5) unsigned NOT NULL default '0', `start_time` datetime NOT NULL default '0000-00-00 00:00:00', `end_time` datetime NOT NULL default '0000-00-00 00:00:00', PRIMARY KEY  (`id`)) TYPE=MyISAM;");
 
+	db_install_execute("0.8.6", "ALTER TABLE `graph_tree_items` ADD `host_grouping_type` TINYINT( 3 ) UNSIGNED DEFAULT '1' NOT NULL, ADD `sort_children_type` TINYINT( 3 ) UNSIGNED DEFAULT '1' NOT NULL;");
+	db_install_execute("0.8.6", "ALTER TABLE `host_snmp_query` ADD `sort_field` VARCHAR( 50 ) NOT NULL, ADD `title_format` VARCHAR( 50 ) NOT NULL, ADD `reindex_method` TINYINT( 3 ) UNSIGNED NOT NULL;");
+	db_install_execute("0.8.6", "ALTER TABLE `graph_tree_items` CHANGE `order_key` `order_key` VARCHAR( 100 ) DEFAULT '0' NOT NULL;");
 
-	*/
+	db_install_execute("0.8.6", "UPDATE snmp_query_graph_rrd_sv set text = REPLACE(text,' (In)','') where snmp_query_graph_id = 2;");
+
+	/* update the sort cache */
+	$host_snmp_query = db_fetch_assoc("select host_id,snmp_query_id from host_snmp_query");
+
+	if (sizeof($host_snmp_query) > 0) {
+		foreach ($host_snmp_query as $item) {
+			update_data_query_sort_cache($item["host_id"], $item["snmp_query_id"]);
+		}
+	}
+
+	/* update trees to three characters per tier */
+	$trees = db_fetch_assoc("select id from graph_tree");
+
+	if (sizeof($trees) > 0) {
+		foreach ($trees as $tree) {
+			$tree_items = db_fetch_assoc("select
+				graph_tree_items.id,
+				graph_tree_items.order_key
+				from graph_tree_items
+				where graph_tree_items.graph_tree_id='" . $tree["id"] . "'
+				order by graph_tree_items.order_key");
+
+			if (sizeof($tree_items) > 0) {
+				$_tier = 0;
+
+				foreach ($tree_items as $tree_item) {
+					$tier = tree_tier($tree_item["order_key"], 2);
+
+					/* back off */
+					if ($tier < $_tier) {
+						for ($i=$_tier; $i>$tier; $i--) {
+							print "reset ctr = $i\n";
+							$tier_counter[$i] = 0;
+						}
+					}
+
+					/* we key tier==0 off of '1' and tier>0 off of '0' */
+					if (!isset($tier_counter[$tier])) {
+						$tier_counter[$tier] = 1;
+					}else{
+						$tier_counter[$tier]++;
+					}
+
+					$search_key = preg_replace("/0+$/", "", $tree_item["order_key"]);
+					if (strlen($search_key) % 2 != 0) { $search_key .= "0"; }
+
+					$new_search_key = "";
+					for ($i=1; $i<$tier; $i++) {
+						$new_search_key .= str_pad(strval($tier_counter[$i]),3,'0',STR_PAD_LEFT);
+					}
+
+					/* build the new order key string */
+					$key = str_pad($new_search_key . str_pad(strval($tier_counter[$tier]),3,'0',STR_PAD_LEFT), 90, '0', STR_PAD_RIGHT);
+
+					db_execute("update graph_tree_items set order_key='$key' where id=" . $tree_item["id"]);
+
+					$_tier = $tier;
+				}
+			}
+		}
+	}
 }
 
 ?>
--- install/index.php
+++ install/index.php
@@ -27,7 +27,7 @@
 include("../include/config.php");
 include("../include/config_settings.php");
 
-$cacti_versions = array("0.8", "0.8.1", "0.8.2", "0.8.2a", "0.8.3", "0.8.3a", "0.8.4", "0.8.5", "0.8.5a");
+$cacti_versions = array("0.8", "0.8.1", "0.8.2", "0.8.2a", "0.8.3", "0.8.3a", "0.8.4", "0.8.5", "0.8.5a", "0.8.6");
 
 $old_cacti_version = db_fetch_cell("select cacti from version");
 
@@ -52,19 +52,19 @@ if ($old_cacti_version == $config["cacti_version"]) {
 
 function db_install_execute($cacti_version, $sql) {
 	$sql_install_cache = (isset($_SESSION["sess_sql_install_cache"]) ? $_SESSION["sess_sql_install_cache"] : array());
-	
+
 	if (db_execute($sql)) {
 		$sql_install_cache{sizeof($sql_install_cache)}[$cacti_version][1] = $sql;
 	}else{
 		$sql_install_cache{sizeof($sql_install_cache)}[$cacti_version][0] = $sql;
 	}
-	
+
 	$_SESSION["sess_sql_install_cache"] = $sql_install_cache;
 }
 
 function find_best_path($binary_name) {
 	$search_paths = array("/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin");
-	
+
 	for ($i=0; $i<count($search_paths); $i++) {
 		if ((file_exists($search_paths[$i] . "/" . $binary_name)) && (is_executable($search_paths[$i] . "/" . $binary_name))) {
 			return $search_paths[$i] . "/" . $binary_name;
@@ -92,7 +92,7 @@ $input["path_rrdtool"] = $settings["path"]["path_rrdtool"];
 
 if ($config["cacti_server_os"] == "unix") {
 	$which_rrdtool = find_best_path("rrdtool");
-	
+
 	if (!empty($which_rrdtool)) {
 		$input["path_rrdtool"]["default"] = $which_rrdtool;
 	}elseif (read_config_option("path_rrdtool") != "<DEFAULT>") {
@@ -109,7 +109,7 @@ $input["path_php_binary"] = $settings["path"]["path_php_binary"];
 
 if ($config["cacti_server_os"] == "unix") {
 	$which_php = find_best_path("php");
-	
+
 	if (!empty($which_php)) {
 		$input["path_php_binary"]["default"] = $which_php;
 	}elseif (read_config_option("path_php_binary") != "<DEFAULT>") {
@@ -124,9 +124,9 @@ if ($config["cacti_server_os"] == "unix") {
 /* snmpwalk Binary Path */
 if ($config["cacti_server_os"] == "unix") {
 	$input["path_snmpwalk"] = $settings["path"]["path_snmpwalk"];
-	
+
 	$which_snmpwalk = find_best_path("snmpwalk");
-	
+
 	if (!empty($which_snmpwalk)) {
 		$input["path_snmpwalk"]["default"] = $which_snmpwalk;
 	}elseif (read_config_option("path_snmpwalk") != "<DEFAULT>") {
@@ -139,9 +139,9 @@ if ($config["cacti_server_os"] == "unix") {
 /* snmpget Binary Path */
 if ($config["cacti_server_os"] == "unix") {
 	$input["path_snmpget"] = $settings["path"]["path_snmpget"];
-	
+
 	$which_snmpget = find_best_path("snmpget");
-	
+
 	if (!empty($which_snmpget)) {
 		$input["path_snmpget"]["default"] = $which_snmpget;
 	}elseif (read_config_option("path_snmpget") != "<DEFAULT>") {
@@ -189,46 +189,46 @@ if (empty($_REQUEST["step"])) {
 if ($_REQUEST["step"] == "4") {
 	include_once("../lib/data_query.php");
 	include_once("../lib/utility.php");
-	
+
 	$i = 0;
-	
+
 	/* get all items on the form and write values for them  */
 	while (list($name, $array) = each($input)) {
 		if (isset($_POST[$name])) {
 			db_execute("update settings set value='" . $_POST[$name] . "' where name='$name'");
 		}
 	}
-	
+
 	setcookie(session_name(),"",time() - 3600,"/");
-	
+
 	kill_session_var("sess_config_array");
 	kill_session_var("sess_host_cache_array");
-	
+
 	/* just in case we have hard drive graphs to deal with */
 	run_data_query(db_fetch_cell("select id from host where hostname='127.0.0.1'"), 6);
-	
+
 	/* it's always a good idea to re-populate the poller cache to make sure everything is refreshed and
 	up-to-date */
 	repopulate_poller_cache();
-	
+
 	db_execute("delete from version");
 	db_execute("insert into version (cacti) values ('" . $config["cacti_version"] . "')");
-	
+
 	header ("Location: ../index.php");
 	exit;
 }elseif (($_REQUEST["step"] == "8") && ($_REQUEST["install_type"] == "3")) {
 	/* try to find current (old) version in the array */
 	$version_index = array_search($old_cacti_version, $cacti_versions);
-	
+
 	/* if the version is not found, die */
 	if (!is_int(array_search($old_cacti_version, $cacti_versions))) {
 		print "	<p style='font-family: Verdana, Arial; font-size: 16px; font-weight: bold; color: red;'>Error</p>
-			<p style='font-family: Verdana, Arial; font-size: 12px;'>Invalid Cacti version 
+			<p style='font-family: Verdana, Arial; font-size: 12px;'>Invalid Cacti version
 			<strong>$old_cacti_version</strong>, cannot upgrade to <strong>" . $config["cacti_version"] . "
 			</strong></p>";
 		exit;
 	}
-	
+
 	/* loop from the old version to the current, performing updates for each version in between */
 	for ($i=($version_index+1); $i<count($cacti_versions); $i++) {
 		if ($cacti_versions[$i] == "0.8.1") {
@@ -255,6 +255,9 @@ if ($_REQUEST["step"] == "4") {
 				/* no database upgrades for 0.8.5 -> 0.8.5a */
 				$_REQUEST["step"] = "3";
 			}
+		}elseif ($cacti_versions[$i] == "0.8.6") {
+			include ("0_8_5a_to_0_8_6.php");
+			upgrade_to_0_8_6();
 		}
 	}
 }
@@ -270,12 +273,12 @@ if ($_REQUEST["step"] == "4") {
 			font-size: 10pt;
 			font-family: Verdana, Arial, sans-serif;
 		}
-		
+
 		.code
 		{
 			font-family: Courier New, Courier;
 		}
-		
+
 		.header-text
 		{
 			color: white;
@@ -302,53 +305,53 @@ if ($_REQUEST["step"] == "4") {
 				<tr>
 					<td width="100%" style="font-size: 12px;">
 						<?php if ($_REQUEST["step"] == "1") { ?>
-						
-						<p>Thanks for taking the time to download and install cacti, the complete graphing 
-						solution for your network. Before you can start making cool graphs, there are a few 
+
+						<p>Thanks for taking the time to download and install cacti, the complete graphing
+						solution for your network. Before you can start making cool graphs, there are a few
 						pieces of data that cacti needs to know.</p>
-						
+
 						<p>Make sure you have read and followed the required steps needed to install cacti
-						before continuing. Install information can be found for 
+						before continuing. Install information can be found for
 						<a href="../docs/html/install_unix.html">Unix</a> and <a href="../docs/html/install_windows.html">Win32</a>-based operating systems.</p>
-						
+
 						<p>Also, if this is an upgrade, be sure to reading the <a href="../docs/html/upgrade.html">Upgrade</a> information file.</p>
-						
+
 						<p>Cacti is licensed under the GNU General Public License, you must agree
 						to its provisions before continuing:</p>
-						
+
 						<p class="code">This program is free software; you can redistribute it and/or
 						modify it under the terms of the GNU General Public License
 						as published by the Free Software Foundation; either version 2
 						of the License, or (at your option) any later version.</p>
-						
+
 						<p class="code">This program is distributed in the hope that it will be useful,
 						but WITHOUT ANY WARRANTY; without even the implied warranty of
 						MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 						GNU General Public License for more details.</p>
-						
+
 						<?php }elseif ($_REQUEST["step"] == "2") { ?>
-						
+
 						<p>Please select the type of installation</p>
-						
+
 						<p>
 						<select name="install_type">
 							<option value="1"<?php print ($default_install_type == "1") ? " selected" : "";?>>New Install</option>
 							<option value="3"<?php print ($default_install_type == "3") ? " selected" : "";?>>Upgrade from cacti 0.8.x</option>
 						</select>
 						</p>
-						
+
 						<p>The following information has been determined from cacti's configuration file.
 						If it is not correct, please edit 'include/config.php' before continuing.</p>
-						
+
 						<p class="code">
 						<?php	print "Database User: $database_username<br>";
 							print "Database Hostname: $database_hostname<br>";
 							print "Database: $database_default<br>";
 							print "Server Operating System Type: " . $config["cacti_server_os"] . "<br>"; ?>
 						</p>
-						
+
 						<?php }elseif ($_REQUEST["step"] == "3") { ?>
-						
+
 						<p>Make sure all of these values are correct before continuing.</p>
 						<?php
 						$i = 0;
@@ -357,11 +360,11 @@ if ($_REQUEST["step"] == "4") {
 						while (list($name, $array) = each($input)) {
 							if (isset($input[$name])) {
 								$current_value = $array["default"];
-								
+
 								/* run a check on the path specified only if specified above, then fill a string with
 								the results ('FOUND' or 'NOT FOUND') so they can be displayed on the form */
 								$form_check_string = "";
-								
+
 								if ($array["method"] == "textbox") {
 									if (@file_exists($current_value)) {
 										$form_check_string = "<font color='#008000'>[FOUND]</font> ";
@@ -369,18 +372,18 @@ if ($_REQUEST["step"] == "4") {
 										$form_check_string = "<font color='#FF0000'>[NOT FOUND]</font> ";
 									}
 								}
-								
+
 								/* draw the acual header and textbox on the form */
 								print "<p><strong>" . $form_check_string . $array["friendly_name"] . "</strong>";
-								
+
 								if (!empty($array["friendly_name"])) {
 									print ": " . $array["description"];
 								}else{
 									print "<strong>" . $array["description"] . "</strong>";
 								}
-								
+
 								print "<br>";
-								
+
 								switch ($array["method"]) {
 								case 'textbox':
 									form_text_box($name, $current_value, "", "", "40", "text");
@@ -389,30 +392,30 @@ if ($_REQUEST["step"] == "4") {
 									form_dropdown($name, $array["array"], "", "", $current_value, "", "");
 									break;
 								}
-								
+
 								print "<br></p>";
 							}
-							
+
 							$i++;
 						}?>
-						
+
 						<p><strong><font color="#FF0000">NOTE:</font></strong> Once you click "Finish",
 						all of your settings will be saved and your database will be upgraded if this
 						is an upgrade. You can change any of the settings on this screen at a later
 						time by going to "Cacti Settings" from within Cacti.</p>
-						
+
 						<?php }elseif ($_REQUEST["step"] == "8") { ?>
-						
+
 						<p>Upgrade results:</p>
-						
+
 						<?php
 						$current_version  = "";
 						$upgrade_results = "";
 						$failed_sql_query = false;
-						
+
 						$fail_text = "<span style='color: red; font-weight: bold; font-size: 12px;'>[Fail]</span>&nbsp;";
 						$success_text = "<span style='color: green; font-weight: bold; font-size: 12px;'>[Success]</span>&nbsp;";
-						
+
 						if (isset($_SESSION["sess_sql_install_cache"])) {
 							while (list($index, $arr1) = each($_SESSION["sess_sql_install_cache"])) {
 								while (list($version, $arr2) = each($arr1)) {
@@ -421,38 +424,38 @@ if ($_REQUEST["step"] == "4") {
 											$version_index = array_search($version, $cacti_versions);
 											$upgrade_results .= "<p><strong>" . $cacti_versions{$version_index-1}  . " -> " . $cacti_versions{$version_index} . "</strong></p>\n";
 										}
-										
+
 										$upgrade_results .= "<p class='code'>" . (($status == 0) ? $fail_text : $success_text) . nl2br($sql) . "</p>\n";
-										
+
 										/* if there are one or more failures, make a note because we are going to print
 										out a warning to the user later on */
 										if ($status == 0) {
 											$failed_sql_query = true;
 										}
-										
+
 										$current_version = $version;
 									}
 								}
 							}
-						
+
 							kill_session_var("sess_sql_install_cache");
 						}else{
 							print "<em>No SQL queries have been executed.</em>";
 						}
-						
+
 						if ($failed_sql_query == true) {
 							print "<p><strong><font color='#FF0000'>WARNING:</font></strong> One or more of the SQL queries needed to
 								upgraded your Cacti installation has failed. Please see below for more details. Your
-								Cacti MySQL user must have <strong>SELECT, INSERT, UPDATE, DELETE, ALTER, CREATE, and DROP</strong> 
-								permissions. You should try executing the failed queries as 'root' to ensure that you do not have 
+								Cacti MySQL user must have <strong>SELECT, INSERT, UPDATE, DELETE, ALTER, CREATE, and DROP</strong>
+								permissions. You should try executing the failed queries as 'root' to ensure that you do not have
 								a permissions problem.</p>\n";
 						}
-						
+
 						print $upgrade_results;
 						?>
-						
+
 						<?php }?>
-						
+
 						<p align="right"><input type="image" src="install_<?php if ($_REQUEST["step"] == "3") {?>finish<?php }else{?>next<?php }?>.gif" alt="<?php if ($_REQUEST["step"] == "3"){?>Finish<?php }else{?>Next<?php }?>"></p>
 					</td>
 				</tr>
