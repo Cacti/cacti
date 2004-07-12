59496cfd0e041013e31a5fcf3835e109001ac304
Larry Adams <thewitness@cacti.net>
2004-07-12 19:34:58 +0000

    added support for snmp_timeout and snmp_retries in ucd/net snmp options as well.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@1695 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/snmp.php
+++ lib/snmp.php
@@ -64,9 +64,9 @@ function cacti_snmp_get($hostname, $community, $oid, $version, $username, $passw
 		if (empty($snmp_auth)) { return; }
 
 		if (read_config_option("snmp_version") == "ucd-snmp") {
-			$snmp_value = exec(read_config_option("path_snmpget") . " -O vt -v$version -t $timeout $hostname:$port $snmp_auth $oid");
+			$snmp_value = exec(read_config_option("path_snmpget") . " -O vt -v$version -t $timeout -r $retries $hostname:$port $snmp_auth $oid");
 		}elseif (read_config_option("snmp_version") == "net-snmp") {
-			$snmp_value = exec(read_config_option("path_snmpget") . " -O vt $snmp_auth -v $version -t $timeout $hostname:$port $oid");
+			$snmp_value = exec(read_config_option("path_snmpget") . " -O vt $snmp_auth -v $version -t $timeout -r $retries $hostname:$port $oid");
 		}
 	}
 
