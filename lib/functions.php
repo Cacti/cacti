4121280dec9272341bb8354748445ea43389020e
Larry Adams <thewitness@cacti.net>
2004-08-25 09:10:25 +0000

    change default logfile to cacti.log
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@2007 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/functions.php
+++ lib/functions.php
@@ -319,7 +319,7 @@ function cacti_log($string, $output = false, $environ = "CMDPHP") {
 	/* Log to Logfile */
 	if ((($logdestination == 1) || ($logdestination == 2)) && (read_config_option("log_verbosity") != POLLER_VERBOSITY_NONE)) {
 		if ($logfile == "") {
-			$logfile = $config["base_path"] . "/log/rrd.log";
+			$logfile = $config["base_path"] . "/log/cacti.log";
 		}
 
 		/* echo the data to the log (append) */
