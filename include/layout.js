c3229ca4fb70dfb7feed6e84d28ae344a8ad94ad
Ian Berry <iberry@cacti.net>
2004-09-17 03:49:12 +0000

    fix js error when data query indexes go away (thanks lr)
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/trunk/cacti@2081 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/layout.js
+++ include/layout.js
@@ -27,16 +27,16 @@
 function gt_update_selection_indicators() {
 	if (document.getElementById) {
 		there_are_any_unchecked_ones = false;
-		
+
 		for (var j = 0; j < document.chk.elements.length; j++) {
 			if (document.chk.elements[j].name.substr(0,3) == 'cg_') {
 				if (document.chk.elements[j].checked == false) {
-					there_are_any_unchecked_ones = true;      
+					there_are_any_unchecked_ones = true;
 				}
-				
+
 				if (!isNaN(document.chk.elements[j].name.substr(3))) {
 					lineid = document.getElementById('gt_line' + document.chk.elements[j].name.substr(3));
-					
+
 					if (document.chk.elements[j].checked) {
 						lineid.style.backgroundColor = 'gold';
 					}else{
@@ -50,13 +50,13 @@ function gt_update_selection_indicators() {
 
 function gt_select_line(graph_template_id, update) {
 	if (gt_is_disabled(graph_template_id)) { return; }
-	
+
 	if (document.getElementById) {
 		msgid = document.getElementById('cg_' + graph_template_id);
 		lineid = document.getElementById('gt_line'+ graph_template_id);
-		
+
 		if (!update) msgid.checked = !msgid.checked;
-		
+
 		gt_update_selection_indicators();
 	}
 }
@@ -67,23 +67,23 @@ function gt_is_disabled(graph_template_id) {
 			return true;
 		}
 	}
-	
+
 	return false;
 }
 
 function gt_update_deps(num_columns) {
 	gt_reset_deps(num_columns);
-	
+
 	for (var i = 0; i < gt_created_graphs.length; i++) {
 		for (var j = 0; j < num_columns; j++) {
 			lineid = document.getElementById('gt_text' + gt_created_graphs[i] + '_' + j);
 			lineid.style.color = '999999';
 		}
-		
+
 		chkbx = document.getElementById('cg_' + gt_created_graphs[i]);
 		chkbx.style.visibility = 'hidden';
 		chkbx.checked = false;
-		
+
 		lineid = document.getElementById('gt_line' + gt_created_graphs[i]);
 		lineid.style.backgroundColor = '';
 	}
@@ -91,14 +91,14 @@ function gt_update_deps(num_columns) {
 
 function gt_reset_deps(num_columns) {
 	var prefix = 'cg_'
-	
+
 	for (var i = 0; i < document.chk.elements.length; i++) {
 		if (document.chk.elements[i].name.substr( 0, prefix.length ) == prefix) {
 			for (var j = 0; j < num_columns; j++) {
 				lineid = document.getElementById('gt_text' + document.chk.elements[i].name.substr(prefix.length) + '_' + j);
 				lineid.style.color = '000000';
 			}
-			
+
 			chkbx = document.getElementById('cg_' + document.chk.elements[i].name.substr(prefix.length));
 			chkbx.style.visibility = 'visible';
 		}
@@ -109,15 +109,15 @@ function gt_reset_deps(num_columns) {
 function dq_update_selection_indicators() {
 	if (document.getElementById) {
 		there_are_any_unchecked_ones = false;
-		
+
 		for (var j = 0; j < document.chk.elements.length; j++) {
 			if( document.chk.elements[j].name.substr( 0, 3 ) == 'sg_') {
 				if (document.chk.elements[j].checked == false) {
-					there_are_any_unchecked_ones = true;      
+					there_are_any_unchecked_ones = true;
 				}
-				
+
 				lineid = document.getElementById('line'+ document.chk.elements[j].name.substr(3));
-				
+
 				if (document.chk.elements[j].checked) {
 					lineid.style.backgroundColor = 'gold';
 				}else{
@@ -130,13 +130,13 @@ function dq_update_selection_indicators() {
 
 function dq_select_line(snmp_query_id, snmp_index, update) {
 	if (dq_is_disabled(snmp_query_id, snmp_index)) { return; }
-	
+
 	if (document.getElementById) {
 		msgid = document.getElementById('sg_' + snmp_query_id + '_' + snmp_index);
 		lineid = document.getElementById('line'+ snmp_query_id + '_' + snmp_index);
-		
+
 		if (!update) msgid.checked = !msgid.checked;
-		
+
 		dq_update_selection_indicators();
 	}
 }
@@ -144,47 +144,49 @@ function dq_select_line(snmp_query_id, snmp_index, update) {
 function dq_is_disabled(snmp_query_id, snmp_index) {
 	dropdown = document.getElementById('sgg_' + snmp_query_id);
 	var snmp_query_graph_id = dropdown.value
-	
+
 	for (var i = 0; i < created_graphs[snmp_query_graph_id].length; i++) {
 		if (created_graphs[snmp_query_graph_id][i] == snmp_index) {
 			return true;
 		}
 	}
-	
+
 	return false;
 }
 
 function dq_update_deps(snmp_query_id, num_columns) {
 	dq_reset_deps(snmp_query_id, num_columns);
-	
+
 	dropdown = document.getElementById('sgg_' + snmp_query_id);
 	var snmp_query_graph_id = dropdown.value
-	
+
 	for (var i = 0; i < created_graphs[snmp_query_graph_id].length; i++) {
 		for (var j = 0; j < num_columns; j++) {
 			lineid = document.getElementById('text' + snmp_query_id + '_' + created_graphs[snmp_query_graph_id][i] + '_' + j);
-			lineid.style.color = '999999';
+			if (lineid) { lineid.style.color = '999999' };
 		}
-		
+
 		chkbx = document.getElementById('sg_' + snmp_query_id + '_' + created_graphs[snmp_query_graph_id][i]);
-		chkbx.style.visibility = 'hidden';
-		chkbx.checked = false;
-		
+		if ( chkbx ) {
+			chkbx.style.visibility = 'hidden';
+			chkbx.checked = false;
+		}
+
 		lineid = document.getElementById('line' + snmp_query_id + '_' + created_graphs[snmp_query_graph_id][i]);
-		lineid.style.backgroundColor = '';
+		if (lineid) { lineid.style.backgroundColor = '' };
 	}
 }
 
 function dq_reset_deps(snmp_query_id, num_columns) {
 	var prefix = 'sg_' + snmp_query_id + '_'
-	
+
 	for (var i = 0; i < document.chk.elements.length; i++) {
 		if (document.chk.elements[i].name.substr( 0, prefix.length ) == prefix) {
 			for (var j = 0; j < num_columns; j++) {
 				lineid = document.getElementById('text' + snmp_query_id + '_' + document.chk.elements[i].name.substr(prefix.length) + '_' + j);
 				lineid.style.color = '000000';
 			}
-			
+
 			chkbx = document.getElementById('sg_' + snmp_query_id + '_' + document.chk.elements[i].name.substr(prefix.length));
 			chkbx.style.visibility = 'visible';
 		}
