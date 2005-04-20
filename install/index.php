b2dbc5c1ad77c4e7c7426b1668b8a39e41e26e66
Larry Adams <thewitness@cacti.net>
2005-04-20 22:10:18 +0000

    another change for upcoming release
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@2709 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/index.php
+++ install/index.php
@@ -29,7 +29,7 @@ include("../include/config.php");
 /* allow the upgrade script to run for as long as it needs to */
 ini_set("max_execution_time", "0");
 
-$cacti_versions = array("0.8", "0.8.1", "0.8.2", "0.8.2a", "0.8.3", "0.8.3a", "0.8.4", "0.8.5", "0.8.5a", "0.8.6", "0.8.6a", "0.8.6b", "0.8.6c");
+$cacti_versions = array("0.8", "0.8.1", "0.8.2", "0.8.2a", "0.8.3", "0.8.3a", "0.8.4", "0.8.5", "0.8.5a", "0.8.6", "0.8.6a", "0.8.6b", "0.8.6c", "0.8.6d");
 
 $old_cacti_version = db_fetch_cell("select cacti from version");
 
@@ -265,6 +265,9 @@ if ($_REQUEST["step"] == "4") {
 		}elseif ($cacti_versions[$i] == "0.8.6a") {
 			include ("0_8_6_to_0_8_6a.php");
 			upgrade_to_0_8_6a();
+		}elseif ($cacti_versions[$i] == "0.8.6d") {
+			include ("0_8_6c_to_0_8_6d.php");
+			upgrade_to_0_8_6d();
 		}
 	}
 
