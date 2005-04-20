96bbf4eef6831e2ec3bbabffb8959b64711679cc
Larry Adams <thewitness@cacti.net>
2005-04-20 22:11:44 +0000

    another change for upcoming release
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@2710 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/0_8_6c_to_0_8_6d.php
+++ install/0_8_6c_to_0_8_6d.php
@@ -29,4 +29,5 @@ function upgrade_to_0_8_6d() {
 	db_install_execute("0.8.6d", "ALTER TABLE `host_snmp_cache` CHANGE `snmp_index` `snmp_index` VARCHAR( 100 ) NOT NULL;");
 	db_install_execute("0.8.6d", "ALTER TABLE `data_local` CHANGE `snmp_index` `snmp_index` VARCHAR( 100 ) NOT NULL;");
 	db_install_execute("0.8.6d", "ALTER TABLE `graph_local` CHANGE `snmp_index` `snmp_index` VARCHAR( 100 ) NOT NULL;");
+}	
 ?>
