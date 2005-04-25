395307f432d422c6e9fd521a5c661a28ddce3bcb
Ian Berry <iberry@cacti.net>
2005-04-25 00:14:58 +0000

    make cross-platform compliant (thanks kuuse@redantigua.com)
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@2714 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/diskfree.sh
+++ scripts/diskfree.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-df --block-size=1024 -P $1 | perl -ape '$F[4]=~tr/%//d;}{print "megabytes:$F[3] percent:$F[4]"';
+df -k $1 | grep -v Filesystem| awk '{printf "megabytes:" $4 " percent:" int($5)}'
