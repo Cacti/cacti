954b00a3e03c126550b5318c79be960901107f5c
Larry Adams <thewitness@cacti.net>
2005-06-10 23:04:15 +0000

    resolve issue with zoom graph and missing dhtml include
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@2951 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- graph.php
+++ graph.php
@@ -31,8 +31,9 @@ if (!isset($_REQUEST["view_type"])) { $_REQUEST["view_type"] = ""; }
 $guest_account = true;
 include("./include/auth.php");
 include("./lib/rrd.php");
+include("./lib/html_tree.php");
 include("./include/top_graph_header.php");
-
+                               
 /* ================= input validation ================= */
 input_validate_input_regex(get_request_var("rra_id"), "^([0-9]+|all)$");
 input_validate_input_number(get_request_var("local_graph_id"));
