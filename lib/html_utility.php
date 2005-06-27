bb834403ee100f4ff3db45e41ffc4fc0d410ac5b
Tony Roman <rony@cacti.net>
2005-06-27 12:08:35 +0000

    Invalid version committed, corrected
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3019 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/html_utility.php
+++ lib/html_utility.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2005 The Cacti Group                                      |
+ | Copyright (C) 2004 Ian Berry                                            |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
@@ -13,12 +13,14 @@
  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
  | GNU General Public License for more details.                            |
  +-------------------------------------------------------------------------+
- | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ | cacti: a php-based graphing solution                                    |
  +-------------------------------------------------------------------------+
- | This code is designed, written, and maintained by the Cacti Group. See  |
- | about.php and/or the AUTHORS file for specific developer information.   |
+ | Most of this code has been designed, written and is maintained by       |
+ | Ian Berry. See about.php for specific developer credit. Any questions   |
+ | or comments regarding this code should be directed to:                  |
+ | - iberry@raxnet.net                                                     |
  +-------------------------------------------------------------------------+
- | http://www.cacti.net/                                                   |
+ | - raXnet - http://www.raxnet.net/                                       |
  +-------------------------------------------------------------------------+
 */
 
@@ -40,8 +42,6 @@
 function inject_form_variables(&$form_array, $arg1 = array(), $arg2 = array(), $arg3 = array(), $arg4 = array()) {
 	$check_fields = array("value", "array", "friendly_name", "description", "sql", "sql_print", "form_id", "items");
 
-	reset($form_array);
-
 	/* loop through each available field */
 	while (list($field_name, $field_array) = each($form_array)) {
 		/* loop through each sub-field that we are going to check for variables */
@@ -58,9 +58,6 @@ function inject_form_variables(&$form_array, $arg1 = array(), $arg2 = array(), $
 					}else{
 						/* the existing value is probably a single variable */
 						$form_array[$field_name][$field_to_check] = str_replace($matches[0], ${$matches[1]}, $field_array[$field_to_check]);
-
-						/* extra global variables */
-						//$form_array[$field_name][$field_to_check] = str_replace("|var:current_page|", basename($_SERVER["PHP_SELF"]), $field_array[$field_to_check]);
 					}
 				}else{
 					/* copy the value down from the array/key specified in the variable */
@@ -95,12 +92,10 @@ function form_alternate_row_color($row_color1, $row_color2, $row_value) {
    @arg $html_boolean - the value of the HTML checkbox
    @returns - true or false based on the value of the HTML checkbox */
 function html_boolean($html_boolean) {
-	if ($html_boolean == "") {
-		return 0;
-	}else if ($html_boolean == "on") {
-		return 1;
+	if ($html_boolean == "on") {
+		return true;
 	}else{
-		return $html_boolean;
+		return false;
 	}
 }
 
@@ -111,9 +106,9 @@ function html_boolean($html_boolean) {
      checkbox */
 function html_boolean_friendly($html_boolean) {
 	if ($html_boolean == "on") {
-		return _("Selected");
+		return "Selected";
 	}else{
-		return _("Not Selected");
+		return "Not Selected";
 	}
 }
 
@@ -130,6 +125,61 @@ function get_checkbox_style() {
 		return "padding: 4px; margin: 4px;";
 	}
 }
+
+/* get_request_var - returns the current value of a PHP $_GET variable, optionally
+     returning a default value if the request variable does not exist
+   @arg $name - the name of the request variable. this should be a valid key in the
+     $_GET array
+   @arg $default - the value to return if the specified name does not exist in the
+     $_GET array
+   @returns - the value of the request variable */
+function get_request_var($name, $default = "")
+{
+	if (isset($_GET[$name]))
+	{
+		return $_GET[$name];
+	} else
+	{
+		return $default;
+	}
+}
+
+/* get_request_var_post - returns the current value of a PHP $_POST variable, optionally
+     returning a default value if the request variable does not exist
+   @arg $name - the name of the request variable. this should be a valid key in the
+     $_POST array
+   @arg $default - the value to return if the specified name does not exist in the
+     $_POST array
+   @returns - the value of the request variable */
+function get_request_var_post($name, $default = "")
+{
+	if (isset($_POST[$name]))
+	{
+		return $_POST[$name];
+	} else
+	{
+		return $default;
+	}
+}
+
+/* get_request_var_request - returns the current value of a PHP $_POST variable, optionally
+     returning a default value if the request variable does not exist
+   @arg $name - the name of the request variable. this should be a valid key in the
+     $_REQUEST array
+   @arg $default - the value to return if the specified name does not exist in the
+     $_REQUEST array
+   @returns - the value of the request variable */
+function get_request_var_request($name, $default = "")
+{
+	if (isset($_REQUEST[$name]))
+	{
+		return $_REQUEST[$name];
+	} else
+	{
+		return $default;
+	}
+}
+
 
 /* load_current_session_value - finds the correct value of a variable that is being
      cached as a session variable on an HTML form
@@ -162,17 +212,17 @@ function get_colored_device_status($disabled, $status) {
 		);
 
 	if ($disabled) {
-		return "<span style='color: #$disabled_color'>" . _("Disabled") . "</a>";
+		return "<span style='color: #$disabled_color'>Disabled</a>";
 	}else{
 		switch ($status) {
 			case HOST_DOWN:
-				return "<span style='color: #" . $status_colors[HOST_DOWN] . "'>" . _("Down") . "</a>"; break;
+				return "<span style='color: #" . $status_colors[HOST_DOWN] . "'>Down</a>"; break;
 			case HOST_RECOVERING:
-				return "<span style='color: #" . $status_colors[HOST_RECOVERING] . "'>" . _("Recovering") . "</a>"; break;
+				return "<span style='color: #" . $status_colors[HOST_RECOVERING] . "'>Recovering</a>"; break;
 			case HOST_UP:
-				return "<span style='color: #" . $status_colors[HOST_UP] . "'>" . _("Up") . "</a>"; break;
+				return "<span style='color: #" . $status_colors[HOST_UP] . "'>Up</a>"; break;
 			default:
-				return "<span style='color: #0000ff'>" . _("Unknown") . "</a>"; break;
+				return "<span style='color: #0000ff'>Unknown</a>"; break;
 		}
 	}
 }
