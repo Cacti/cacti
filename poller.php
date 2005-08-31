fc41efe3507e665bf53d9b0591159c4517516ccf
Larry Adams <thewitness@cacti.net>
2005-08-31 02:23:29 +0000

    make export work properly
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3177 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- poller.php
+++ poller.php
@@ -224,7 +224,7 @@ if (read_config_option("poller_enabled") == "on") {
 
 	/* graph export */
 	$command_string = read_config_option("path_php_binary");
-	$extra_args = "-q " . $config["base_path"] . "/export.php";
+	$extra_args = "-q " . $config["base_path"] . "/poller_export.php";
 	exec_background($command_string, "$extra_args");
 
 	if ($method == "cactid") {
