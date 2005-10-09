45d9dae93255c214f68b3547aac9ef8b14a6beb3
Larry Adams <thewitness@cacti.net>
2005-10-09 19:27:42 +0000

    additional changes for MySQL 5.x
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3213 860744bd-22fc-0310-8c96-e9fe5004b5ca

---
+++ install/0_8_6g_to_0_8_6h.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004 Ian Berry                                            |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | cacti: a php-based graphing solution                                    |
+ +-------------------------------------------------------------------------+
+ | Most of this code has been designed, written and is maintained by       |
+ | Ian Berry. See about.php for specific developer credit. Any questions   |
+ | or comments regarding this code should be directed to:                  |
+ | - iberry@raxnet.net                                                     |
+ +-------------------------------------------------------------------------+
+ | - raXnet - http://www.raxnet.net/                                       |
+ +-------------------------------------------------------------------------+
+*/
+
+function upgrade_to_0_8_6h() {
+	/* changes for ping result times */
+	db_install_execute("0.8.6h", "ALTER TABLE `cacti086` `host` MODIFY COLUMN `min_time` DECIMAL(10,5) DEFAULT 9.99999;");
+	db_install_execute("0.8.6h", "ALTER TABLE `cacti086` `host` MODIFY COLUMN `max_time` DECIMAL(10,5) DEFAULT 0.00000;");
+	db_install_execute("0.8.6h", "ALTER TABLE `cacti086` `host` MODIFY COLUMN `cur_time` DECIMAL(10,5) DEFAULT 0.00000;");
+	db_install_execute("0.8.6h", "ALTER TABLE `cacti086` `host` MODIFY COLUMN `avg_time` DECIMAL(10,5) DEFAULT 0.00000;");
+ }
+?>
