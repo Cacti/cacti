4d91d01620d9539864bc5bd7bb63b4bcf23c3087
Larry Adams <thewitness@cacti.net>
2005-12-03 21:37:09 +0000

    lvm patch to add additional filtering to graph_items
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3398 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/html/inc_graph_items_filter_table.php
+++ include/html/inc_graph_items_filter_table.php
@@ -8,33 +8,35 @@
 					</td>
 					<td>
 						<select name="cbo_host_id" onChange="window.location=document.form_graph_items.cbo_host_id.options[document.form_graph_items.cbo_host_id.selectedIndex].value">
-							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_host_id=-1&ds_data_template_id=<?php print $_REQUEST["ds_data_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["ds_host_id"] == "-1") {?> selected<?php }?>>Any</option>
-							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_host_id=0&ds_data_template_id=<?php print $_REQUEST["ds_data_template_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["ds_host_id"] == "0") {?> selected<?php }?>>None</option>
+							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_host_id=-1&ds_data_template_id=<?php print $_REQUEST["ds_data_template_id"];?>"<?php if ($_REQUEST["ds_host_id"] == "-1") {?> selected<?php }?>>Any</option>
+							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_host_id=0&ds_data_template_id=<?php print $_REQUEST["ds_data_template_id"];?>"<?php if ($_REQUEST["ds_host_id"] == "0") {?> selected<?php }?>>None</option>
 							<?php
 							$hosts = db_fetch_assoc("select id,CONCAT_WS('',description,' (',hostname,')') as name from host order by description,hostname");
 
 							if (sizeof($hosts) > 0) {
 								foreach ($hosts as $host) {
-									print "<option value='graphs_items.php?action=item_edit&local_graph_id=" . $_REQUEST["local_graph_id"] . "&ds_host_id=" . $host["id"] . "&ds_data_template_id=" . $_REQUEST["ds_data_template_id"] . "&filter=" . $_REQUEST["filter"] . "'"; if ($_REQUEST["ds_host_id"] == $host["id"]) { print " selected"; } print ">" . title_trim($host["name"], 25) . "</option>\n";
+									print "<option value='graphs_items.php?action=item_edit&local_graph_id=" . $_REQUEST["local_graph_id"] . "&ds_host_id=" . $host["id"] . "&ds_data_template_id=" . $_REQUEST["ds_data_template_id"] . "'"; if ($_REQUEST["ds_host_id"] == $host["id"]) { print " selected"; } print ">" . $host["name"] . "</option>\n";
 								}
 							}
 							?>
 
 						</select>
 					</td>
+				</tr>
+				<tr>
 					<td>
-						&nbsp;Data Template:&nbsp;
+						Data Template:&nbsp;
 					</td>
 					<td>
 						<select name="cbo_data_template_id" onChange="window.location=document.form_graph_items.cbo_data_template_id.options[document.form_graph_items.cbo_data_template_id.selectedIndex].value">
-							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_data_template_id=-1&ds_host_id=<?php print $_REQUEST["ds_host_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["ds_data_template_id"] == "-1") {?> selected<?php }?>>Any</option>
-							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_data_template_id=0&ds_host_id=<?php print $_REQUEST["ds_host_id"];?>&filter=<?php print $_REQUEST["filter"];?>"<?php if ($_REQUEST["ds_data_template_id"] == "0") {?> selected<?php }?>>None</option>
+							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_data_template_id=-1&ds_host_id=<?php print $_REQUEST["ds_host_id"];?>"<?php if ($_REQUEST["ds_data_template_id"] == "-1") {?> selected<?php }?>>Any</option>
+							<option value="graphs_items.php?action=item_edit&local_graph_id=<?php print $_REQUEST["local_graph_id"];?>&ds_data_template_id=0&ds_host_id=<?php print $_REQUEST["ds_host_id"];?>"<?php if ($_REQUEST["ds_data_template_id"] == "0") {?> selected<?php }?>>None</option>
 							<?php
 							$data_templates = db_fetch_assoc("select id, name from data_template order by name");
 
 							if (sizeof($data_templates) > 0) {
 								foreach ($data_templates as $data_template) {
-									print "<option value='graphs_items.php?action=item_edit&local_graph_id=" . $_REQUEST["local_graph_id"] . "&ds_data_template_id=" . $data_template["id"]. "&ds_host_id=" . $_REQUEST["ds_host_id"]  . "&filter=" . $_REQUEST["filter"] . "'"; if ($_REQUEST["ds_data_template_id"] == $data_template["id"]) { print " selected"; } print ">" . title_trim($data_template["name"], 25) . "</option>\n";
+									print "<option value='graphs_items.php?action=item_edit&local_graph_id=" . $_REQUEST["local_graph_id"] . "&ds_data_template_id=" . $data_template["id"]. "&ds_host_id=" . $_REQUEST["ds_host_id"] . "'"; if ($_REQUEST["ds_data_template_id"] == $data_template["id"]) { print " selected"; } print ">" . $data_template["name"] . "</option>\n";
 								}
 							}
 							?>
