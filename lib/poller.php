f9ddcf25ed2c7cad2d2eb5d08a7a8cb7505c09f1
Larry Adams <thewitness@cacti.net>
2005-12-10 02:12:39 +0000

    dealing with issue relative to updates in SVN
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3419 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/poller.php
+++ lib/poller.php
@@ -24,6 +24,7 @@
  +-------------------------------------------------------------------------+
 */
 
+
 /* exec_poll - executes a command and returns its output
    @arg $command - the command to execute
    @returns - the output of $command after execution */
