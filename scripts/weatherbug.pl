bfc60412497cdfc6349357995239861c8571cc37
Larry Adams <thewitness@cacti.net>
2006-03-12 17:57:52 +0000

    Allow weatherbug.pl to return negative numbers. Courtesy Mark S. Krenz (mkrenz at cook-inc dot com)
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3569 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/weatherbug.pl
+++ scripts/weatherbug.pl
@@ -2,7 +2,7 @@
 
 $output = `/bin/bash -c 'wget --quiet -O - \"http:\/\/wisapidata.weatherbug.com\/WxDataISAPI\/WxDataISAPI.dll?Magic=10991&RegNum=3647055&ZipCode=17241&StationID=NWVLL&Units=0&Version=2.7&Fore=1&t=1015084854\/"'`;
 
-$output =~ s/[^0-9|\|]*//gi;
+$output =~ s/[^-0-9|\|]*//gi;
 
 @weather = split(/\|/, $output);
 
@@ -20,7 +20,6 @@ $output =~ s/[^0-9|\|]*//gi;
 # [14] - Dew Point
 # [15] - Wind Chill
 
-print "current_temp:" . $weather[3] . " wind_speed:" . $weather[5] . " barometer:" . $weather[10] . " humidity:" . 
-	$weather[11] . " high_temp:" . $weather[12] . " low_temp:" . $weather[13] . " dew_point_temp:" . 
+print "current_temp:" . $weather[3] . " wind_speed:" . $weather[5] . " barometer:" . $weather[10] . " humidity:" .
+	$weather[11] . " high_temp:" . $weather[12] . " low_temp:" . $weather[13] . " dew_point_temp:" .
 	$weather[14] . " wind_chill_temp:" . $weather[15];
-
