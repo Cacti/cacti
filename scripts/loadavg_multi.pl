49a8b58caee8f7aac1ab9a714494a59be72fa56a
Ian Berry <iberry@cacti.net>
2006-04-28 02:52:36 +0000

    Make load average script compatible with Mac OS X systems (thanks Marc Liyanage)
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3608 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/loadavg_multi.pl
+++ scripts/loadavg_multi.pl
@@ -7,6 +7,6 @@ close(PROCESS);
 
 #   9:36pm  up 15 days, 11:37,  2 users,  load average: 0.14, 0.13, 0.10
 
-$avg =~ s/^.*:\s(\d+\.\d{2}),\s(\d+\.\d{2}),\s(\d+\.\d{2})$//;
+$avg =~ s/^.*:\s(\d+\.\d{2}),?\s(\d+\.\d{2}),?\s(\d+\.\d{2})$//;
 
 print "1min:$1 5min:$2 10min:$3";
