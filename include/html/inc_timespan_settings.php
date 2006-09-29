f6a161f344283bbbf5602d8f82b37bd31c4cad1a
Larry Adams <thewitness@cacti.net>
2006-09-29 00:44:24 +0000

    remove issue with session variable not being set.  This code needs an overhaul.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3793 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/html/inc_timespan_settings.php
+++ include/html/inc_timespan_settings.php
@@ -116,7 +116,7 @@ function process_user_input(&$timespan) {
 		}
 	}else {
 		if ((isset($_GET["predefined_timespan"]) && ($_GET["predefined_timespan"] != GT_CUSTOM)) ||
-			(!isset($_GET["predefined_timespan"]) && ($_SESSION["custom"] == 0)) ||
+			(!isset($_GET["predefined_timespan"]) && ((isset($_SESSION["custom"])) && ($_SESSION["custom"] == 0))) ||
 			(!isset($_SESSION["sess_current_date1"]))) {
 			set_preset_timespan($timespan);
 		}else {
