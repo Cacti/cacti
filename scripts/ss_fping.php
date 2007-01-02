b11bf9946e3436b61760ef289a6aee89bb22e7b7
Larry Adams <thewitness@cacti.net>
2007-01-02 17:00:24 +0000

    additional exploit targets
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3850 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/ss_fping.php
+++ scripts/ss_fping.php
@@ -1,7 +1,11 @@
 <?php
 #!/usr/bin/php -q
 
-//STANDARD SCRIPT SERVER HEADER!!!
+/* do NOT run this script through a web browser */
+if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
+   die("<br><strong>This script is only meant to run at the command line.</strong>");
+}
+
 $no_http_headers = true;
 
 /* display No errors */
--- scripts/ss_host_cpu.php
+++ scripts/ss_host_cpu.php
@@ -1,4 +1,10 @@
 <?php
+
+/* do NOT run this script through a web browser */
+if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
+   die("<br><strong>This script is only meant to run at the command line.</strong>");
+}
+
 $no_http_headers = true;
 
 /* display No errors */
