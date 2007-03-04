8ff975973af94c7cc5dd5bb7cb9cd5ff0c34b83b
Tony Roman <rony@cacti.net>
2007-03-04 20:11:14 +0000

    Proper date range needed in copyright
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3896 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/0_8_6h_to_0_8_6i.php
+++ install/0_8_6h_to_0_8_6i.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2007 The Cacti Group                                      |
+ | Copyright (C) 2004-2007 The Cacti Groupi                                |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
