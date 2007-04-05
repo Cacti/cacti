1b5a3634809ce0dbff457482cc23dc6f00e1bb5c
Larry Adams <thewitness@cacti.net>
2007-04-05 11:12:35 +0000

    small formatting error.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/branches/BRANCH_0_8_6/cacti@3909 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cmd.php
+++ cmd.php
@@ -24,7 +24,7 @@
 
 /* do NOT run this script through a web browser */
 if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
-   die("<br><strong>This script is only meant to run at the command line.</strong>");
+	die("<br><strong>This script is only meant to run at the command line.</strong>");
 }
 
 $start = date("Y-n-d H:i:s"); // for runtime measurement
