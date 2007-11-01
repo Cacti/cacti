362f1200f74c1b1e007ac42d9c20567ac6826490
Larry Adams <thewitness@cacti.net>
2007-11-01 00:35:23 +0000

    -bug#0001043: Graph Templates drop down populates with duplicates -bug#0001058: Graph Filter Dropdowns to Not Respect Security Settings for User
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4288 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/html/inc_graph_filter_table.php
+++ include/html/inc_graph_filter_table.php
@@ -11,7 +11,17 @@
 							<option value="-1"<?php if ($_REQUEST["host_id"] == "-1") {?> selected<?php }?>>Any</option>
 							<option value="0"<?php if ($_REQUEST["host_id"] == "0") {?> selected<?php }?>>None</option>
 							<?php
-							$hosts = db_fetch_assoc("select id,CONCAT_WS('',description,' (',hostname,')') as name from host order by description,hostname");
+							/* get policy information for the sql where clause */
+							$sql_where = get_graph_permissions_sql($current_user["policy_graphs"], $current_user["policy_hosts"], $current_user["policy_graph_templates"]);
+
+							$hosts = db_fetch_assoc("SELECT DISTINCT host.id, CONCAT_WS('',host.description,' (',host.hostname,')') as name
+								FROM (graph_templates_graph,graph_local)
+								LEFT JOIN host ON (host.id=graph_local.host_id)
+								LEFT JOIN graph_templates ON (graph_templates.id=graph_local.graph_template_id)
+								LEFT JOIN user_auth_perms ON ((graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (host.id=user_auth_perms.item_id and user_auth_perms.type=3 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . "))
+								WHERE graph_templates_graph.local_graph_id=graph_local.id
+								" . (empty($sql_where) ? "" : "and $sql_where") . "
+								ORDER BY name");
 
 							if (sizeof($hosts) > 0) {
 							foreach ($hosts as $host) {
@@ -21,7 +31,7 @@
 							?>
 						</select>
 					</td>
-					<td width="50">
+					<td width="70">
 						&nbsp;Template:&nbsp;
 					</td>
 					<td width="1">
@@ -30,11 +40,13 @@
 							<option value="0"<?php if ($_REQUEST["template_id"] == "0") {?> selected<?php }?>>None</option>
 							<?php
 							$templates = db_fetch_assoc("SELECT DISTINCT graph_templates.id, graph_templates.name
-								FROM graph_templates
-								INNER JOIN graph_templates_graph
-								ON graph_templates.id=graph_templates_graph.graph_template_id
-								WHERE graph_templates_graph.local_graph_id>0
-								ORDER BY graph_templates.name");
+								FROM (graph_templates_graph,graph_local)
+								LEFT JOIN host ON (host.id=graph_local.host_id)
+								LEFT JOIN graph_templates ON (graph_templates.id=graph_local.graph_template_id)
+								LEFT JOIN user_auth_perms ON ((graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (host.id=user_auth_perms.item_id and user_auth_perms.type=3 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . "))
+								WHERE graph_templates_graph.local_graph_id=graph_local.id
+								" . (empty($sql_where) ? "" : "and $sql_where") . "
+								ORDER BY name");
 
 							if (sizeof($templates) > 0) {
 							foreach ($templates as $template) {
