25b0eaebe5c00af10249ecf3a3851c20757b719a
Larry Adams <thewitness@cacti.net>
2007-11-11 06:08:25 +0000

    -bug#0001078: Undefined variable: rra in graph.php on line 241
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4327 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- graph.php
+++ graph.php
@@ -238,7 +238,7 @@ case 'properties':
 					</td>
 					<td valign='top' style='padding: 3px;'>
 						<a href='graph.php?action=zoom&local_graph_id=<?php print $_GET["local_graph_id"];?>&rra_id=<?php print $_GET["rra_id"];?>&view_type=<?php print $_REQUEST["view_type"];?>'><img src='images/graph_zoom.gif' border='0' alt='Zoom Graph' title='Zoom Graph' style='padding: 3px;'></a><br>
-						<a href='graph_xport.php?local_graph_id=<?php print $_GET["local_graph_id"];?>&rra_id=<?php print $rra["id"];?>&view_type=<?php print $_REQUEST["view_type"];?>'><img src='images/graph_query.png' border='0' alt='CSV Export' title='CSV Export' style='padding: 3px;'></a><br>
+						<a href='graph_xport.php?local_graph_id=<?php print $_GET["local_graph_id"];?>&rra_id=<?php print $_GET["rra_id"];?>&view_type=<?php print $_REQUEST["view_type"];?>'><img src='images/graph_query.png' border='0' alt='CSV Export' title='CSV Export' style='padding: 3px;'></a><br>
 					</td>
 				</tr>
 				<tr>
