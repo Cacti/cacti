97b1d8f3118961d5403edc463de1d2eaf56d70ba
Larry Adams <thewitness@cacti.net>
2008-01-29 00:52:13 +0000

    Prepping Database Upgrade.  Now to perform table audit.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4435 860744bd-22fc-0310-8c96-e9fe5004b5ca

---
+++ install/0_8_7a_to_0_8_7b.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2008 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+function upgrade_to_0_8_7b() {
+	/* add Task Item Id Index */
+	db_install_execute("0.8.7b", "ALTER TABLE `graph_templates_item` ADD INDEX ( `task_item_id` )");
+}
+?>
