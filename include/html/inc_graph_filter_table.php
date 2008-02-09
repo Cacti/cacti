bfa7a3df2bba3245aa3580e909c7d90ff25afb76
Reinhard Scheck <gandalf@cacti.net>
2008-02-09 21:15:07 +0000

    Again: bug#0001114: Graphs with no template and/or no host cause filtering errors on Graph Mgt. page Did someone revert my commit#4456?
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4490 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- include/html/inc_graph_filter_table.php
+++ include/html/inc_graph_filter_table.php
@@ -17,8 +17,8 @@
 								$sql_where = get_graph_permissions_sql($current_user["policy_graphs"], $current_user["policy_hosts"], $current_user["policy_graph_templates"]);
 
 								$hosts = db_fetch_assoc("SELECT DISTINCT host.id, CONCAT_WS('',host.description,' (',host.hostname,')') as name
-									FROM (graph_templates_graph,graph_local)
-									LEFT JOIN host ON (host.id=graph_local.host_id)
+									FROM (graph_templates_graph,host)
+									LEFT JOIN graph_local ON (graph_local.host_id=host.id)
 									LEFT JOIN graph_templates ON (graph_templates.id=graph_local.graph_template_id)
 									LEFT JOIN user_auth_perms ON ((graph_templates_graph.local_graph_id=user_auth_perms.item_id and user_auth_perms.type=1 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (host.id=user_auth_perms.item_id and user_auth_perms.type=3 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . ") OR (graph_templates.id=user_auth_perms.item_id and user_auth_perms.type=4 and user_auth_perms.user_id=" . $_SESSION["sess_user_id"] . "))
 									WHERE graph_templates_graph.local_graph_id=graph_local.id
