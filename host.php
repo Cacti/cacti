158741ca35a40c9eda31cc5d44f78dd9ff8e6899
Larry Adams <thewitness@cacti.net>
2008-03-02 13:46:44 +0000

    -bug#0001100: spine hangs on unreachable hosts
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4542 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- host.php
+++ host.php
@@ -147,7 +147,7 @@ function form_save() {
 			raise_message(4);
 		}else{
 			$host_id = api_device_save($_POST["id"], $_POST["host_template_id"], $_POST["description"],
-				$_POST["hostname"], $_POST["snmp_community"], $_POST["snmp_version"],
+				trim($_POST["hostname"]), $_POST["snmp_community"], $_POST["snmp_version"],
 				$_POST["snmp_username"], $_POST["snmp_password"],
 				$_POST["snmp_port"], $_POST["snmp_timeout"],
 				(isset($_POST["disabled"]) ? $_POST["disabled"] : ""),
