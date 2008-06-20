ae27cea88584c1eb53566ced922eafa283ef0f09
Larry Adams <thewitness@cacti.net>
2008-06-20 19:39:47 +0000

    -QA: Having to revert a few "concepts".  Should be working now.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4610 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cli/add_graphs.php
+++ cli/add_graphs.php
@@ -195,7 +195,7 @@ if (sizeof($parms)) {
 
 	/* Some sanity checking... */
 	if ($dsGraph["snmpQueryId"] != "") {
-		if ($snmpQueries[$dsGraph["snmpQueryId"]] != "") {
+		if (!isset($snmpQueries[$dsGraph["snmpQueryId"]])) {
 			echo "ERROR: Unknown snmp-query-id (" . $dsGraph["snmpQueryId"] . ")\n";
 			echo "Try --list-snmp-queries\n";
 			exit(1);
@@ -210,7 +210,7 @@ if (sizeof($parms)) {
 		}
 
 		if ($dsGraph["snmpQueryType"] != "") {
-			if ($snmp_query_types[$dsGraph["snmpQueryType"]] == "") {
+			if (!isset($snmp_query_types[$dsGraph["snmpQueryType"]])) {
 				echo "ERROR: Unknown snmp-query-type-id (" . $dsGraph["snmpQueryType"] . ")\n";
 				echo "Try --snmp-query-id=" . $dsGraph["snmpQueryId"] . " --list-query-types\n";
 				exit(1);
@@ -237,7 +237,7 @@ if (sizeof($parms)) {
 
 	/* More sanity checking */
 	if ($dsGraph["snmpField"] != "") {
-		if ($snmpFields[$dsGraph["snmpField"]] == "") {
+		if (!isset($snmpFields[$dsGraph["snmpField"]])) {
 			echo "ERROR: Unknown snmp-field " . $dsGraph["snmpField"] . " for host $hostId\n";
 			echo "Try --list-snmp-fields\n";
 			exit(1);
@@ -246,7 +246,7 @@ if (sizeof($parms)) {
 		$snmpValues = getSNMPValues($hostId, $dsGraph["snmpField"], $dsGraph["snmpQueryId"]);
 
 		if ($dsGraph["snmpValue"] != "") {
-			if($snmpValues[$dsGraph["snmpValue"]] == "") {
+			if(!isset($snmpValues[$dsGraph["snmpValue"]])) {
 				echo "ERROR: Unknown snmp-value for field " . $dsGraph["snmpField"] . " - " . $dsGraph["snmpValue"] . "\n";
 				echo "Try --snmp-field=" . $dsGraph["snmpField"] . " --list-snmp-values\n";
 				exit(1);
