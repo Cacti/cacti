9b8da42a99d855f1b1dbcf5e3ff1084945738e71
Reinhard Scheck <gandalf@cacti.net>
2008-08-24 14:38:33 +0000

    -feature#0001177: CLI Reindexing - poller_reindex_hosts.php: allow filter on "host description"
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@4659 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cli/poller_reindex_hosts.php
+++ cli/poller_reindex_hosts.php
@@ -47,31 +47,34 @@ if (sizeof($parms) == 0) {
 	exit;
 }
 
-$debug    = FALSE;
-$query_id = "";
+$debug		= FALSE;
+$host_id	= "";
+$query_id	= "All";		/* just to mimic the old behaviour */
+$host_descr	= "";
 
 foreach($parms as $parameter) {
 	@list($arg, $value) = @explode("=", $parameter);
 
 	switch ($arg) {
 	case "-id":
+	case "--id":
 		$host_id = $value;
 		break;
 	case "-qid":
+	case "--qid":
 		$query_id = $value;
 		break;
+	case "-host-descr":
+	case "--host-descr":
+		$host_descr = $value;
+		break;
 	case "-d":
+	case "--debug":
 		$debug = TRUE;
 		break;
 	case "-h":
-		display_help();
-		exit;
 	case "-v":
-		display_help();
-		exit;
 	case "--version":
-		display_help();
-		exit;
 	case "--help":
 		display_help();
 		exit;
@@ -97,37 +100,47 @@ if (strtolower($host_id) == "all") {
 if (strtolower($query_id) == "all") {
 	/* do nothing */
 }else if (is_numeric($query_id)) {
-	$sql_where .= (strlen($sql_where) ? " AND snmp_query_id=$query_id": "WHERE snmp_query_id=$query_id");
+	$sql_where .= (strlen($sql_where) ? " AND snmp_query_id=$query_id": " WHERE snmp_query_id=$query_id");
 }else{
 	print "ERROR: You must specify either a query_id or 'all' to proceed.\n";
 	display_help();
 	exit;
 }
 
-$data_queries = db_fetch_assoc("SELECT host_id, snmp_query_id FROM host_snmp_query" . $sql_where);
+/* allow for additional filtering on host description */
+if (strlen($host_descr)) {
+	$sql_where .= (strlen($sql_where) ? " AND host.description like '%%" . $host_descr . "%%' AND host.id=host_snmp_query.host_id" : " WHERE host.description like '%%" . $host_descr . "%%' AND host.id=host_snmp_query.host_id");
+	$data_queries = db_fetch_assoc("SELECT host_id, snmp_query_id FROM host_snmp_query,host" . $sql_where);
+} else {
+	$data_queries = db_fetch_assoc("SELECT host_id, snmp_query_id FROM host_snmp_query" . $sql_where);
+}
 
 /* issue warnings and start message if applicable */
 print "WARNING: Do not interrupt this script.  Reindexing can take quite some time\n";
 debug("There are '" . sizeof($data_queries) . "' data queries to run");
 
 $i = 1;
-foreach ($data_queries as $data_query) {
-	if (!$debug) print ".";
-	debug("Data query number '" . $i . "' starting");
-	run_data_query($data_query["host_id"], $data_query["snmp_query_id"]);
-	debug("Data query number '" . $i . "' ending");
-	$i++;
+if (sizeof($data_queries)) {
+	foreach ($data_queries as $data_query) {
+		if (!$debug) print ".";
+		debug("Data query number '" . $i . "' host: '".$data_query["host_id"]."' SNMP Query Id: '".$data_query["snmp_query_id"]."' starting");
+		run_data_query($data_query["host_id"], $data_query["snmp_query_id"]);
+		debug("Data query number '" . $i . "' host: '".$data_query["host_id"]."' SNMP Query Id: '".$data_query["snmp_query_id"]."' ending");
+		$i++;
+	}
 }
 
 /*	display_help - displays the usage of the function */
 function display_help () {
-print "Cacti Reindex Host Script 1.1, Copyright 2007-2008 - The Cacti Group\n\n";
-	print "usage: poller_reindex_hosts.php -id=[host_id|All] [--qid=[ID]] [-d] [-h] [--help] [-v] [--version]\n\n";
-	print "-id=host_id   - The host_id to have data queries reindexed or 'All' to reindex all hosts\n";
-	print "-qid=query_id - Only index on a specific data query id\n";
-	print "-d            - Display verbose output during execution\n";
-	print "-v --version  - Display this help message\n";
-	print "-h --help     - Display this help message\n";
+	print "Cacti Reindex Host Script 1.2, Copyright 2007-2008 - The Cacti Group\n\n";
+	print "usage: poller_reindex_hosts.php --id=[host_id|All] [--qid=[ID|All]] [--host-descr=[description]]\n";
+	print "                           [-d] [-h] [--help] [-v] [--version]\n\n";
+	print "--id=host_id             - The host_id to have data queries reindexed or 'All' to reindex all hosts\n";
+	print "--qid=query_id           - Only index on a specific data query id; defaults to 'All'\n";
+	print "--host-descr=description - The host description to filter by (SQL filters acknowledged)\n";
+	print "--debug                  - Display verbose output during execution\n";
+	print "-v --version             - Display this help message\n";
+	print "-h --help                - Display this help message\n";
 }
 
 function debug($message) {
