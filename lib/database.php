3975536fe19f3a1b60ecfd89a9a8373a97bb495a
Larry Adams <thewitness@cacti.net>
2009-09-25 01:11:28 +0000

    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@5305 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/database.php
+++ lib/database.php
@@ -261,13 +261,13 @@ function array_to_sql_or($array, $sql_column) {
 	}
 
 	if (count($array) > 0) {
-		$sql_or = "(";
+		$sql_or = "($sql_column IN(";
 
 		for ($i=0;($i<count($array));$i++) {
-			$sql_or .= $sql_column . "='" . $array[$i] . "'";
-
-			if (($i+1) < count($array)) {
-				$sql_or .= " OR ";
+			if (is_int($array[$i])) {
+				$sql_or .= ($i == 0) ? "":"," . $array[$i];
+			}else{
+				$sql_or .= ($i == 0) ? "'":",'" . $array[$i] . "'";
 			}
 		}
 
