810e86cc159deddf470d4ee6fcd5b07e01b7a910
Reinhard Scheck <gandalf@cacti.net>
2010-04-19 19:47:18 +0000

    backport of latest changes to array_to_sql_or
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@5771 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/database.php
+++ lib/database.php
@@ -264,7 +264,11 @@ function array_to_sql_or($array, $sql_column) {
 		$sql_or = "($sql_column IN(";
 
 		for ($i=0;($i<count($array));$i++) {
-			$sql_or .= (($i == 0) ? "'":",'") . $array[$i] . "'";
+			if (is_array($array[$i]) && array_key_exists($sql_column, $array[$i])) {
+				$sql_or .= (($i == 0) ? "'":",'") . $array[$i][$sql_column] . "'";
+			} else {
+				$sql_or .= (($i == 0) ? "'":",'") . $array[$i] . "'";
+			}
 		}
 
 		$sql_or .= "))";
