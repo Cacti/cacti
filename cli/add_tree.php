63e048f43cc10f742fcab60bbf92661446efe8a1
Larry Adams <thewitness@cacti.net>
2010-05-07 14:26:30 +0000

    -bug#0001725: Error while trying to add a tree node via cli
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@5875 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cli/add_tree.php
+++ cli/add_tree.php
@@ -33,6 +33,7 @@ $no_http_headers = true;
 include(dirname(__FILE__)."/../include/global.php");
 include_once($config["base_path"]."/lib/api_automation_tools.php");
 include_once($config["base_path"].'/lib/tree.php');
+include_once($config["base_path"].'/lib/api_tree.php');
 
 /* process calling arguments */
 $parms = $_SERVER["argv"];
