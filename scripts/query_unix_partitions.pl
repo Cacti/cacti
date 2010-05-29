557feade0d4eb820c2e676d28249e01a58a0495e
Reinhard Scheck <gandalf@cacti.net>
2010-05-29 11:18:24 +0000

    -bug#0001771: "index count changed" not implemented for query_unix_partitions.pl
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@5960 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/query_unix_partitions.pl
+++ scripts/query_unix_partitions.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl
 
-if (($ARGV[0] ne "query") && ($ARGV[0] ne "get") && ($ARGV[0] ne "index")) {
-	print "usage:\n\n./query_unix_partitions.pl index\n./query_unix_partitions.pl query {device,mount,total,used,available,percent}\n./query_unix_partitions.pl get {device,mount,total,used,available,percent} DEVICE\n";
+if (($ARGV[0] ne "query") && ($ARGV[0] ne "get") && ($ARGV[0] ne "index") && ($ARGV[0] ne "num_indexes")) {
+	print "usage:\n\n";
+	print "./query_unix_partitions.pl index\n";
+	print "./query_unix_partitions.pl num_indexes\n";
+	print "./query_unix_partitions.pl query {device,mount,total,used,available,percent}\n";
+	print "./query_unix_partitions.pl get {device,mount,total,used,available,percent} DEVICE\n";
 	exit;
 }
 
 open(DF, "/bin/df -P -k|");
 
+$count=0;
 while (<DF>) {
 	#/dev/hda2             20157744  18553884    579860  97% /var
 	if (/^(\/\S+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+(\/\S*)$/) {
@@ -21,6 +26,8 @@ while (<DF>) {
 
 		if ($ARGV[0] eq "index") {
 			print "$1\n";
+		}elsif ($ARGV[0] eq "num_indexes") {
+			$count++;
 		}elsif (($ARGV[0] eq "get") && ($ARGV[2] eq $1)) {
 			print $output{$ARGV[1]};
 		}elsif ($ARGV[0] eq "query") {
@@ -30,3 +37,7 @@ while (<DF>) {
 }
 
 close(DF);
+$count++;
+if ($ARGV[0] eq "num_indexes") {
+	print "$count\n";
+}
