2f57a5fa33d9c143134fc59870bef658de86239f
Reinhard Scheck <gandalf@cacti.net>
2011-01-03 21:19:36 +0000

    small helper script to re-associate rra's to data-templates and related data sources The need arises, because template import (without RRA override) always associates ALL existing RRAs to a new data template. This is usually not what you will want.
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@6196 860744bd-22fc-0310-8c96-e9fe5004b5ca

---
+++ cli/data_template_associate_rra.php
@@ -0,0 +1,110 @@
+#!/usr/bin/php -q
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2011 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+/* do NOT run this script through a web browser */
+if (!isset ($_SERVER["argv"][0]) || isset ($_SERVER['REQUEST_METHOD']) || isset ($_SERVER['REMOTE_ADDR'])) {
+	die("<br><strong>This script is only meant to run at the command line.</strong>");
+}
+
+/* We are not talking to the browser */
+$no_http_headers = true;
+
+include (dirname(__FILE__) . "/../include/global.php");
+
+/* process calling arguments */
+$parms = $_SERVER["argv"];
+$me    = array_shift($parms);
+$debug = FALSE;	# no debug mode
+array_shift($parms);
+$template_id = 0;
+
+if (sizeof($parms)) {
+	foreach ($parms as $parameter) {
+		@ list ($arg, $value) = @ explode("=", $parameter);
+
+		switch ($arg) {
+			case "-d":
+			case "--debug":				$debug 						= TRUE; 		break;
+			case "--rra":				$rra						= trim($value);	break;
+			case "--data-template-id" :	$data_template_id 			= trim($value);	break;
+			case "-V":
+			case "-H":
+			case "--help":
+			case "--version":		display_help($me);								exit(0);
+			case "--quiet":			$quietMode = TRUE;								break;
+			default:				echo __("ERROR: Invalid Argument: (%s)", $arg) . "\n\n"; display_help($me); exit(1);
+		}
+	}
+
+	$data_template = db_fetch_row("SELECT * from data_template WHERE id=$data_template_id");
+	$data_rra = explode(':', $rra);
+	associate($data_template["id"], $data_rra);
+
+} else {
+	display_help();
+	exit (0);
+}
+
+
+function associate($data_template_id, $data_rra) {
+
+	/* get a list of data sources using this template 
+	 * including the template itself */
+	$data_sources = db_fetch_assoc("SELECT
+			data_template_data.id
+			FROM data_template_data
+			WHERE data_template_id=$data_template_id");
+
+	if (sizeof($data_sources) > 0) {
+		foreach ($data_sources as $data_source) {
+
+			/* make sure to update the 'data_template_data_rra' table for each data source */
+			db_execute("DELETE
+						FROM data_template_data_rra
+						WHERE data_template_data_id=" . $data_source["id"]);
+
+			reset($data_rra);
+
+			if (sizeof($data_rra) > 0) {
+				foreach ($data_rra as $rra) {
+					db_execute("INSERT INTO data_template_data_rra
+									(data_template_data_id,rra_id)
+									VALUES (" . $data_source["id"] . "," . $rra["rra_id"] . ")");
+				}
+			}
+		}
+	}
+	return;
+}
+
+function display_help() {
+	echo "Data Template Associate RRA Script 1.0, Copyright 2004-2010 - The Cacti Group\n\n";
+	echo "A simple command line utility to associate RRA definitions to a data template in Cacti\n\n";
+	echo "usage: $me --rra='rra-1:..:rra-n' --data-template-id=[ID]\n";
+	echo "Required:\n";
+	echo "    --rra                  the rra ids that shall be associated, seperated by colon\n";
+	echo "    --data-template-id     the data template id\n";
+}
+?>
