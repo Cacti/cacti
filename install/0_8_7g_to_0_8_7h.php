4ed4fa00a07569a8dc8037f39d406c33a73c9f6d
Reinhard Scheck <gandalf@cacti.net>
2011-03-27 07:35:50 +0000

    use standard upgrade function calls to provide data for the web interface during update use logging for upgrading via cli script
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@6353 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/0_8_7g_to_0_8_7h.php
+++ install/0_8_7g_to_0_8_7h.php
@@ -23,37 +23,47 @@
 */
 
 function upgrade_to_0_8_7h() {
-	require_once("../lib/poller.php");
+	global $config;
+	require_once($config["base_path"] . "/lib/poller.php");
 
 	/* speed up the reindexing */
 	$_columns = array_rekey(db_fetch_assoc("SHOW COLUMNS FROM host_snmp_cache"), "Field", "Field");
 	if (!in_array("present", $_columns)) {
-		db_execute("ALTER TABLE host_snmp_cache ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `oid`");
-		db_execute("ALTER TABLE host_snmp_cache ADD INDEX present (present)");
+		db_install_execute("0.8.7h", "ALTER TABLE host_snmp_cache ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `oid`");
+		db_install_execute("0.8.7h", "ALTER TABLE host_snmp_cache ADD INDEX present (present)");
+		cacti_log(__FUNCTION__ . " upgrade table host_snmp_cache", false, "UPGRADE");
 	}
 	$_columns = array_rekey(db_fetch_assoc("SHOW COLUMNS FROM poller_item"), "Field", "Field");
 	if (!in_array("present", $_columns)) {
-		db_execute("ALTER TABLE poller_item ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `action`");
-		db_execute("ALTER TABLE poller_item ADD INDEX present (present)");
+		db_install_execute("0.8.7h", "ALTER TABLE poller_item ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `action`");
+		db_install_execute("0.8.7h", "ALTER TABLE poller_item ADD INDEX present (present)");
+		cacti_log(__FUNCTION__ . " upgrade table poller_item", false, "UPGRADE");
 	}
 	$_columns = array_rekey(db_fetch_assoc("SHOW COLUMNS FROM poller_reindex"), "Field", "Field");
 	if (!in_array("present", $_columns)) {
-		db_execute("ALTER TABLE poller_reindex ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `action`");
-		db_execute("ALTER TABLE poller_reindex ADD INDEX present (present)");
+		db_install_execute("0.8.7h", "ALTER TABLE poller_reindex ADD COLUMN present tinyint NOT NULL DEFAULT '1' AFTER `action`");
+		db_install_execute("0.8.7h", "ALTER TABLE poller_reindex ADD INDEX present (present)");
+		cacti_log(__FUNCTION__ . " upgrade table poller_reindex", false, "UPGRADE");
 	}
 
 	$_columns = array_rekey(db_fetch_assoc("SHOW COLUMNS FROM host"), "Field", "Field");
 	if (!in_array("device_threads", $_columns)) {
-		db_execute("ALTER TABLE host ADD COLUMN device_threads tinyint(2) unsigned NOT NULL DEFAULT '1' AFTER max_oids;");
+		db_install_execute("0.8.7h", "ALTER TABLE host ADD COLUMN device_threads tinyint(2) unsigned NOT NULL DEFAULT '1' AFTER max_oids;");
+		cacti_log(__FUNCTION__ . " upgrade table host", false, "UPGRADE");
 	}
 
-	db_execute("ALTER TABLE `data_template_rrd` ADD UNIQUE INDEX `duplicate_dsname_contraint`(`local_data_id`, `data_source_name`, `data_template_id`)");
+	$_keys = array_rekey(db_fetch_assoc("SHOW KEYS FROM data_template_rrd"), "Key_name", "Key_name");
+	if (!in_array("duplicate_dsname_contraint", $_keys)) {
+		db_install_execute("0.8.7h", "ALTER TABLE `data_template_rrd` ADD UNIQUE INDEX `duplicate_dsname_contraint`(`local_data_id`, `data_source_name`, `data_template_id`)");
+		cacti_log(__FUNCTION__ . " upgrade table data_template_rrd", false, "UPGRADE");
+	}
 
 	/* update the reindex cache, as we now introduced more options for "index count changed" */
 	$host_snmp_query = db_fetch_assoc("select host_id,snmp_query_id from host_snmp_query");
 	if (sizeof($host_snmp_query) > 0) {
 		foreach ($host_snmp_query as $item) {
 			update_reindex_cache($item["host_id"], $item["snmp_query_id"]);
+			cacti_log(__FUNCTION__ . " update_reindex_cache for Host[" . $item["host_id"] . "] DQ[" . $item["snmp_query_id"] . "]", false, "UPGRADE");
 		}
 	}
 
