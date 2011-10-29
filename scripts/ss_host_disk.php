ec11544a3229c4d04b308abd6d7bf28211a5470b
Larry Adams <thewitness@cacti.net>
2011-10-29 02:06:17 +0000

    -bug#0002084: Incorrect normalisation of hrStorageTable values over 2^31
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@6879 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- scripts/ss_host_disk.php
+++ scripts/ss_host_disk.php
@@ -65,12 +65,12 @@ function ss_host_disk($hostname, $host_id, $snmp_auth, $cmd, $arg1 = "", $arg2 =
 		for ($i=0;($i<sizeof($return_arr));$i++) {
 			print $return_arr[$i] . "\n";
 		}
-		
+
 	}elseif ($cmd == "num_indexes") {
 		$return_arr = ss_host_disk_reindex(cacti_snmp_walk($hostname, $snmp_community, $oids["index"], $snmp_version, $snmp_auth_username, $snmp_auth_password, $snmp_auth_protocol, $snmp_priv_passphrase, $snmp_priv_protocol, $snmp_context, $snmp_port, $snmp_timeout, $ping_retries, $max_oids, SNMP_POLLER));
 
 		print sizeof($return_arr);
-		
+
 	}elseif ($cmd == "query") {
 		$arg = $arg1;
 
@@ -87,10 +87,10 @@ function ss_host_disk($hostname, $host_id, $snmp_auth, $cmd, $arg1 = "", $arg2 =
 		if (($arg == "total") || ($arg == "used")) {
 			$sau = preg_replace("/[^0-9]/i", "", db_fetch_cell("select field_value from host_snmp_cache where host_id=$host_id and field_name='hrStorageAllocationUnits' and snmp_index='$index'"));
 			$snmp_data = cacti_snmp_get($hostname, $snmp_community, $oids[$arg] . ".$index", $snmp_version, $snmp_auth_username, $snmp_auth_password, $snmp_auth_protocol,$snmp_priv_passphrase,$snmp_priv_protocol, $snmp_context, $snmp_port, $snmp_timeout, $ping_retries, SNMP_POLLER);
-			if($snmp_data<0){
-					return (abs($snmp_data) + 2147483647) * $sau;
+			if ($snmp_data < 0) {
+				return ($snmp_data + 4294967296) * $sau;
 			} else {
-					return $snmp_data * $sau;
+				return $snmp_data * $sau;
 			}
 		}else{
 			return cacti_snmp_get($hostname, $snmp_community, $oids[$arg] . ".$index", $snmp_version, $snmp_auth_username, $snmp_auth_password, $snmp_auth_protocol,$snmp_priv_passphrase,$snmp_priv_protocol, $snmp_context, $snmp_port, $snmp_timeout, $ping_retries, SNMP_POLLER);
