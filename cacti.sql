94a9e40451a2fb4731aa2dd7c5fa059c0802bbe0
Tony Roman <rony@cacti.net>
2012-02-27 01:21:21 +0000

    Update database creation script to support pia
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@7007 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cacti.sql
+++ cacti.sql
@@ -2036,6 +2036,71 @@ INSERT INTO host_template_snmp_query VALUES (7,8);
 INSERT INTO host_template_snmp_query VALUES (7,9);
 INSERT INTO host_template_snmp_query VALUES (8,6);
 
+-- 
+-- Table structure for table `plugin_config`
+-- 
+
+CREATE TABLE `plugin_config` (
+  `id` int(8) NOT NULL auto_increment,
+  `directory` varchar(32) NOT NULL default '',
+  `name` varchar(64) NOT NULL default '',
+  `status` tinyint(2) NOT NULL default '0',
+  `author` varchar(64) NOT NULL default '',
+  `webpage` varchar(255) NOT NULL default '',
+  `version` varchar(8) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `status` (`status`),
+  KEY `directory` (`directory`)
+) ENGINE=MyISAM;
+
+-- 
+-- Table structure for table `plugin_hooks`
+-- 
+
+CREATE TABLE `plugin_hooks` (
+  `id` int(8) NOT NULL auto_increment,
+  `name` varchar(32) NOT NULL default '',
+  `hook` varchar(64) NOT NULL default '',
+  `file` varchar(255) NOT NULL default '',
+  `function` varchar(128) NOT NULL default '',
+  `status` int(8) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `hook` (`hook`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM;
+
+-- 
+-- Table structure for table `plugin_realms`
+-- 
+
+CREATE TABLE `plugin_realms` (
+  `id` int(8) NOT NULL auto_increment,
+  `plugin` varchar(32) NOT NULL default '',
+  `file` text NOT NULL,
+  `display` varchar(64) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `plugin` (`plugin`)
+) ENGINE=MyISAM;
+
+-- 
+-- Table structure for table `plugin_db_changes`
+-- 
+
+CREATE TABLE `plugin_db_changes` (
+  `id` int(10) NOT NULL auto_increment,
+  `plugin` varchar(16) NOT NULL default '',
+  `table` varchar(64) NOT NULL default '',
+  `column` varchar(64) NOT NULL,
+  `method` varchar(16) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `plugin` (`plugin`),
+  KEY `method` (`method`)
+) ENGINE=MyISAM;
+
+REPLACE INTO `plugin_realms` VALUES (1, 'internal', 'plugins.php', 'Plugin Management');
+INSERT INTO `plugin_hooks` VALUES (1, 'internal', 'config_arrays', '', 'plugin_config_arrays', 1);
+INSERT INTO `plugin_hooks` VALUES (2, 'internal', 'draw_navigation_text', '', 'plugin_draw_navigation_text', 1);
+
 --
 -- Table structure for table `poller`
 --
@@ -2049,11 +2114,6 @@ CREATE TABLE poller (
 ) ENGINE=MyISAM;
 
 --
--- Dumping data for table `poller`
---
-
-
---
 -- Table structure for table `poller_command`
 --
 
@@ -2064,11 +2124,6 @@ CREATE TABLE poller_command (
   command varchar(200) NOT NULL default '',
   PRIMARY KEY  (poller_id,action,command)
 ) ENGINE=MyISAM;
-
---
--- Dumping data for table `poller_command`
---
-
 
 --
 -- Table structure for table `poller_item`
@@ -2108,11 +2163,6 @@ CREATE TABLE poller_item (
 ) ENGINE=MyISAM;
 
 --
--- Dumping data for table `poller_item`
---
-
-
---
 -- Table structure for table `poller_output`
 --
 
@@ -2123,11 +2173,6 @@ CREATE TABLE poller_output (
   output text NOT NULL,
   PRIMARY KEY  (local_data_id,rrd_name,time)
 ) ENGINE=MyISAM;
-
---
--- Dumping data for table `poller_output`
---
-
 
 --
 -- Table structure for table `poller_reindex`
@@ -2146,11 +2191,6 @@ CREATE TABLE poller_reindex (
 ) ENGINE=MyISAM;
 
 --
--- Dumping data for table `poller_reindex`
---
-
-
---
 -- Table structure for table `poller_time`
 --
 
@@ -2162,11 +2202,6 @@ CREATE TABLE poller_time (
   end_time datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (id)
 ) ENGINE=MyISAM;
-
---
--- Dumping data for table `poller_time`
---
-
 
 --
 -- Table structure for table `rra`
