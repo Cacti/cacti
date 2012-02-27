a257958a0b53ff36544665ef4ba559b59d207c32
Tony Roman <rony@cacti.net>
2012-02-27 07:04:37 +0000

    Update copyright to 2012
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@7016 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- cli/add_tree.php
+++ cli/add_tree.php
@@ -2,7 +2,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2004-2011 The Cacti Group                                 |
+ | Copyright (C) 2004-2012 The Cacti Group                                 |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
@@ -327,7 +327,7 @@ if (sizeof($parms)) {
 }
 
 function display_help() {
-	echo "Add Tree Script 1.0, Copyright 2004-2011 - The Cacti Group\n\n";
+	echo "Add Tree Script 1.0, Copyright 2004-2012 - The Cacti Group\n\n";
 	echo "A simple command line utility to add objects to a tree in Cacti\n\n";
 	echo "usage: add_tree.php  --type=[tree|node] [type-options] [--quiet]\n\n";
 	echo "Tree options:\n";
