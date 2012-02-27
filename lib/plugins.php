346de0d7d7e9038685a9c5e6a04ef1e1e2ac009d
Tony Roman <rony@cacti.net>
2012-02-27 01:45:42 +0000

    Fix minor issue with plugin lib when session is no available
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.7@7009 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/plugins.php
+++ lib/plugins.php
@@ -441,7 +441,7 @@ function api_plugin_user_realm_auth ($filename = '') {
 	global $user_realms, $user_auth_realms, $user_auth_realm_filenames;
 	/* list all realms that this user has access to */
 	if (!isset($user_realms)) {
-		if (read_config_option('global_auth') == 'on' || read_config_option('auth_method') != 0) {
+		if (isset($_SESSION["sess_user_id"]) && (read_config_option('global_auth') == 'on' || read_config_option('auth_method') != 0)) {
 			$user_realms = db_fetch_assoc("select realm_id from user_auth_realm where user_id=" . $_SESSION["sess_user_id"], false);
 			$user_realms = array_rekey($user_realms, "realm_id", "realm_id");
 		}else{
