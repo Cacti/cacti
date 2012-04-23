d2490576c43b3f0bd0091efdc20110aca4fcca15
Tony Roman <rony@cacti.net>
2012-04-23 01:36:43 +0000

    Update for version 0.8.8a
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.8@7124 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- install/index.php
+++ install/index.php
@@ -33,7 +33,7 @@ if (!verify_php_extensions()) {exit;}
 $cacti_versions = array("0.8", "0.8.1", "0.8.2", "0.8.2a", "0.8.3", "0.8.3a", "0.8.4", "0.8.5", "0.8.5a",
 	"0.8.6", "0.8.6a", "0.8.6b", "0.8.6c", "0.8.6d", "0.8.6e", "0.8.6f", "0.8.6g", "0.8.6h", "0.8.6i", "0.8.6j", "0.8.6k",
 	"0.8.7", "0.8.7a", "0.8.7b", "0.8.7c", "0.8.7d", "0.8.7e", "0.8.7f", "0.8.7g", "0.8.7h", "0.8.7i",
-	"0.8.8");
+	"0.8.8", "0.8.8a");
 
 $old_cacti_version = db_fetch_cell("select cacti from version");
 
@@ -445,6 +445,9 @@ if ($_REQUEST["step"] == "4") {
 		}elseif ($cacti_versions[$i] == "0.8.8") {
 			include ("0_8_7i_to_0_8_8.php");
 			upgrade_to_0_8_8();
+		}elseif ($cacti_versions[$i] == "0.8.8a") {
+			include ("0_8_8_to_0_8_8a.php");
+			upgrade_to_0_8_8a();
 		}
 	}
 }
