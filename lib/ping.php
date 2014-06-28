f06002e09255698a8fd958a34e9eda896b6ed30b
Jimmy Conner <cigamit@cacti.net>
2014-06-28 06:31:40 +0000

    -bug#0002438: Down Host Detection issue when using SNMP Desc or SNMP getNext
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti/cacti/branches/0.8.8@7456 860744bd-22fc-0310-8c96-e9fe5004b5ca

--- lib/ping.php
+++ lib/ping.php
@@ -571,7 +571,7 @@ class Net_Ping
 			$this->snmp_status = 0.000;
 		}else if (($avail_method == AVAIL_SNMP_AND_PING) && ($ping_result == false)) {
 			$snmp_result = false;
-		}else if (($avail_method == AVAIL_SNMP) || ($avail_method == AVAIL_SNMP_AND_PING)) {
+		}else if (($avail_method == AVAIL_SNMP) || ($avail_method == AVAIL_SNMP_AND_PING) || ($avail_method == AVAIL_SNMP_GET_SYSDESC) || ($avail_method == AVAIL_SNMP_GET_NEXT)) {
 			if (($this->host["snmp_community"] == "") && ($this->host["snmp_version"] != 3)) {
 				/* snmp version 1/2 without community string assume SNMP test to be successful
 				   due to backward compatibility issues */
@@ -610,6 +610,8 @@ class Net_Ping
 					return false;
 				}
 			case AVAIL_SNMP:
+			case AVAIL_SNMP_GET_NEXT:
+			case AVAIL_SNMP_GET_SYSDESC:
 				if ($snmp_result) {
 					return true;
 				}else{
