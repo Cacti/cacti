bad07946abf48be645c650dd55b1383bbe3bedd5
cigamit <cigamit@boxeed.in>
2015-01-22 23:01:22 -0600

    Bug 0002511: DSStats and Boost tables missing
    Add Boost MyISAM tables to cacti.sql
    Leave Memory table file for easy conversion for now

--- cacti.sql
+++ cacti.sql
@@ -2310,6 +2310,28 @@ CREATE TABLE poller_output (
 ) ENGINE=MyISAM;
 
 --
+-- Table structure for table `poller_output_boost`
+--
+
+CREATE TABLE  `poller_output_boost` (
+  `local_data_id` mediumint(8) unsigned NOT NULL default '0',
+  `rrd_name` varchar(19) NOT NULL default '',
+  `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `output` varchar(512) NOT NULL,
+  PRIMARY KEY USING BTREE (`local_data_id`,`rrd_name`,`time`)
+) ENGINE=MyISAM ROW_FORMAT=FIXED;
+
+--
+-- Table structure for table `poller_output_boost_processes`
+--
+
+CREATE TABLE  `poller_output_boost_processes` (
+  `sock_int_value` bigint(20) unsigned NOT NULL auto_increment,
+  `status` varchar(255) default NULL,
+  PRIMARY KEY  (`sock_int_value`)
+) ENGINE=MEMORY;
+
+--
 -- Table structure for table `poller_output_realtime`
 --
 
