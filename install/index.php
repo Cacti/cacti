e099608bcc414526d053b91afde740bc5ff48ebe
Tony Roman <troman@barracuda.com>
2015-07-03 10:24:14 -0400

    Updates for 0.8.8e release

--- install/index.php
+++ install/index.php
@@ -35,7 +35,7 @@ if (!verify_php_extensions()) {exit;}
 $cacti_versions = array('0.8', '0.8.1', '0.8.2', '0.8.2a', '0.8.3', '0.8.3a', '0.8.4', '0.8.5', '0.8.5a',
 	'0.8.6', '0.8.6a', '0.8.6b', '0.8.6c', '0.8.6d', '0.8.6e', '0.8.6f', '0.8.6g', '0.8.6h', '0.8.6i', '0.8.6j', '0.8.6k',
 	'0.8.7', '0.8.7a', '0.8.7b', '0.8.7c', '0.8.7d', '0.8.7e', '0.8.7f', '0.8.7g', '0.8.7h', '0.8.7i',
-	'0.8.8', '0.8.8a', '0.8.8b', '0.8.8c', '0.8.8d', '1.0.0');
+	'0.8.8', '0.8.8a', '0.8.8b', '0.8.8c', '0.8.8d', '0.8.8e', '1.0.0');
 
 $old_cacti_version = db_fetch_cell('SELECT cacti FROM version');
 
@@ -530,6 +530,9 @@ if ($step == '4') {
 		}elseif ($cacti_versions[$i] == '0.8.8d') {
 			include ('0_8_8c_to_0_8_8d.php');
 			upgrade_to_0_8_8d();
+		}elseif ($cacti_versions[$i] == '0.8.8e') {
+			include ('0_8_8d_to_0_8_8e.php');
+			upgrade_to_0_8_8e();
 		}elseif ($cacti_versions[$i] == '1.0.0') {
 			include ('0_8_8d_to_1_0_0.php');
 			upgrade_to_1_0_0();
