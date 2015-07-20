8b7025dd459fdbd99d1dc400a17dc7abb4d4a7ad
cigamit <jimmy@sqmail.org>
2015-07-20 08:11:39 -0500

    Correct SQL error in permissions
    Correcting SQL error in permissions due to bad syntax.

--- lib/auth.php
+++ lib/auth.php
@@ -964,7 +964,7 @@ function get_allowed_devices($sql_where = '', $order_by = 'description', $limit 
 	}
 
 	if ($host_id > 0) {
-		$sql_where .= (strlen($sql_where) ? ' AND ':' ') . " gl.host_id=$host_id";
+		$sql_where .= (strlen($sql_where) ? ' AND ':'WHERE ') . " gl.host_id=$host_id";
 	}
 
 	$i          = 0;
@@ -1089,7 +1089,7 @@ function get_allowed_graph_templates($sql_where = '', $order_by = 'name', $limit
 	}
 
 	if ($graph_template_id > 0) {
-		$sql_where .= (strlen($sql_where) ? ' AND ':' ') . " gl.graph_template_id=$graph_template_id";
+		$sql_where .= (strlen($sql_where) ? ' AND ':'WHERE ') . " gl.graph_template_id=$graph_template_id";
 	}
 
 	$i          = 0;
