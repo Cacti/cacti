3773047fbf5d0542a9e14eddfeea20a50cf69d92
cigamit <jimmy@sqmail.org>
2015-08-06 19:59:17 -0500

    Move cacti_db_version up a bit in the stack
    Remove one undefined index log message

--- include/global.php
+++ include/global.php
@@ -210,6 +210,9 @@ include_once($config['library_path'] . '/database.php');
 /* connect to the database server */
 db_connect_real($database_hostname, $database_username, $database_password, $database_default, $database_type, $database_port, $database_ssl);
 
+/* gather the existing cactidb version */
+$config['cacti_db_version'] = db_fetch_cell("SELECT cacti FROM version LIMIT 1");
+
 /* include additional modules */
 include_once($config['library_path'] . '/functions.php');
 include_once($config['library_path'] . '/auth.php');
@@ -247,5 +250,4 @@ api_plugin_hook('config_insert');
 
 /* current cacti version */
 $config['cacti_version'] = '1.0.0';
-$config['cacti_db_version'] = db_fetch_cell("SELECT cacti FROM version LIMIT 1");
 
