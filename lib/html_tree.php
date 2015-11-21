1d53cabfa5111c894b86a29e4a24ff0fa3ba6563
cigamit <jimmy@sqmail.org>
2015-11-21 15:43:31 -0600

    show the correct number of columns in tree mode
    When columns are > 5 issues present themselves

--- lib/html_tree.php
+++ lib/html_tree.php
@@ -880,7 +880,7 @@ function grow_right_pane_tree($tree_id, $leaf_id, $host_group_data) {
 	$total_rows = sizeof($graph_list);
 
 	/* generate page list */
-	$nav = html_nav_bar("graph_view.php?action=tree_content&tree_id=$tree_id&leaf_id=$leaf_id&nodeid=" . get_request_var_request('nodeid') . '&host_group_data=' . get_request_var_request('host_group_data'), MAX_DISPLAY_PAGES, get_request_var_request('page'), get_request_var_request('graphs'), $total_rows, 5, 'Graphs', 'page', 'main');
+	$nav = html_nav_bar("graph_view.php?action=tree_content&tree_id=$tree_id&leaf_id=$leaf_id&nodeid=" . get_request_var_request('nodeid') . '&host_group_data=' . get_request_var_request('host_group_data'), MAX_DISPLAY_PAGES, get_request_var_request('page'), get_request_var_request('graphs'), $total_rows, get_request_var_request('columns'), 'Graphs', 'page', 'main');
 
 	print $nav;
 
