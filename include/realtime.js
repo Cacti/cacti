b979bf215bae0ad6aaf8f4b7407ddbc453caaafc
cigamit <jimmy@sqmail.org>
2016-03-19 12:23:47 -0500

    have realtime not use pace

--- include/realtime.js
+++ include/realtime.js
@@ -112,20 +112,18 @@ function realtimeGrapher() {
 
 			position = $('#wrapper_'+local_graph_id).find('img').position();
 
-			Pace.stop;
+			Pace.ignore(function() {
+				$.get(urlPath+'graph_realtime.php?action=countdown&top='+parseInt(position.top)+'&left='+parseInt(position.left)+(isThumb ? '&graph_nolegend=true':'')+'&graph_end=0&graph_start=-'+graph_start+'&local_graph_id='+local_graph_id+'&ds_step='+ds_step+'#count='+count, function(data) {
+					results = $.parseJSON(data);
 
-			$.get(urlPath+'graph_realtime.php?action=countdown&top='+parseInt(position.top)+'&left='+parseInt(position.left)+(isThumb ? '&graph_nolegend=true':'')+'&graph_end=0&graph_start=-'+graph_start+'&local_graph_id='+local_graph_id+'&ds_step='+ds_step+'#count='+count, function(data) {
-				Pace.start;
+					$('#graph_'+results.local_graph_id).prop('src', 'data:image/png;base64,'+results.data).change();
 
-				results = $.parseJSON(data);
-
-				$('#graph_'+results.local_graph_id).prop('src', 'data:image/png;base64,'+results.data).change();
-
-				if (isThumb) {
-					$('#graph_'+results.local_graph_id).width(rtWidth).height(rtHeight);
-				}else{
-					$('#graph_'+results.local_graph_id);
-				}
+					if (isThumb) {
+						$('#graph_'+results.local_graph_id).width(rtWidth).height(rtHeight);
+					}else{
+						$('#graph_'+results.local_graph_id);
+					}
+				});
 			});
 		}
 	}
