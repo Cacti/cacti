fcbbdf149b307084cff47deafb3f6780c4f65fd3
cigamit <jimmy@sqmail.org>
2016-03-19 15:41:30 -0500

    insure graph action pointers are good

--- include/layout.js
+++ include/layout.js
@@ -1056,7 +1056,7 @@ function initializeGraphs() {
 			setFilters();
 		}else{
 			keepRealtime[graph_id]  = $('#wrapper_'+graph_id).html();
-			$(this).html("<i style='text-align:center;padding:0px;' title='Click again to take this Graph out of Realtime' class='fa fa-circle-o-notch fa-spin'/>");
+			$(this).html("<i style='text-align:center;padding:0px;' title='Click again to take this Graph out of Realtime' class='drillDown fa fa-circle-o-notch fa-spin'/>");
 			$(this).find('i').tooltip();
 			realtimeArray[graph_id] = true;
 			setFilters();
