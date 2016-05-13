a6b9d168f1a0a3c635bb5f818d22468505ac78ce
cigamit <jimmy@sqmail.org>
2016-05-13 09:27:02 -0500

    correcting some fatal error

--- lib/data_query.php
+++ lib/data_query.php
@@ -28,6 +28,7 @@ function run_data_query($host_id, $snmp_query_id) {
 	query_debug_timer_start();
 
 	include_once($config['library_path'] . '/poller.php');
+	include_once($config['library_path'] . '/api_data_source.php');
 	include_once($config['library_path'] . '/utility.php');
 
 	query_debug_timer_offset('data_query', "Running data query [$snmp_query_id].");
