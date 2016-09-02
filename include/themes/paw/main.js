d69e3f38893d2c681a63af70dfec9170779383ad
Browniebraun <browniebraun@users.noreply.github.com>
2016-09-02 22:58:22 +0200

    one step closer :/

--- include/themes/paw/main.js
+++ include/themes/paw/main.js
@@ -2,7 +2,7 @@
 var pageName = basename($(location).attr('pathname'));
 
 
-/* only perform the the whole resizing blabla at the final end of the windows resize event */
+/* only perform the recalculation of elements at the final end of the windows resize event */
 var waitForFinalEvent = (function () {
   var timers = {};
   return function (callback, ms, uniqueId) {
@@ -16,24 +16,50 @@ var waitForFinalEvent = (function () {
   };
 })();
 
+$(window).resize(function () {
+	waitForFinalEvent(function(){
+		/* close open dropdown menues first off */
+		$('.dropdownMenu > ul').hide();
+		
+		heightPage = $(window).height();
+		heightPageHead = $('#cactiPageHead').outerHeight();
+		heightPageContent = heightPage -heightPageHead +1;
+		$('body').css('height', heightPage);
+		$('#cactiContent').css('height', heightPageContent);
+		
+		/* check visibility of all tabs */
+		$('.maintabs nav ul li a').each( function() {
+			id = $(this).attr('id');
+			if( $(this).offset().top !== 0 ) {
+				if( $('#' + id + '-ellipsis').length == 0 ) {
+					var str = $(this).parent().html();
+					var str2 = str.replace( id , id + '-ellipsis');
+					$('#submenu-ellipsis').prepend('<li>' + str2 + '</li>');
+				}
+			}else {
+				
+				$('#' + id + '-ellipsis').parent().remove();
+			}
+		});
+		if($("#submenu-ellipsis li").length == 0) {
+			$(".ellipsis").hide(0);
+		}else {
+			$(".ellipsis").show(0);
+		}
+	}, 200, "resize-content");
+});
+
+$('<div id="cactiPageBottom" class="cactiPageBottom"></div>').insertAfter('#cactiContent');
+
 
 function themeReady() {
+
 	var pageName = basename($(location).attr('pathname'));
 	var hostTimer = false;
 	var clickTimeout = false;
 	var hostOpen = false;
 
-	$(window).resize(function () {
-		waitForFinalEvent(function(){
-			heightPage = $(window).height();
-			heightPageHead = $('#cactiPageHead').outerHeight();
-			heightPageBottom = $('#cactiPageBottom').outerHeight();
-			heightPageContent = heightPage -heightPageHead -heightPageBottom +1;
-			
-			$('body').css('height', heightPage);
-			$('#cactiContent').css('height', heightPageContent);
-		}, 200, "resize-content");
-	});
+
 	
 	// Setup the navigation menu
 	setMenuVisibility();
@@ -57,15 +83,33 @@ function themeReady() {
 	$('.cactiConsoleNavigationArea').find('#menu').appendTo($('.cactiConsoleNavigationArea').find('#navigation'));
 	$('.cactiConsoleNavigationArea').find('#navigation > table').remove();
 
-	/* Hey - No footer available ? */
-	$('<div id="cactiPageBottom" class="cactiPageBottom"></div>').insertAfter('#cactiContent');
+	/* 'ellipsis' menu in the middle */
+	if($('.ellipsis').length == 0 ){
+		$('<div class="maintabs ellipsis">'
+			+'<nav><ul>'
+				+'<li class="maintabs-submenu">'
+					+'<a class="submenu-ellipsis" href="#"><i class="fa fa-angle-down"></i></a></li>'
+			+'</ul></nav>'
+		+'</div>').insertAfter('.maintabs');	
+	}
+	$('<div class="dropdownMenu">'
+		+'<ul id="submenu-ellipsis" class="submenuoptions" style="display:none;">'
+		+'</ul>'
+	+'</div>').appendTo('body');	
 	
+	
+	/* Hey - No footer available ? */
+	//if($('#cactiPageBottom').length == 0) {
+		//$('<div id="cactiPageBottom" class="cactiPageBottom"></div>').insertAfter('#cactiContent');
+	//}
 	/* Console? Nope! */
 	submenu_counter = 10;
-	$('.maintabs nav ul li a').each( function() {	
-		if ( $(this).html() == 'Console') {
+		
+	$('.maintabs nav ul li a').each( function() {
+		id = $(this).attr('id');
+		if ( id == 'maintab-anchor-console') {
 			$(this).html("<i class='fa fa-paw'/>").css({'border-right':'1px solid #aaa'});
-		}else if ( $(this).html() == 'Graphs' && $(this).parent().hasClass('maintabs-has-submenu') == 0 ) {
+		}else if ( id == 'maintab-anchor-graphs' && $(this).parent().hasClass('maintabs-has-submenu') == 0 ) {
 			submenu_counter++;
 			$(this).parent().addClass('maintabs-has-submenu');
 			$('<li class="maintabs-submenu"><a class="submenu-' + submenu_counter + '" href="#"><i class="fa fa-angle-down"></i></a></li>').insertAfter( $(this) );
@@ -76,9 +120,13 @@ function themeReady() {
 					+'<li><a href="graph_view.php?action=preview">Preview View</a></li>'
 				+'</ul>'
 			+'</div>').appendTo('body');
-		}
+		}else {
+			/* plugin stuff here ? */
+		}		
 	});
 	
+	
+
 	
 	/* user menu on the right ... */
 	if($('.usertabs').length == 0 ){
@@ -88,7 +136,7 @@ function themeReady() {
 				+'<li><a class="pic" href="graph_settings.php"><i class="fa fa-cog"></i></a></li>'
 				+'<li class="action-icon-user"><a class="pic" href="#"><i class="fa fa-user"></i></a></li>'
 			+'</ul></nav>'
-		+'</div>').insertAfter('.maintabs');	
+		+'</div>').insertAfter('.ellipsis');	
 	}
 	
 	ajaxAnchors();
@@ -100,15 +148,23 @@ function themeReady() {
 	
 	
 	$(window).trigger('resize');
-	$('.action-icon-user').unbind('click').click( function() {
-		
-		$('.menuoptions').slideToggle(120);
-		slideUp(120);
+	
+	
+	$('.action-icon-user').unbind().click(function(event) {
+		event.preventDefault();
+
+		if( $('.menuoptions').is(':visible') === false ) {
+			$('.submenuoptions').slideUp(120);
+			$('.menuoptions').slideDown(120);
+		}else {
+			$('.menuoptions').slideUp(120);
+		}
 		return false;
 	});
 
-	$('.maintabs-submenu').unbind('click').click( function() {
-
+	$('.maintabs-submenu').unbind('click').click(function(event) {
+		event.preventDefault();
+		
 		submenu_index = $(this).children('a:first').attr('class');
 		submenu = $('#'+submenu_index);
 		
@@ -118,7 +174,15 @@ function themeReady() {
 			$('.menuoptions').slideUp(120);
 			/* re-position */
 			position = $(this).parent('.maintabs-has-submenu').position();
-			submenu.css({'left':position.left}).slideDown(120);
+			if(!position) {
+				position = $(this).position();
+				submenu.css({'left':position.left - parseInt(submenu.outerWidth()) + parseInt($(this).outerWidth()) }).slideDown(120);
+
+			}else {
+				/* move dd to the left */
+
+				submenu.css({'left':position.left}).slideDown(120);
+			}
 		}else {
 			submenu.slideUp(120);
 		}
@@ -145,7 +209,7 @@ function themeReady() {
 
 	/* Notification Handler */
 	if( $("#message").length ) {
-		alert($('#message_container').html());
+	//	alert($('#message_container').html());
 	}
 
 	/* Replace icons */
