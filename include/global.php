01962ea575508b9b9368937676d6557103a6db3e
cigamit <jimmy@sqmail.org>
2016-10-19 22:25:38 -0500

    Can't check if table exists before database is initialized

--- include/global.php
+++ include/global.php
@@ -162,12 +162,6 @@ $colors['form_alternate2'] = 'E5E5E5';
 /* include base modules */
 include_once($config['library_path'] . '/database.php');
 
-if (isset($cacti_db_session) && $cacti_db_session && db_table_exists('sessions')) {
-	include(dirname(__FILE__) . '/session.php');
-}else{
-	$cacti_db_session = false;
-}
-
 if ((isset($no_http_headers) && $no_http_headers == true) || in_array(basename($_SERVER['PHP_SELF']), $no_http_header_files, true)) {
 	$is_web = false;
 }elseif ($_SERVER['PHP_SELF'] != '') {
@@ -180,6 +174,12 @@ if (!db_connect_real($database_hostname, $database_username, $database_password,
 	print 'FATAL: Connection to Cacti database failed. Please insure the database is running and your credentials in config.php are valid.';
 	print $is_web ? '</p>':'';
 	exit;
+}
+
+if (isset($cacti_db_session) && $cacti_db_session && db_table_exists('sessions')) {
+	include(dirname(__FILE__) . '/session.php');
+}else{
+	$cacti_db_session = false;
 }
 
 /* include additional modules */
