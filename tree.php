db6d8209589b77c7c3e00967b0f1ed47a77abda0
cigamit <jimmy@sqmail.org>
2017-03-05 10:59:15 -0600

    Resolving Issue #375
    Drag and Drop of Devices and Graphs allows dropping onto self

--- tree.php
+++ tree.php
@@ -978,10 +978,13 @@ function tree_edit() {
 					},
 					'core' : {
 						'animation' : 0,
-						'check_callback' : true
+						'check_callback' : function(operation, node, node_parent, node_position, more) {
+							return false;  // not dragging onto self
+						}
 					},
 					'dnd' : {
-						'always_copy' : true
+						'always_copy' : true,
+						'check_while_dragging': true
 					},
 					'themes' : { 'stripes' : true },
 					'plugins' : [ 'wholerow', <?php if ($editable) {?>'dnd', <?php }?>'types' ]
