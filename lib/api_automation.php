570466c2b63525e699fbd5cb1cecad8796c94da7
cigamit <jimmy@sqmail.org>
2017-03-31 07:54:15 -0500

    Resolving Issue #499
    SQL Error in Graph Automation resulting in no Graphs on Tree

--- lib/api_automation.php
+++ lib/api_automation.php
@@ -780,7 +780,7 @@ function display_new_graphs($rule, $url) {
 		}
 
 		/* rule item filter first */
-		$sql_filter	= build_rule_item_filter($rule_items, 'a.');
+		$sql_filter	= build_rule_item_filter($rule_items, ' a.');
 
 		/* filter on on the display filter next */
 		$sql_having = build_graph_object_sql_having($rule, get_request_var('filter'));
@@ -1487,7 +1487,7 @@ function build_graph_object_sql_having($rule, $filter) {
 			$i = 0;
 
 			foreach($field_names as $column) {
-				$sql_having .= ($i == 0 ? '':' OR ') . $column['field_name'] . ' LIKE "%' . $filter . '%"';
+				$sql_having .= ($i == 0 ? '':' OR ') . '`' . $column['field_name'] . '`' . ' LIKE "%' . $filter . '%"';
 				$i++;
 			}
 
@@ -1592,7 +1592,7 @@ function build_rule_item_filter($automation_rule_items, $prefix = '') {
 
 			# field name
 			if ($automation_rule_item['field'] != '') {
-				$sql_filter .= (' ' . $prefix . $automation_rule_item['field']);
+				$sql_filter .= (' ' . $prefix . '`' . $automation_rule_item['field'] . '`');
 				#
 				$sql_filter .= ' ' . $automation_op_array['op'][$automation_rule_item['operator']] . ' ';
 				if ($automation_op_array['binary'][$automation_rule_item['operator']]) {
