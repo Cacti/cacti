edd08cf26b9d7751e948b097f5b46c819c575752
cigamit <jimmy@sqmail.org>
2017-04-02 07:41:47 -0500

    Updating some files to 1.1.2 release
    These won't fire until the version variable is updated in global.php

--- install/1_1_1_to_1_1_2.php
+++ install/1_1_1_to_1_1_2.php
@@ -23,5 +23,16 @@
 */
 
 function upgrade_to_1_1_2() {
+	db_install_execute('ALTER TABLE `graph_templates_item` 
+		DROP INDEX `local_graph_id`, 
+		ADD INDEX `local_graph_id_sequence` (`local_graph_id`, `sequence`)');	
+
+	db_install_execute('ALTER TABLE `graph_tree_items` 
+		DROP INDEX `parent`, 
+		ADD INDEX `parent_position` (`parent`, `position`)');
+	
+	db_install_execute('ALTER TABLE `graph_template_input_defs` 
+		COMMENT = \'Stores the relationship for what graph items are associated\';');
+
 	db_install_execute('ALTER TABLE `graph_tree` ADD INDEX `sequence` (`sequence`)');
 }
