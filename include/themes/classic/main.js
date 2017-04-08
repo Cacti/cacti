22c975e38034f5404a8685a6341b1a3131994bbd
cigamit <jimmy@sqmail.org>
2017-04-08 18:06:22 -0500

    Resolving Issue #532
    Unreliable scroll height causes issues in Chrome

--- include/themes/classic/main.js
+++ include/themes/classic/main.js
@@ -1,19 +1,26 @@
 function themeReady() {
-	height = document.body.scrollHeight;
-	$('body').css('height', height);
-	$('#navigation').css('height', height);
+	height = get_height();
+	$('#navigation, .cactiConsoleNavigationArea').css('height', height);
 	$('#navigation').show();
 
 	$(window).unbind().resize(function(event) {
-		height       = document.body.scrollHeight;
+		height = get_height();
 		treeWidth    = $('#navigation').width();
 		totalWidth   = $('body').width();
-		contentWidth = totalWidth - treeWidth - 23;
+		contentWidth = totalWidth - treeWidth - 25;
 		$('#navigation').css('width', treeWidth);
 		$('#navigation_right').css('width', contentWidth);
 
 		if (!$(event.target).hasClass('ui-resizable')) {
-			$('#navigation').css('height', height);
+			$('#navigation, .cactiConsoleNavigationArea').css('height', height);
 		}
 	});
+}
+
+function get_height() {
+	nsh  = parseInt($('#navigation').prop('scrollHeight'));
+	nrsh = parseInt($('#navigation_right').prop('scrollHeight'));
+	nh   = parseInt($('#navigation').height());
+	nrh  = parseInt($('#navigation_right').height());
+	return Math.max(nsh, nrsh, nh, nrh);
 }
