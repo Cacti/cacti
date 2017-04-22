d7e5c2abfb798a366c09396154daa820ef947a5d
cigamit <jimmy@sqmail.org>
2017-04-22 08:21:44 -0500

    Make upgrade script repeatable without errors

--- install/1_1_3_to_1_1_4.php
+++ install/1_1_3_to_1_1_4.php
@@ -23,68 +23,97 @@
 */
 
 function upgrade_to_1_1_4() {
-	db_install_execute('ALTER TABLE `cdef` ADD INDEX (`hash`)');
+	if (!db_index_exists('cdef', 'hash')) {
+		db_install_execute('ALTER TABLE `cdef` ADD INDEX (`hash`)');
+	}
 
-	db_install_execute(
-		'ALTER TABLE `cdef_items` 
-			DROP INDEX `cdef_id`,
-			ADD INDEX `cdef_id_sequence` (`cdef_id`, `sequence`)'
-	);
+	if (db_index_exists('cdef_items', 'cdef_id')) {
+		db_install_execute(
+			'ALTER TABLE `cdef_items` 
+				DROP INDEX `cdef_id`,
+				ADD INDEX `cdef_id_sequence` (`cdef_id`, `sequence`)'
+		);
+	}
 
-	db_install_execute(
-		'ALTER TABLE `data_input_fields` 
-			DROP INDEX `type_code`,
-			ADD INDEX `type_code_data_input_id` (`type_code`, `data_input_id`)'
-	);
+	if (db_index_exists('data_input_fields', 'type_code')) {
+		db_install_execute(
+			'ALTER TABLE `data_input_fields` 
+				DROP INDEX `type_code`,
+				ADD INDEX `type_code_data_input_id` (`type_code`, `data_input_id`)'
+		);
+	}
 
-	db_install_execute(
-		'ALTER TABLE `data_local` 
-			DROP INDEX `host_id`,
-			ADD INDEX `host_id_snmp_query_id` (`host_id`, `snmp_query_id`)'
-	);
+	if (db_index_exists('data_local', 'host_id')) {
+		db_install_execute(
+			'ALTER TABLE `data_local` 
+				DROP INDEX `host_id`,
+				ADD INDEX `host_id_snmp_query_id` (`host_id`, `snmp_query_id`)'
+		);
+	}
 
-	db_install_execute(
-		'ALTER TABLE `snmpagent_cache` 
-			DROP INDEX `mib`,
-			ADD INDEX `mib_name` (`mib`, `name`)'
-	);
+	if (db_index_exists('snmpagent_cache', 'mib')) {
+		db_install_execute(
+			'ALTER TABLE `snmpagent_cache` 
+				DROP INDEX `mib`,
+				ADD INDEX `mib_name` (`mib`, `name`)'
+		);
+	}
 
-	$result = db_fetch_cell("SHOW KEYS FROM host WHERE Column_name LIKE 'hostname'");
-	if (empty($result)) {
+	if (!db_index_exists('host', 'hostname')) {
 		db_install_execute('ALTER TABLE `host` ADD INDEX (`hostname`)');
 	}
 
-	db_install_execute(
-		'ALTER TABLE `snmpagent_managers_notifications` 
-			DROP INDEX `manager_id`,
-			DROP INDEX `manager_id2`,
-			ADD INDEX `manager_id_notification` (`manager_id`,`notification`)'
-	);
+	if (db_index_exists('snmpagent_managers_notifications', 'manager_id')) {
+		db_install_execute(
+			'ALTER TABLE `snmpagent_managers_notifications` 
+				DROP INDEX `manager_id`,
+				DROP INDEX `manager_id2`,
+				ADD INDEX `manager_id_notification` (`manager_id`,`notification`)'
+		);
+	}
 
-	db_install_execute(
-		'ALTER TABLE `snmpagent_notifications_log` 
-			DROP INDEX `manager_id`,
-			DROP INDEX `manager_id2`,
-			ADD INDEX `manager_id_notification` (`manager_id`,`notification`)'
-	);
+	if (db_index_exists('snmpagent_notifications_log', 'manager_id')) {
+		db_install_execute(
+			'ALTER TABLE `snmpagent_notifications_log` 
+				DROP INDEX `manager_id`,
+				DROP INDEX `manager_id2`,
+				ADD INDEX `manager_id_notification` (`manager_id`,`notification`)'
+		);
+	}
 
-	db_install_execute('ALTER TABLE `user_auth_group_members` DROP INDEX `group_id`');
-	db_install_execute('ALTER TABLE `user_auth_group_realm` DROP INDEX `group_id`');
-	db_install_execute('ALTER TABLE `user_log` DROP INDEX `username`');
-	db_install_execute('ALTER TABLE `vdef` ADD INDEX `hash` (`hash`)');
+	if (db_index_exists('user_auth_group_members', 'group_id')) {
+		db_install_execute('ALTER TABLE `user_auth_group_members` DROP INDEX `group_id`');
+	}
 
-	db_install_execute(
-		'ALTER TABLE `vdef_items` 
-			DROP INDEX `vdef_id`, 
-			ADD INDEX `vdef_id_sequence` (`vdef_id`, `sequence`)'
-	);
+	if (db_index_exists('user_auth_group_realm', 'group_id')) {
+		db_install_execute('ALTER TABLE `user_auth_group_realm` DROP INDEX `group_id`');
+	}
 
-	db_install_execute(
-		'ALTER TABLE `graph_templates_item`
-  			ADD INDEX `lgi_gti` (`local_graph_id`, `graph_template_id`)'
-	);
+	if (db_index_exists('user_log', 'username')) {
+		db_install_execute('ALTER TABLE `user_log` DROP INDEX `username`');
+	}
 
-	db_install_execute('ALTER TABLE `poller_item` ADD INDEX `poller_id_host_id` (`poller_id`, `host_id`)');
+	if (!db_index_exists('vdef', 'hash')) {
+		db_install_execute('ALTER TABLE `vdef` ADD INDEX `hash` (`hash`)');
+	}
 
-	db_install_execute('ALTER TABLE `automation_networks` ADD COLUMN `site_id` INT UNSIGNED DEFAULT "1" AFTER `poller_id`');
+	if (db_index_exists('vdef_items', 'vdef_id')) {
+		db_install_execute(
+			'ALTER TABLE `vdef_items` 
+				DROP INDEX `vdef_id`, 
+				ADD INDEX `vdef_id_sequence` (`vdef_id`, `sequence`)'
+		);
+	}
+
+	if (!db_index_exists('graph_templates_item', 'lgi_gti')) {
+		db_install_execute('ALTER TABLE `graph_templates_item` ADD INDEX `lgi_gti` (`local_graph_id`, `graph_template_id`)');
+	}
+
+	if (!db_index_exists('poller_item', 'poller_id_host_id')) {
+		db_install_execute('ALTER TABLE `poller_item` ADD INDEX `poller_id_host_id` (`poller_id`, `host_id`)');
+	}
+
+	if (!db_column_exists('automation_networks', 'site_id')) {
+		db_install_execute('ALTER TABLE `automation_networks` ADD COLUMN `site_id` INT UNSIGNED DEFAULT "1" AFTER `poller_id`');
+	}
 }
