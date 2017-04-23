7ecafd34e8437d3e39242efaedf2cb832799a67f
cigamit <jimmy@sqmail.org>
2017-04-23 06:21:46 -0500

    Resolving Issue #601
    Correcting some translation issues.

--- lib/html.php
+++ lib/html.php
@@ -1117,7 +1117,7 @@ function draw_actions_dropdown($actions_array, $delete_action = 1) {
 	global $config;
 
 	if (!isset($actions_array[0])) {
-		$my_actions[0]  = 'Choose an action';
+		$my_actions[0]  = __('Choose an action');
 		$my_actions    += $actions_array;
 		$actions_array  = $my_actions;
 	}
