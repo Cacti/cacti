49a31b81057a69456df05af621ae2bbe35470cd7
cigamit <jimmy@sqmail.org>
2017-04-30 19:55:36 -0500

    Small wc3 validation error in checkbox function

--- lib/html_form.php
+++ lib/html_form.php
@@ -780,7 +780,7 @@ function form_checkbox($form_name, $form_previous_value, $form_caption, $form_de
 	}
 
 	if (strlen($class)) {
-		$class = " class='$class'";
+		$class = ' ' . trim($class);
 	}
 
 	if (strlen($on_change)) {
@@ -793,7 +793,7 @@ function form_checkbox($form_name, $form_previous_value, $form_caption, $form_de
 		$checked = " aria-checked='false'";
 	}
 
-	print "<input class='formCheckbox' type='checkbox' id='$form_name' name='$form_name'" . $on_change . $class . $checked . ">" . ($form_caption != '' ? " <label class='formCheckboxLabel' for='$form_name'>$form_caption</label>\n":"");
+	print "<input class='formCheckbox$class' type='checkbox' id='$form_name' name='$form_name'" . $on_change . $checked . ">" . ($form_caption != '' ? " <label class='formCheckboxLabel' for='$form_name'>$form_caption</label>\n":"");
 }
 
 /* form_radio_button - draws a standard html radio button
