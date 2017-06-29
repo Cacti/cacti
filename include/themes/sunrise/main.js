0b7bd8b3642ff8c5c178040214163e278a0affc4
browniebraun <browniebraun@outlook.com>
2017-06-29 17:22:27 +0200

    - bug fix for ellipses in Paw, Paper-plane and Sunrise
    Signed-off-by: browniebraun <browniebraun@outlook.com>

--- include/themes/sunrise/main.js
+++ include/themes/sunrise/main.js
@@ -42,6 +42,7 @@ function keepWindowSize() {
 						var str = $(this).parent().html();
 						var str2 = str.replace( id , id + '-ellipsis');
 						$('#submenu-ellipsis').prepend('<li>' + str2 + '</li>');
+						$('#'+ id + '-ellipsis').css('visibility','');
 						$(this).css('visibility', 'hidden');
 					}
 				}else {
@@ -113,10 +114,6 @@ function themeReady() {
 			+'</ul></nav>'
 		+'</div>').insertAfter('.maintabs');
 	}
-	$('<div class="dropdownMenu">'
-		+'<ul id="submenu-ellipsis" class="submenuoptions" style="display:none;">'
-		+'</ul>'
-	+'</div>').appendTo('body');
 	$('<div class="dropdownMenu">'
 		+'<ul id="submenu-ellipsis" class="submenuoptions" style="display:none;">'
 		+'</ul>'
