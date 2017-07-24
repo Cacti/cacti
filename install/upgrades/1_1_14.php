0693c4af12f6bdfa43984a7c22ec18c71c7b4fd3
Tony Roman <rony@cacti.net>
2017-07-24 10:03:28 -0400

    Fix PHP Fatal error in install upgrades from 1.1.11 or earlier

--- install/upgrades/1_1_14.php
+++ install/upgrades/1_1_14.php
@@ -22,7 +22,7 @@
  +-------------------------------------------------------------------------+
 */
 
-function upgrade_to_1_1_11() {
+function upgrade_to_1_1_14() {
 	db_install_execute('ALTER TABLE automation_networks
 		MODIFY COLUMN subnet_range VARCHAR(1024) NOT NULL DEFAULT ""');
 }
