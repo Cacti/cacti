53a862c3458525b1282c9978ca13208c6205bb15
patrick <patrick@patrickrademaker.nl>
2017-07-30 18:39:40 +0200

    Let's have this a bit more optimized. :)
    Reference commit: 912e927b9730139046c37e8e72331d84310ff651

--- graphs.php
+++ graphs.php
@@ -262,19 +262,23 @@ function form_save() {
 			}
 		}
 
-		$lg_template_id = db_fetch_cell_prepared('SELECT graph_template_id
-			FROM graph_local
-			WHERE id = ?',
-			array($local_graph_id));
-
-		$lg_dq_id = db_fetch_cell_prepared('SELECT snmp_query_id
-			FROM graph_lcoal
-			WHERE id = ?',
-			array($local_graph_id));
-
 		if (!is_error_message()) {
+				$lg_template_id = db_fetch_cell_prepared(
+					'SELECT graph_template_id
+					 FROM graph_local
+					 WHERE id = ?',
+					array($local_graph_id)
+				);
+
 			if ($lg_template_id > 0) {
 				change_graph_template($local_graph_id, $gt_id_unparsed);
+
+				$lg_dq_id = db_fetch_cell_prepared(
+					'SELECT snmp_query_id
+					 FROM graph_lcoal
+					 WHERE id = ?',
+					array($local_graph_id)
+				);
 
 				if ($lg_dq_id > 0) {
 					update_graph_data_query_cache($local_graph_id);
