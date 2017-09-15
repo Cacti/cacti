f6c9aea325bdbbf00751a6226d53a6ebb80f59dc
DavidLiedke <liedke.david@googlemail.com>
2017-09-15 13:46:46 +0200

    Update phptwig to 1.24.2

--- include/phptwig/Template.php
+++ include/phptwig/Template.php
@@ -370,6 +370,12 @@ abstract class Twig_Template implements Twig_TemplateInterface
             }
 
             throw $e;
+        } catch (Throwable $e) {
+            while (ob_get_level() > $level) {
+                ob_end_clean();
+            }
+
+            throw $e;
         }
 
         return ob_get_clean();
@@ -581,7 +587,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
                 return;
             }
 
-            throw new Twig_Error_Runtime(sprintf('Method "%s" for object "%s" does not exist', $item, get_class($object)), -1, $this->getTemplateName());
+            throw new Twig_Error_Runtime(sprintf('Neither the property "%1$s" nor one of the methods "%1$s()", "get%1$s()"/"is%1$s()" or "__call()" exist and have public access in class "%2$s"', $item, get_class($object)), -1, $this->getTemplateName());
         }
 
         if ($isDefinedTest) {
