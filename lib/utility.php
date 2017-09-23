61ae57c5d20cdf6ab4897cf0f093ddda49346d08
Patrick <patrick@patrickrademaker.nl>
2017-09-23 23:27:28 +0200

    Updated Dutch translations (#999)
    - 1007 translations remaining (70% translated)
    - English typo fixes

--- lib/utility.php
+++ lib/utility.php
@@ -683,7 +683,7 @@ function utilities_get_mysql_recommendations() {
 		'version' => array(
 			'value' => '5.6',
 			'measure' => 'gt',
-			'comment' => __('MySQL 5.6+ and MariaDB 10.0+ are great releases, and are very good versions to choose. Make sure you run the very latest release though which fixes a long standing low level networking issue that was casuing spine many issues with reliability.')
+			'comment' => __('MySQL 5.6+ and MariaDB 10.0+ are great releases, and are very good versions to choose. Make sure you run the very latest release though which fixes a long standing low level networking issue that was causing spine many issues with reliability.')
 			)
 	);
 
@@ -714,7 +714,7 @@ function utilities_get_mysql_recommendations() {
 				'value' => 'utf8',
 				'class' => 'warning',
 				'measure' => 'equal',
-				'comment' => __('When using Cacti with languages other than English, it is important ot use the utf8 character set as some characters take more than a single byte.  If you are first just now installing Cacti, stop, make the changes and start over again.  If your Cacti has been running and is in production, see the internet for instructions on converting your databases and tables if you plan on supporting other languages.')
+				'comment' => __('When using Cacti with languages other than English, it is important to use the utf8 character set as some characters take more than a single byte. If you are first just now installing Cacti, stop, make the changes and start over again. If your Cacti has been running and is in production, see the internet for instructions on converting your databases and tables if you plan on supporting other languages.')
 				)
 		);
 	} else {
