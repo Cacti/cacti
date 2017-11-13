6e7d2617eef2f3faa1fc1fe93fed2f8738bb76df
Browniebraun <browniebraun@users.noreply.github.com>
2017-11-13 15:09:24 +0100

    usage of get_default_contextoption

--- cmd_realtime.php
+++ cmd_realtime.php
@@ -138,17 +138,9 @@ if (sizeof($polling_items)) {
 				WHERE id = ?',
 				array($col_poller_id));
 
-			if (get_url_type() == 'https') {
-				$fgc_contextoption = array(
-					'ssl' => array(
-						'verify_peer' => false,
-						'verify_peer_name' => false,
-						'allow_self_signed' => true,
-					)
-				);
-
+			$fgc_contextoption = get_default_contextoption();
+			if($fgc_contextoption) {
 				$fgc_context = stream_context_create($fgc_contextoption);
-
 				$output = file_get_contents(get_url_type() . '://' . $hostname . $config['url_path'] . '/remote_agent.php?action=polldata&host_id=' . $host_id . '&local_data_id=' . $data_source . '&poller_id=' . $poller_id, FALSE, $fgc_context);
 			} else {
 				$output = file_get_contents(get_url_type() . '://' . $hostname . $config['url_path'] . '/remote_agent.php?action=polldata&host_id=' . $host_id . '&local_data_id=' . $data_source . '&poller_id=' . $poller_id);
