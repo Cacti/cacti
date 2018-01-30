872e9f2f5766a48c8d8b69d58117509bbb16f8cd
DavidLiedke <liedke.david@googlemail.com>
2018-01-30 09:11:04 +0100

    Add script to create file with md5sum

---
+++ cli/create_md5sum.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A simple bash script to create MD5SUM from all cacti files
+
+#Delete old .md5sum file
+rm .md5sum
+
+#Create new .md5sum
+find . -type f -not -path "./.git/*" -print0 | xargs -0 md5sum > /tmp/MD5SUM; mv /tmp/MD5SUM .md5sum
