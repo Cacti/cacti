4440feb9a33e13619e6df45aa6c864856b1fe8cc
cigamit <jimmy@sqmail.org>
2018-04-06 18:49:04 -0500

    Minor spelling correstion on #1511

--- poller.php
+++ poller.php
@@ -193,13 +193,14 @@ if (sizeof($poller)) {
 }
 
 // correct for possible poller output not empty occurances
-$ds_needing_fixes = db_fetch_assoc_prepared('SELECT local_data_id, MIN(rrd_next_step) AS next_step,
-	COUNT(DISTINCT rrd_next_step) AS intances
+$ds_needing_fixes = db_fetch_assoc_prepared('SELECT local_data_id,
+	MIN(rrd_next_step) AS next_step,
+	COUNT(DISTINCT rrd_next_step) AS instances
 	FROM poller_item
 	WHERE poller_id = ?
 	AND rrd_num > 1
 	GROUP BY local_data_id
-	HAVING intances > 1',
+	HAVING instances > 1',
 	array($poller_id));
 
 if (sizeof($ds_needing_fixes)) {
