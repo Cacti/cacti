9ae631431abbe80e64d467f2212420c08138644d
Jimmy Conner <jimmy@sqmail.org>
2018-04-12 21:36:52 -0500

    Update test_email.php

--- cli/test_email.php
+++ cli/test_email.php
@@ -1,5 +1,35 @@
+#!/usr/bin/php -q
 <?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2018 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDtool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+/* do NOT run this script through a web browser */
+if (!isset($_SERVER['argv'][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
+	die('<br><strong>This script is only meant to run at the command line.</strong>');
+}
+
 $no_http_headers = true;
+
 include('../include/global.php');
 
 global $config;
@@ -11,9 +41,12 @@ $message .= '<b>Method</b>: ';
 print 'Checking Configuration...';
 
 $ping_results = true;
+
 $how = read_config_option('settings_how');
-if ($how < 0 || $how > 2)
+if ($how < 0 || $how > 2) {
 	$how = 0;
+}
+
 if ($how == 0) {
 	$mail = 'PHP\'s Mailer Class';
 } else if ($how == 1) {
