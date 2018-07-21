0bd4fe4b388fd9c0c5e4adf9b26a1a4e58d08951
cigamit <jimmy@sqmail.org>
2018-07-21 10:47:53 -0500

    Minor update for log_validation setting

--- install/upgrades/1_2_0.php
+++ install/upgrades/1_2_0.php
@@ -82,7 +82,10 @@ function upgrade_to_1_2_0() {
 
   db_install_execute("ALTER TABLE graph_tree_items
 		MODIFY COLUMN sort_children_type tinyint(3) unsigned NOT NULL DEFAULT '0'");
-	
-	db_install_execute('UPDATE graph_templates_graph 
+
+	db_install_execute('UPDATE graph_templates_graph
 		SET t_title="" WHERE t_title IS NULL or t_title="0"');
+
+	db_install_execute('UPDATE settings
+		SET name="log_validation" WHERE name="developer_mode"');
 }
