cf1a0e47be4f93ca4717d45d4e36910747645e4b
cigamit <jimmy@sqmail.org>
2018-08-08 07:45:42 -0500

    Resolving Issue #1818
    After unlocking a tree, F5 loads page without header resulting in error

--- lib/api_tree.php
+++ lib/api_tree.php
@@ -39,7 +39,7 @@ function api_tree_lock($tree_id, $user_id = 0, $web = true) {
 		array($user_id, $tree_id));
 
 	if ($web) {
-		header('Location: tree.php?action=edit&header=false&id=' . $tree_id);
+		header('Location: tree.php?action=edit&id=' . $tree_id);
 	}
 }
 
@@ -60,7 +60,7 @@ function api_tree_unlock($tree_id, $user_id = 0, $web = true) {
 		array($user_id, $tree_id));
 
 	if ($web) {
-		header('Location: tree.php?action=edit&header=false&id=' . $tree_id);
+		header('Location: tree.php?action=edit&id=' . $tree_id);
 	}
 }
 
