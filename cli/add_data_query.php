5a17a9cecce0964bb1ff54a54f82f6e2471b2fe5
cigamit <jimmy@sqmail.org>
2018-08-11 11:54:43 -0500

    PSR echo to print and proper return code

--- cli/add_data_query.php
+++ cli/add_data_query.php
@@ -56,7 +56,7 @@ if (sizeof($parms)) {
 			case '--host-id':
 				$host_id = trim($value);
 				if (!is_numeric($host_id)) {
-					echo "ERROR: You must supply a valid host-id to run this script!\n";
+					print "ERROR: You must supply a valid host-id to run this script!\n";
 					exit(1);
 				}
 	
@@ -64,7 +64,7 @@ if (sizeof($parms)) {
 			case '--data-query-id':
 				$data_query_id = $value;
 				if (!is_numeric($data_query_id)) {
-					echo "ERROR: You must supply a numeric data-query-id for all hosts!\n";
+					print "ERROR: You must supply a numeric data-query-id for all hosts!\n";
 					exit(1);
 				}
 
@@ -89,7 +89,7 @@ if (sizeof($parms)) {
 							$reindex_method = DATA_QUERY_AUTOINDEX_FIELD_VERIFICATION;
 							break;
 						default:
-							echo "ERROR: You must supply a valid reindex method for all hosts!\n";
+							print "ERROR: You must supply a valid reindex method for all hosts!\n";
 							exit(1);
 					}
 				}
@@ -98,12 +98,12 @@ if (sizeof($parms)) {
 			case '-V':
 			case '-v':
 				display_version();
-				exit;
+				exit(0);
 			case '--help':
 			case '-H':
 			case '-h':
 				display_help();
-				exit;
+				exit(0);
 			case '--list-hosts':
 				$displayHosts = true;
 				break;
@@ -114,7 +114,7 @@ if (sizeof($parms)) {
 				$quietMode = true;
 				break;
 			default:
-				echo "ERROR: Invalid Argument: ($arg)\n\n";
+				print "ERROR: Invalid Argument: ($arg)\n\n";
 				display_help();
 				exit(1);
 		}
@@ -137,17 +137,17 @@ if (sizeof($parms)) {
 	 * for update / insert options
 	 */
 	if (!isset($host_id)) {
-		echo "ERROR: You must supply a valid host-id for all hosts!\n";
+		print "ERROR: You must supply a valid host-id for all hosts!\n";
 		exit(1);
 	}
 
 	if (!isset($data_query_id)) {
-		echo "ERROR: You must supply a valid data-query-id for all hosts!\n";
+		print "ERROR: You must supply a valid data-query-id for all hosts!\n";
 		exit(1);
 	}
 
 	if (!isset($reindex_method)) {
-		echo "ERROR: You must supply a valid reindex-method for all hosts!\n";
+		print "ERROR: You must supply a valid reindex-method for all hosts!\n";
 		exit(1);
 	}
 
@@ -157,7 +157,7 @@ if (sizeof($parms)) {
 	 */
 	$host_name = db_fetch_cell('SELECT hostname FROM host WHERE id = ' . $host_id);
 	if (!isset($host_name)) {
-		echo "ERROR: Unknown Host Id ($host_id)\n";
+		print "ERROR: Unknown Host Id ($host_id)\n";
 		exit(1);
 	}
 
@@ -166,7 +166,7 @@ if (sizeof($parms)) {
 	 */
 	$data_query_name = db_fetch_cell('SELECT name FROM snmp_query WHERE id = ' . $data_query_id);
 	if (!isset($data_query_name)) {
-		echo "ERROR: Unknown Data Query Id ($data_query_id)\n";
+		print "ERROR: Unknown Data Query Id ($data_query_id)\n";
 		exit(1);
 	}
 
@@ -176,7 +176,7 @@ if (sizeof($parms)) {
 	$exists_already = db_fetch_cell("SELECT host_id FROM host_snmp_query WHERE host_id=$host_id AND snmp_query_id=$data_query_id AND reindex_method=$reindex_method");
 	if ((isset($exists_already)) &&
 		($exists_already > 0)) {
-		echo "ERROR: Data Query is already associated for host: ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
+		print "ERROR: Data Query is already associated for host: ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
 		exit(1);
 	} else {
 		db_execute('REPLACE INTO host_snmp_query 
@@ -191,10 +191,10 @@ if (sizeof($parms)) {
 	}
 
 	if (is_error_message()) {
-		echo "ERROR: Failed to add this data query for host ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
+		print "ERROR: Failed to add this data query for host ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
 		exit(1);
 	} else {
-		echo "Success - Host ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
+		print "Success - Host ($host_id: $host_name) data query ($data_query_id: $data_query_name) reindex method ($reindex_method: " . $reindex_types[$reindex_method] . ")\n";
 		exit;
 	}
 } else {
@@ -205,24 +205,24 @@ if (sizeof($parms)) {
 /*  display_version - displays version information */
 function display_version() {
 	$version = get_cacti_version();
-	echo "Cacti Add Data Query Utility, Version $version, " . COPYRIGHT_YEARS . "\n";
+	print "Cacti Add Data Query Utility, Version $version, " . COPYRIGHT_YEARS . "\n";
 }
 
 function display_help() {
 	display_version();
 
-	echo "\nusage: add_data_query.php --host-id=[ID] --data-query-id=[dq_id] --reindex-method=[method] [--quiet]\n\n";
-	echo "Required Options:\n";
-	echo "    --host-id         the numerical ID of the host\n";
-	echo "    --data-query-id   the numerical ID of the data_query to be added\n";
-	echo "    --reindex-method  the reindex method to be used for that data query\n";
-	echo "                      0|None   = no reindexing\n";
-	echo "                      1|Uptime = Uptime goes Backwards\n";
-	echo "                      2|Index  = Index Count Changed\n";
-	echo "                      3|Fields = Verify all Fields\n\n";
-	echo "List Options:\n";
-	echo "    --list-hosts\n";
-	echo "    --list-data-queries\n";
-	echo "    --quiet - batch mode value return\n\n";
-	echo "If the data query was already associated, it will be reindexed.\n\n";
+	print "\nusage: add_data_query.php --host-id=[ID] --data-query-id=[dq_id] --reindex-method=[method] [--quiet]\n\n";
+	print "Required Options:\n";
+	print "    --host-id         the numerical ID of the host\n";
+	print "    --data-query-id   the numerical ID of the data_query to be added\n";
+	print "    --reindex-method  the reindex method to be used for that data query\n";
+	print "                      0|None   = no reindexing\n";
+	print "                      1|Uptime = Uptime goes Backwards\n";
+	print "                      2|Index  = Index Count Changed\n";
+	print "                      3|Fields = Verify all Fields\n\n";
+	print "List Options:\n";
+	print "    --list-hosts\n";
+	print "    --list-data-queries\n";
+	print "    --quiet - batch mode value return\n\n";
+	print "If the data query was already associated, it will be reindexed.\n\n";
 }
