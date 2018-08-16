8025ac10cfa74df443d6c886130780f7776ba5e6
netniV <netniv@hotmail.com>
2018-08-16 19:16:38 +0000

    Redirect to permission denied page instead of displaying local message

--- clog_user.php
+++ clog_user.php
@@ -29,7 +29,7 @@ include_once('./lib/clog_webapi.php');
 
 /* check edit/alter permissions */
 if (!clog_authorized()) {
-	echo __('FATAL: YOU DO NOT HAVE ACCESS TO THIS AREA OF CACTI');
+	header('Location: permission_denied.php');
 	exit;
 }
 
