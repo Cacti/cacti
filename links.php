5179a477306b4cea33e57e06ec3fd67b2d89c980
cigamit <jimmy@sqmail.org>
2018-08-26 14:50:57 -0500

    Correct visual issue on linnks page

--- links.php
+++ links.php
@@ -425,7 +425,7 @@ function pages() {
 			$i++;
 		}
 	} else {
-		print "<tr><td><em>" . __('No Pages Found') . "</em></td></tr>\n";
+		print "<tr><td colspan='" . (sizeof($nav) + 1) . "'><em>" . __('No Pages Found') . "</em></td></tr>\n";
 	}
 
 	html_end_box(false);
