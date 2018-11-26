5c8ebf0a6a4707f9ef27c9f26bc15124723d4069
cigamit <jimmy@sqmail.org>
2018-11-26 05:48:31 -0600

    Preparing Beta4

--- include/global.php
+++ include/global.php
@@ -45,7 +45,7 @@ $cacti_version = trim($cacti_version);
 
 /* define cacti version */
 define('CACTI_VERSION', $cacti_version);
-define('CACTI_VERSION_BETA', 3);
+define('CACTI_VERSION_BETA', 4);
 
 // define documentation table of contents
 define('CACTI_DOCUMENTATION_TOC', 'docs/Table-of-Contents.html');
