55271a34b894ab51f8103db83e515af4759eabb9
cigamit <jimmy@sqmail.org>
2019-01-14 18:40:00 -0600

    issue: Legacy Cacti CLI check function proving unreliable on modern php

--- include/cli_check.php
+++ include/cli_check.php
@@ -23,7 +23,7 @@
 */
 
 /* do NOT run this script through a web browser */
-if (!isset($_SERVER['argv'][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
+if (strpos(php_sapi_name(), 'cli') === false) {
 	die('<br><strong>This script is only meant to run at the command line.</strong>');
 }
 
