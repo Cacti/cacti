9ea5ad9d133100b1faac7acb8e0212dd955cb516
cigamit <jimmy@sqmail.org>
2019-02-23 09:17:40 -0600

    Change logic after double review

--- lib/data_query.php
+++ lib/data_query.php
@@ -140,14 +140,15 @@ function run_data_query($host_id, $snmp_query_id) {
 
 	$remap = false;
 	if (query_check_suitable($new_sort_field, $old_sort_field, $host_id, $snmp_query_id)) {
-		if ($old_sort_field != $new_sort_field && $old_sort_field != '') {
-			query_debug_timer_offset('data_query', __('WARNING: Sort Field Association has Changed.  Re-mapping issues may occur!'));
+		if ($old_sort_field != $new_sort_field) {
+			if ($old_sort_field != '') {
+				query_debug_timer_offset('data_query', __('WARNING: Sort Field Association has Changed.  Re-mapping issues may occur!'));
 
-			cacti_log('WARNING: Sort Field has Changed for Device[' . $host_id . '] and DQ[' . $snmp_query_id . '].  Old Sort:' . $old_sort_field . ', New Sort:' . $new_sort_field . '.  Re-mapping issues may occur!', false, 'REINDEX');
+				cacti_log('WARNING: Sort Field has Changed for Device[' . $host_id . '] and DQ[' . $snmp_query_id . '].  Old Sort:' . $old_sort_field . ', New Sort:' . $new_sort_field . '.  Re-mapping issues may occur!', false, 'REINDEX');
+			}
 
+			$remap = true;
 		}
-
-		$remap = true;
 	} else {
 		$new_sort_field = $old_sort_field;
 	}
