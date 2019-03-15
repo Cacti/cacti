1d4195ca13dfd33b920bd02aae45a903fbac6c9c
netniV <netniv@hotmail.com>
2019-03-15 09:32:55 +0000

    Resolving issue #2522
    This commit corrects an incorrect PR which was correcting the usage of an invaid keyword 'DELECT'.  It was assumed that the correct keyword was SELECT when in fact it should have been DELETE

--- install/upgrades/1_0_0.php
+++ install/upgrades/1_0_0.php
@@ -1243,7 +1243,7 @@ function upgrade_to_1_0_0() {
 							array($keephex, $hex['id']));
 					}
 
-					db_install_execute('SELECT FROM colors WHERE id = ?', array($hex['id']));
+					db_install_execute('DELETE FROM colors WHERE id = ?', array($hex['id']));
 				}
 			}
 		}
@@ -1746,4 +1746,3 @@ function upgrade_to_1_0_0() {
 		}
 	}
 }
-
