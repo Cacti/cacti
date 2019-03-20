9d018229a26cc64840a112db9fc705d6724b7944
netniV <netniv@hotmail.com>
2019-03-20 14:15:02 +0000

    Allow users to ctrl-click to open link in a new tab

--- include/layout.js
+++ include/layout.js
@@ -1071,6 +1071,9 @@ function setGraphTabs() {
 
 function setupResponsiveMenuAndTabs() {
 	$('.maintabs a.lefttab, .dropdownMenu a, .menuoptions a, #gtabs a.righttab').not('[href^="http"], [href^="https"], [href^="#"], [target="_blank"]').off('click').on('click', function(event) {
+		if (!shouldCaptureClick(event))
+			return;
+
 		page = basename($(this).attr('href'));
 
 		if (page == 'logout.php' || page == 'auth_changepassword.php') {
@@ -1976,6 +1979,9 @@ function ajaxAnchors() {
 	var page = basename(location.pathname);
 
 	$('a.pic, a.linkOverDark, a.linkEditMain, a.console, a.hyperLink, a.tab').not('[href^="http"], [href^="https"], [href^="#"], [target="_blank"]').off('click').on('click', function(event) {
+		if (!shouldCaptureClick(event))
+			return;
+
 		event.preventDefault();
 		event.stopPropagation();
 
@@ -2196,8 +2202,16 @@ function setupSortable() {
 	});
 }
 
+function shouldCaptureClick(event) {
+	var evt = event ? event:window.event;
+	return !(evt.shiftKey || evt.altKey || evt.ctrlKey || evt.metaKey);
+}
+
 function setupBreadcrumbs() {
 	$('#breadcrumbs > li > a').click(function(event) {
+		if (!shouldCaptureClick(event))
+			return
+
 		event.preventDefault();
 		event.stopPropagation();
 
@@ -3182,6 +3196,9 @@ function initializeGraphs() {
 		$(this).attr('href', urlPath+'graph.php?local_graph_id='+graph_id).addClass('linkEditMain');;
 
 		$(this).off('click').on('click', function(event) {
+			if (!shouldCaptureClick(event))
+				return;
+
 			var graph_id=$(this).attr('id').replace('graph_','').replace('_mrtg','');
 
 			event.preventDefault();
@@ -3221,6 +3238,9 @@ function initializeGraphs() {
 			'&graph_end='+getTimestampFromDate($('#date2').val())).addClass('linkEditMain');
 
 		$(this).off('click').on('click', function(event) {
+			if (!shouldCaptureClick(event))
+				return;
+
 			var graph_id = $(this).attr('id').replace('graph_','').replace('_csv','');
 			event.preventDefault();
 			event.stopPropagation();
@@ -3335,6 +3355,9 @@ function initializeGraphs() {
 			'&graph_end='+getTimestampFromDate($('#date2').val())).addClass('linkEditMain');
 
 		$(this).off('click').on('click', function(event) {
+			if (!shouldCaptureClick(event))
+				return;
+
 			var graph_id = $(this).attr('id').replace('graph_','').replace('_util','');
 
 			event.preventDefault();
@@ -3366,6 +3389,9 @@ function initializeGraphs() {
 		});
 
 		$(this).off('click').on('click', function(event) {
+			if (!shouldCaptureClick(event))
+				return;
+
 			var graph_id = $(this).attr('id').replace('graph_','').replace('_realtime','');
 
 			event.preventDefault();
