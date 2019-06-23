40a269055882037a76693ff016ba48c67324a10f
cigamit <jimmy@sqmail.org>
2019-06-23 09:12:44 -0500

    Resolving Issue #2573
    Associated graph template for Data Query Disappears

--- lib/graphs.php
+++ lib/graphs.php
@@ -24,70 +24,87 @@
 
 function get_graph_template_details($local_graph_id) {
 	global $config;
-	$graph_local = db_fetch_row_prepared('SELECT id, graph_template_id, snmp_query_id
-		FROM graph_local
-		WHERE id = ?',
+
+	$graph_local = db_fetch_row_prepared('SELECT gl.*, gt.name AS template_name, sqg.name AS query_name
+		FROM graph_local AS gl
+		LEFT JOIN graph_templates AS gt
+		ON gl.graph_template_id = gt.id
+		LEFT JOIN snmp_query_graph AS sqg
+		ON sqg.id = gl.snmp_query_graph_id
+		WHERE gl.id = ?',
 		array($local_graph_id));
 
 	$aggregate = db_fetch_row_prepared('SELECT agt.id, agt.name
-				FROM aggregate_graphs AS ag
-				LEFT JOIN aggregate_graph_templates AS agt
-				ON ag.aggregate_template_id=agt.id
-				WHERE local_graph_id = ?',
-				array($local_graph_id));
+		FROM aggregate_graphs AS ag
+		LEFT JOIN aggregate_graph_templates AS agt
+		ON ag.aggregate_template_id=agt.id
+		WHERE local_graph_id = ?',
+		array($local_graph_id));
 
 	if (!empty($aggregate)) {
 		$url = $config['url_path'] . 'aggregate_graphs.php?action=edit&id=';
 
-		$hasDetail = !empty($aggregate['id']);
+		if (!empty($aggregate['id'])) {
+			return array(
+				'id'                => $local_graph_id,
+				'name'              => $aggregate['name'],
+				'graph_description' => __('Aggregated Device'),
+				'url'               => $url . $local_graph_id,
+				'source'            => GRAPH_SOURCE_AGGREGATE,
+			);
+		} else {
+			return array(
+				'id'                => $local_graph_id,
+				'name'              => __('Not Templated'),
+				'graph_description' => __('Not Applicable'),
+				'url'               => $url . $local_graph_id,
+				'source'            => GRAPH_SOURCE_AGGREGATE,
+			);
+		}
+	} elseif ($graph_local['graph_template_id'] == 0) {
 		return array(
-			'id'                => $local_graph_id,
-			'name'              => $hasDetail ? $aggregate['name'] : __('Not Templated'),
-			'graph_description' => $hasDetail ? __('Aggregated Device') : __('Not Applicable'),
-			'url'               => $url . $local_graph_id,
-			'source'            => GRAPH_SOURCE_AGGREGATE,
-		);
-	} elseif (!cacti_sizeof($graph_local) || $graph_local['graph_template_id'] == 0) {
-		return array(
-			'id'     => 0,
+			'id'     => $local_graph_id,
 			'name'   => __('Not Templated'),
 			'url'    => '',
 			'source' => GRAPH_SOURCE_PLAIN,
 		);
-	} elseif ($graph_local['snmp_query_id'] > 0) {
-		$url = $config['url_path'] . 'data_queries.php?action=item_edit&id=';
+	} elseif ($graph_local['snmp_query_id'] > 0 && $graph_local['snmp_query_graph_id'] > 0) {
+		$url = $config['url_path'] . 'data_queries.php' .
+			'?action=item_edit' .
+			'&id=' . $graph_local['snmp_query_graph_id'] .
+			'&snmp_query_id=' . $graph_local['snmp_query_id'];
 
-		$detail = db_fetch_row_prepared('SELECT sqg.id, sqg.name
-			FROM snmp_query_graph AS sqg
-			INNER JOIN graph_local AS gl
-			ON gl.snmp_query_graph_id=sqg.id
-			AND gl.snmp_query_id=sqg.snmp_query_id
-			WHERE gl.id = ?',
-			array($local_graph_id));
-
-		$hasDetail = (cacti_sizeof($detail));
 		return array(
-			'id'     => $hasDetail ? $detail['id'] : 0,
-			'name'   => $hasDetail ? $detail['name'] : __('Not Found'),
-			'url'    => $hasDetail ? ($url . $detail['id']) : '',
+			'id'     => $graph_local['snmp_query_graph_id'],
+			'name'   => (!empty($graph_local['query_name']) ? $graph_local['query_name'] : __('Not Found')),
+			'url'    => $url,
+			'source' => GRAPH_SOURCE_DATA_QUERY
+		);
+	} elseif ($graph_local['snmp_query_id'] > 0 && $graph_local['snmp_query_graph_id'] == 0) {
+		return array(
+			'id'     => 0,
+			'name'   => __('Damaged Graph'),
+			'url'    => '',
 			'source' => GRAPH_SOURCE_DATA_QUERY
 		);
 	} else {
-		$url = $config['url_path'] . 'graph_templates.php?action=template_edit&id=';
+		if (!empty($graph_local['template_name'])) {
+			$url = $config['url_path'] . 'graph_templates.php?action=template_edit&id=' . $graph_local['graph_template_id'];
 
-		$detail = db_fetch_row_prepared('SELECT gt.id, gt.name
-			FROM graph_templates AS gt
-			INNER JOIN graph_local AS gl
-			ON gl.graph_template_id=gt.id
-			WHERE gl.id = ?',
-			array($local_graph_id));
-
-		$hasDetail = (cacti_sizeof($detail));
-		return array(
-			'id'   => $hasDetail ? $detail['id'] :0,
-			'name' => $hasDetail ? $detail['name'] : __('Not Found'),
-			'url'  => $hasDetail ? ($url . $detail['id']) : '',
-			'source' => GRAPH_SOURCE_TEMPLATE,
-		);
+			return array(
+				'id'     => $graph_local['graph_template_id'],
+				'name'   => $graph_local['template_name'],
+				'url'    => $url,
+				'source' => GRAPH_SOURCE_TEMPLATE,
+			);
+		} else {
+			return array(
+				'id'     => 0,
+				'name'   => __('Not Found'),
+				'url'    => '',
+				'source' => GRAPH_SOURCE_TEMPLATE,
+			);
+		}
 	}
 }
+
