10a8ea93f487930a0e7159b3c2ac99eb9cb0569e
cigamit <jimmy@sqmail.org>
2019-06-25 06:46:03 -0500

    Make travis happy

--- cli/splice_rrd.php
+++ cli/splice_rrd.php
@@ -53,7 +53,7 @@ if (!function_exists('get_cacti_cli_version')) {
 	}
 }
 
-if (!function_exists('is_resource_writeable')) {
+if (!function_exists('is_resource_writable')) {
 	function is_resource_writable($path) {
 		if ($path{strlen($path)-1}=='/') {
 			return is_resource_writable($path.uniqid(mt_rand()).'.tmp');
