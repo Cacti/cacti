a6d5cd267b97ce42feda66b0adbf596dab3f2d01
netniV <netniv@hotmail.com>
2019-08-01 13:06:40 +0000

    Install/Upgrade log does not show anything
    Closes #2832

--- lib/installer.php
+++ lib/installer.php
@@ -3216,7 +3216,7 @@ class Installer implements JsonSerializable {
 		global $config;
 
 		$page_nr = 1;
-		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , $page_no, $total_rows);
+		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , $page_nr, $total_rows);
 
 		$output_log = '';
 		foreach ($logcontents as $logline) {
