24145c5e532633a950f6821d412f775b43a74b85
cigamit <jimmy@sqmail.org>
2019-08-11 13:39:45 -0500

    Resolving Issue #2878
    PHP 7.4 Function get_magic_quotes_gpc() is deprecated

--- include/global.php
+++ include/global.php
@@ -381,20 +381,22 @@ if ($config['is_web']) {
 	if (!session_id()) session_start();
 
 	/* we never run with magic quotes on */
-	if (get_magic_quotes_gpc()) {
-		$process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
-		foreach ($process as $key => $val) {
-			foreach ($val as $k => $v) {
-				unset($process[$key][$k]);
-				if (is_array($v)) {
-					$process[$key][stripslashes($k)] = $v;
-					$process[] = &$process[$key][stripslashes($k)];
-				} else {
-					$process[$key][stripslashes($k)] = stripslashes($v);
+	if (version_compare(PHP_VERSION, '5.4', '<=')) {
+		if (get_magic_quotes_gpc()) {
+			$process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+			foreach ($process as $key => $val) {
+				foreach ($val as $k => $v) {
+					unset($process[$key][$k]);
+					if (is_array($v)) {
+						$process[$key][stripslashes($k)] = $v;
+						$process[] = &$process[$key][stripslashes($k)];
+					} else {
+						$process[$key][stripslashes($k)] = stripslashes($v);
+					}
 				}
 			}
+			unset($process);
 		}
-		unset($process);
 	}
 
 	/* make sure to start only only Cacti session at a time */
