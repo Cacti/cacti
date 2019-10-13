58e2073261b98c3fcb42fc853dbd62a2de5a69c7
cigamit <jimmy@sqmail.org>
2019-10-13 14:32:31 -0500

    Visual change to Aggregates

--- lib/api_aggregate.php
+++ lib/api_aggregate.php
@@ -1684,7 +1684,7 @@ function draw_aggregate_graph_items_list($_graph_id = 0, $_graph_template_id = 0
 			if ($use_custom_class == false) {
 				form_alternate_row();
 			} else {
-				print "<tr class='$customClass'>";
+				print "<tr class='tableRowGraph $customClass'>";
 			}
 
 			/* column 'Graph Item' */
