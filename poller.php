f3ba51193605de876852d4e757217b2953d065f8
cigamit <jimmy@sqmail.org>
2019-12-01 19:23:37 -0600

    Log the automatic enablement of boost

--- poller.php
+++ poller.php
@@ -952,7 +952,10 @@ function log_cacti_stats($loop_start, $method, $concurrent_processes, $max_threa
 function multiple_poller_boost_check() {
 	$pollers = db_fetch_cell('SELECT COUNT(*) FROM poller WHERE disabled="" AND id > 1');
 
-	if ($pollers > 0) {
+	if ($pollers > 0 && read_config_option('boost_rrd_update_enable') == '') {
+		cacti_log('NOTE: A second Cacti data collector has been added.  Therfore, enabling boost automatically!', false, 'POLLER');
+		admin_email(__('Cacti System Notification'), __('NOTE: A second Cacti data collector has been added.  Therfore, enabling boost automatically!'));
+
 		set_config_option('boost_rrd_update_enable', 'on');
 		set_config_option('boost_rrd_update_system_enable', 'on');
 	}
