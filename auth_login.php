990f57daa758588e90fdd5d7b78fa32548fa12d0
kim-fitness <47587743+kim-fitness@users.noreply.github.com>
2020-02-24 10:20:15 +0800

    Re-fix issue #3266

--- auth_login.php
+++ auth_login.php
@@ -333,7 +333,7 @@ if (get_nfilter_request_var('action') == 'login') {
 				ON uag.id=uagm.group_id
 				WHERE user_id=?', array($_SESSION['sess_user_id']));
 
-			if ($group_options > 0) {
+			if (!empty($group_options) && $group_options > 0 && $group_options != 4) {
 				$user['login_opts'] = $group_options;
 			}
 		}
