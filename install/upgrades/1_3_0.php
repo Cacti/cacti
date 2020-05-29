f215644470c18830577bd6f7256d6edd17bf7bc6
netniV <netniv@hotmail.com>
2020-05-29 01:14:46 +0000

    Add missing 'db_install_change_column' function

--- install/upgrades/1_3_0.php
+++ install/upgrades/1_3_0.php
@@ -26,4 +26,5 @@ function upgrade_to_1_3_0() {
 	db_install_change_column('version', array('name' => 'cacti', 'type' => 'char(30)', 'null' => false, 'default' => ''));
 	db_install_add_column('user_auth', array('name' => 'tfa_enabled', 'type' => 'char(3)', 'null' => false, 'default' => ''));
 	db_install_add_column('user_auth', array('name' => 'tfa_secret', 'type' => 'char(50)', 'null' => false, 'default' => ''));
+	db_install_add_column('poller', array('name' => 'log_level', 'type' => 'int', 'null' => false, 'default' => '-1'));
 }
