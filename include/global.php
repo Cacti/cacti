a37dbfabda973a23ab9cfc5ad6436d7bd4d71c2c
TheWitness <thewitness@cacti.net>
2020-09-17 08:48:53 -0400

    Minor tweak to CSP for Issue #3808
    Add image source extensions

--- include/global.php
+++ include/global.php
@@ -430,7 +430,7 @@ if ($config['is_web']) {
 	}
 	$alternates = read_config_option('content_security_alternate_sources');
 
-	header("Content-Security-Policy: default-src *; img-src 'self' data: blob:; style-src 'self' 'unsafe-inline' $alternates; script-src 'self' $script_policy 'unsafe-inline' $alternates; frame-ancestors 'self'; worker-src 'self'");
+	header("Content-Security-Policy: default-src *; img-src 'self' $alternates data: blob:; style-src 'self' 'unsafe-inline' $alternates; script-src 'self' $script_policy 'unsafe-inline' $alternates; frame-ancestors 'self'; worker-src 'self'");
 
 	/* prevent IE from silently rejects cookies sent from third party sites. */
 	header('P3P: CP="CAO PSA OUR"');
