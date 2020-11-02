b5d8df65fb1d08260f2745eeea57c3c24f4e34df
TheWitness <thewitness@cacti.net>
2020-11-01 20:24:59 -0500

    Correct undefined variable during sort

--- lib/html_utility.php
+++ lib/html_utility.php
@@ -784,6 +784,9 @@ function update_order_string($inplace = false) {
 			unset($_SESSION['sort_string'][$page]);
 
 			$_SESSION['sort_data'][$page][get_request_var('sort_column')] = get_request_var('sort_direction');
+
+			$column = get_request_var('sort_column');
+
 			if ($column == 'hostname' || $column == 'ip' || $column == 'ip_address') {
 				$_SESSION['sort_string'][$page] ='ORDER BY INET_ATON(' . $column . ") " . $direction;
 			} else {
