234f90cbab28338c6bbf5143984c2d5617c8d698
Datatech UK Ltd <marc.banyard@datatechuk.com>
2021-01-17 19:11:56 +0000

    Only show sensitive graph information to Template Editors in the List View screen (#4049)
    * Update main.css
    
    Make it more obvious that a slider is disabled.
    
    * Update main.css
    
    Make it more obvious that a radio slider is disabled.
    
    * Update cacti.sql
    
    Fixed the default value for the 30 second polling and amended the default values for the 1 minute polling for new installs
    
    * Add ability to Hide the Graph Drilldown icons from the graph_view.php and graph.php pages
    The icons are on the right hand side of the graphs and the default behaviour is to display them unless you hide them per user in the permissions tab
    
    * Updated CHANGELOG
    
    * Change the Realm from 2001 to 27 as 100 and above are reserved for plug-ins.
    
    * Formatting change for neatness.
    
    * Update CHANGELOG to reference this ticket number. Changed Drilldown to Drilldowns
    
    * Only show sensitive graph information to Template Editors in the List View screen
    
    * Removed duplicate line from CHANGELOG
    
    * Update CHANGELOG to reference this ticket number.
    
    * Updated cacti.pot as requested
    
    Co-authored-by: TheWitness <thewitness@cacti.net>

--- include/global_arrays.php
+++ include/global_arrays.php
@@ -1121,11 +1121,12 @@ $user_auth_realms = array(
 
 	21   => __('Reports Management'),
 	22   => __('Reports Creation'),
-	27   => __('Show Graph Action Icons')
+	27   => __('Show Graph Action Icons'),
+	28   => __('Show User Help Links')
 );
 
 $user_auth_roles = array(
-	__('Normal User')            => array(7, 19, 20, 22, 24, 25, 27),
+	__('Normal User')            => array(7, 19, 20, 22, 24, 25, 27, 28),
 	__('Template Editor')        => array(8, 2, 9, 10, 11, 12, 13, 14, 16, 17),
 	__('General Administration') => array(8, 3, 4, 5, 23, 1043),
 	__('System Administration')  => array(8, 15, 26, 1, 18, 21, 101)
