b9321aa42a02e09ee5c9c4875e290d688dc8d58f
TheWitness <thewitness@cacti.net>
2021-03-25 08:01:40 -0400

    Shorten Text on Script Server Queries and Update language file
    This will trigger the need for a new round of translations.

--- include/global_arrays.php
+++ include/global_arrays.php
@@ -456,7 +456,7 @@ $input_types = array(
 	DATA_INPUT_TYPE_SCRIPT              => __('Script/Command'),  // Action 1:
 	DATA_INPUT_TYPE_SCRIPT_QUERY        => __('Script Query'), // Action 1:
 	DATA_INPUT_TYPE_PHP_SCRIPT_SERVER   => __('Script Server'),
-	DATA_INPUT_TYPE_QUERY_SCRIPT_SERVER => __('Script Query - Script Server')
+	DATA_INPUT_TYPE_QUERY_SCRIPT_SERVER => __('Script Server Query')
 );
 
 $input_types_script = array(
