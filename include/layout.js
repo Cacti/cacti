befc9005e99fdb44aa4b09b87fadced2f21539a6
TheWitness <thewitness@cacti.net>
2021-12-09 08:58:59 -0500

    Fixing Issue #4498 - drop_callback width issue
    Layout function textBoxWidth() does not work on input type form elements

--- include/layout.js
+++ include/layout.js
@@ -254,7 +254,7 @@ $.fn.textWidth = function(text){
  *  without any wrapping. */
 $.fn.textBoxWidth = function() {
 	var org = $(this);
-	var html = $('<span style="display:none;white-space:nowrap;position:absolute;width:auto;left:-9999px">' + org.text() + '</span>');
+	var html = $('<span style="display:none;white-space:nowrap;position:absolute;width:auto;left:-9999px">' + (org.val() || org.text()) + '</span>');
 	html.css('font-family', org.css('font-family'));
 	html.css('font-weight', org.css('font-weight'));
 	html.css('font-size',   org.css('font-size'));
