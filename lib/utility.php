c7cb86ab39590d05533bec08fc3611212e561073
Petr Macek <petr.macek@kostax.cz>
2022-04-25 22:25:32 +0200

    update

--- lib/utility.php
+++ lib/utility.php
@@ -208,9 +208,9 @@ function update_poller_cache($data_source, $commit = false) {
 			$params[] = $data_source['id'];
 
 			$outputs = db_fetch_assoc_prepared('SELECT DISTINCT ' . SQL_NO_CACHE . "
-				sqgr.snmp_field_name, dtr.id as data_template_rrd_id
+				sqgr.snmp_field_name, dtr.id AS data_template_rrd_id
 				FROM snmp_query_graph_rrd AS sqgr
-				INNER JOIN data_template_rrd AS dtr FORCE INDEX (local_data_id)
+				INNER JOIN data_template_rrd AS dtr
 				ON sqgr.data_template_rrd_id = dtr.local_data_template_rrd_id
 				WHERE sqgr.data_template_id = ?
 				AND dtr.local_data_id = ?
