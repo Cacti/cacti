57118a7241f45c9e13eb122c42d46ab204d6ae98
TheWitness <thewitness@cacti.net>
2022-09-15 08:42:34 -0400

    Fixing #4917 - Periodic Realtime Issues
    Under certain circumstances Real Time counter gets stuck and does not count down

--- lib/html.php
+++ lib/html.php
@@ -2593,7 +2593,7 @@ function html_common_header($title, $selectedTheme = '') {
 	print get_md5_include_js('include/js/billboard.js');
 	print get_md5_include_js('include/layout.js');
 	print get_md5_include_js('include/js/pace.js');
-	print get_md5_include_js('include/realtime.js', true);
+	print get_md5_include_js('include/realtime.js');
 	print get_md5_include_js('include/themes/' . $selectedTheme .'/main.js');
 
 	if (isset($path2calendar) && file_exists($path2calendar)) {
