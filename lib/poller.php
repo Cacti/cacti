68fa31ba9f405fcac778f11484742032d95b16c5
TheWitness <thewitness@cacti.net>
2022-11-27 14:20:58 -0500

    Backport exec_with_timeout()

--- lib/poller.php
+++ lib/poller.php
@@ -159,6 +159,103 @@ function exec_background($filename, $args = '', $redirect_args = '') {
 	}
 }
 
+/**
+ * exec_with_timeout - Execute a command and return it's output. Either wait until the
+ * command exits or the timeout has expired.
+ *
+ * @param  (string)      $cmd          Command to execute.
+ * @param  (string)      $output       A return array of output.
+ * @param  (int)         $return_code  The return code from the script
+ * @param  (int)         $timeout      Timeout in seconds.
+ *
+ * @return (string|bool) Either the last line of output or false on error
+ */
+function exec_with_timeout($cmd, &$output, &$return_code, $timeout = 5) {
+	// File descriptors passed to the process.
+	$descriptors = array(
+		0 => array('pipe', 'r'),  // stdin
+		1 => array('pipe', 'w'),  // stdout
+		2 => array('pipe', 'w')   // stderr
+	);
+
+	// Start the process.
+	$process = proc_open('exec ' . $cmd, $descriptors, $pipes);
+
+	if (!is_resource($process)) {
+		return false;
+	}
+
+	// Set the stdout stream to non-blocking.
+	stream_set_blocking($pipes[1], 0);
+
+	// Set the stderr stream to non-blocking.
+	stream_set_blocking($pipes[2], 0);
+
+	// Turn the timeout into microseconds.
+	$timeout = $timeout * 1000000;
+
+	// Output buffer.
+	$buffer = '';
+
+	// While we have time to wait.
+	while ($timeout > 0) {
+		$start = microtime(true);
+
+		// Wait until we have output or the timer expired.
+		$read  = array($pipes[1]);
+		$other = array();
+		stream_select($read, $other, $other, 0, $timeout);
+
+		// Get the status of the process.
+		// Do this before we read from the stream,
+		// this way we can't lose the last bit of output if the process dies between these functions.
+		$status = proc_get_status($process);
+
+		// Read the contents from the buffer.
+		// This function will always return immediately as the stream is non-blocking.
+		$buffer .= stream_get_contents($pipes[1]);
+
+		if (!$status['running']) {
+			// Break from this loop if the process exited before the timeout.
+			break;
+		}
+
+		// Subtract the number of microseconds that we waited.
+		$timeout -= (microtime(true) - $start) * 1000000;
+	}
+
+	// Check if there were any errors.
+	$errors = stream_get_contents($pipes[2]);
+
+	if (isset($status['exitcode'])) {
+		$return_code = $status['exitcode'];
+	} else {
+		$return_code = 1;
+	}
+
+	if (!empty($errors)) {
+		return false;
+	}
+
+	// Kill the process in case the timeout expired and it's still running.
+	// If the process already exited this won't do anything.
+	proc_terminate($process, 9);
+
+	// Close all streams.
+	fclose($pipes[0]);
+	fclose($pipes[1]);
+	fclose($pipes[2]);
+
+	proc_close($process);
+
+	if ($buffer != '') {
+		$output = explode("\n", $buffer);
+		return(end($output));
+	} else {
+		return;
+	}
+}
+
 function file_escaped($file) {
 	if (substr($file, 0, 1) == '"' && substr($file, -1, 1) == '"') {
 		return true;
