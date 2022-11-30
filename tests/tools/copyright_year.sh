25ae53cf727dcf11419222c66a88c9cf6956b159
Mark Brugnoli-Vinten <netniv@hotmail.com>
2022-11-30 13:31:30 +0000

    Add run, and update copyright detection script

--- tests/tools/copyright_year.sh
+++ tests/tools/copyright_year.sh
@@ -1,33 +1,79 @@
-#!/bin/bash
+#!/usr/bin/env bash
+update_copyright() {
+	local file=$1
+	file=${file/$SCRIPT_BASE/};
+	printf -v line "%60s" "$file"
+	if [[ -z "$ERRORS_ONLY" ]]; then
+		echo -n "$line"
+		line=
+	fi
 
-# ----------------------------------------------
-# PHP Files First
-# ----------------------------------------------
-for file in `find . -name \*.php -print`;do 
-  echo -n $file;
-  grep '2004-2021' $file >/dev/null;
-  result=$?
+	old_data=$(grep -e "2004-20[0-9][0-9]" "$1" 2>/dev/null)
+	result=$?
 
-  if [[ $result -ne 1 ]]; then
-    echo " Updating Copyright Data"
-    sed -i s/"2004-2021 The Cacti Group"/"2004-2022 The Cacti Group"/g $file
-  else
-    echo " Skipping Copyright Data"
-  fi
+	if [[ $result -eq 0 ]]; then
+		if [[ $old_data =~ -${YEAR} ]]; then
+			if [[ -z "$ERRORS_ONLY" ]]; then
+				echo "$line Skipping Copyright Data"
+			fi
+		else
+			new_data=${old_data// 2004-20[0-9][0-9] / 2004-$YEAR }
+			echo "$line Updating Copyright Data"
+			printf "\tOld: %s\n\tNew: %s\n\n" "$old_data" "$new_data"
+			sed -i s/"$old_data"/"$new_data"/g $1
+		fi
+	else
+		echo "$line  Copyright not found!"
+		SCRIPT_ERR=1
+	fi
+}
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_BASE=$(realpath "${SCRIPT_DIR}/../../")/
+
+BAD_FOLDERS="include/vendor \*\*/vendor include/fa cache include/js"
+SCRIPT_EXCLUSION=
+for f in $BAD_FOLDERS; do
+	SCRIPT_EXCLUSION="$SCRIPT_EXCLUSION -not -path ${SCRIPT_BASE}$f/\* "
+done
+
+SCRIPT_ERR=0
+YEAR=$(date +"%Y")
+
+ERRORS_ONLY=1
+while [ -n "$1" ]; do
+	case $1 in
+# ------------------------------------------------------------------------------
+# Get inputs from user (Interactive mode)
+# ------------------------------------------------------------------------------
+		"--help")
+			echo "NOTE: Checks all Cacti pages for this years copyright"
+			echo ""
+			echo "usage: copyright_year.sh [-a]"
+			echo ""
+			;;
+		"-A"|"-a")
+			ERRORS_ONLY=0
+			;;
+		*)
+			;;
+	esac
+	shift;
 done
 
 # ----------------------------------------------
-# JavaScript Files First
+# PHP / JS / MD Files
 # ----------------------------------------------
-for file in `find . -name \*.js -print`;do 
-  echo -n $file;
-  grep '2004-2021 The Cacti Group' $file >/dev/null;
-  result=$?
+SCRIPT_INCLUSION=
+SCRIPT_SEPARATOR=
+for ext in sql php js md; do
+	if [ -n "$SCRIPT_INCLUSION" ]; then
+		SCRIPT_SEPARATOR="-o "
+	fi
+	SCRIPT_INCLUSION="$SCRIPT_INCLUSION $SCRIPT_SEPARATOR-name \*.$ext"
+done
 
-  if [[ $result -ne 1 ]]; then
-    echo " Updating Copyright Data"
-    sed -i s/"2004-2021 The Cacti Group"/"2004-2022 The Cacti Group"/g $file
-  else
-    echo " Skipping Copyright Data"
-  fi
+SCRIPT_CMD="find ${SCRIPT_BASE} -type f \( $SCRIPT_INCLUSION \) $SCRIPT_EXCLUSION -print0"
+bash -c "$SCRIPT_CMD" | while IFS= read -r -d '' file; do
+	update_copyright "${file}"
 done
