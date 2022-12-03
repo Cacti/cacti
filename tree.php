8a66c9587ab6eeddcbe424c7a598f45225698466
TheWitness <thewitness@cacti.net>
2022-12-03 15:23:04 -0500

    QA: Remove old calls #4965

--- tree.php
+++ tree.php
@@ -125,14 +125,10 @@ switch (get_request_var('action')) {
 	case 'lock':
 		api_tree_lock(get_request_var('id'), $_SESSION['sess_user_id']);
 		tree_edit(true);
-
-//		header('Location: tree.php?action=edit&header=false&id=' . get_request_var('id'));
 		break;
 	case 'unlock':
 		api_tree_unlock(get_request_var('id'), $_SESSION['sess_user_id']);
 		tree_edit(true);
-
-//		header('Location: tree.php?action=edit&header=false&id=' . get_request_var('id'));
 		break;
 	case 'copy_node':
 		api_tree_copy_node(get_request_var('tree_id'), get_request_var('id'), get_request_var('parent'), get_request_var('position'));
