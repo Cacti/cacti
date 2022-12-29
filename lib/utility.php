27308d840cc994997c767ca412385b13975608b4
TheWitness <thewitness@cacti.net>
2022-12-29 11:32:04 -0500

    QA: Adding intl module dependency
    This is due to some deprecation done by PHP in the PHP8 series

--- lib/utility.php
+++ lib/utility.php
@@ -1664,6 +1664,7 @@ function utility_php_verify_extensions(&$extensions, $source) {
 			'gd'        => array('cli' => false, 'web' => false),
 			'gmp'       => array('cli' => false, 'web' => false),
 			'hash'      => array('cli' => false, 'web' => false),
+			'intl'      => array('cli' => false, 'web' => false),
 			'json'      => array('cli' => false, 'web' => false),
 			'ldap'      => array('cli' => false, 'web' => false),
 			'mbstring'  => array('cli' => false, 'web' => false),
