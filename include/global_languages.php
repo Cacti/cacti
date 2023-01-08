Cacti/cacti

commit 85c11f54fcb9d1d95dbe7826f0da178296748861

Fixing #5147 - Language Processing
Error 500 on apache when changing languages

include/global_languages.php
@@ -533,8 +533,15 @@ function __() {
 			$args[0] = __gettext($args[0]);
 		}
 
-		/* process return string against input arguments */
-		return __uf(call_user_func_array('sprintf', $args));
+		$valid_args = array('%%', '%b', '%c', '%d', '%e', '%E', '%f', '%F', '%g', '%G', '%h', '%H', '%o', '%s', '%u', '%x', '%X');
+
+		if (array_search($args[0], $valid_args) !== false) {
+			/* process return string against input arguments */
+
+			return __uf(call_user_func_array('sprintf', $args));
+		} else {
+			return $args[0];
+		}
 	}
 }
 

lib/functions.php
@@ -6168,8 +6168,10 @@ function CactiErrorHandler($level, $message, $file, $line, $context = array()) {
 	$plugin = (is_array($output_array) && isset($output_array[1]) ? $output_array[1] : '');
 	$error  = 'Unknown error occurred';
 
-	if (!empty($phperrors[$level])) {
+	if ($level !== null && isset($phperrors[$level])) {
 		$error  = 'PHP ' . $phperrors[$level] . ($plugin != '' ? " in  Plugin '$plugin'" : '') . ": $message in file: $file  on line: $line";
+	} else {
+		$error  = 'PHP Unknown Error' . ($plugin != '' ? " in  Plugin '$plugin'" : '') . ": $message in file: $file  on line: $line";
 	}
 
 	switch ($level) {
@@ -6262,9 +6264,15 @@ function CactiShutdownHandler() {
 
 					$plugin = (isset($output_array[1]) ? $output_array[1] : '');
 
-					$message = 'PHP ' . $phperrors[$error['type']] .
-						($plugin != '' ? " in  Plugin '$plugin'" : '') . ': ' . $error['message'] .
-						' in file: ' .  $error['file'] . ' on line: ' . $error['line'];
+					if ($error['type'] !== null && isset($phperrors[$error['type']])) {
+						$message = 'PHP ' . $phperrors[$error['type']] .
+							($plugin != '' ? " in  Plugin '$plugin'" : '') . ': ' . $error['message'] .
+							' in file: ' .  $error['file'] . ' on line: ' . $error['line'];
+					} else {
+						$message = 'PHP Unknown Error' .
+							($plugin != '' ? " in  Plugin '$plugin'" : '') . ': ' . $error['message'] .
+							' in file: ' .  $error['file'] . ' on line: ' . $error['line'];
+					}
 
 					cacti_log($message, false, 'ERROR');
 					cacti_debug_backtrace('PHP ERROR', false, true, 0, 1);

commit fa66678e0bf3e01dd1e1b373700bfba6af32e765

Fix language processing broken by 85c11f54f (#5147)
Closes #5147

include/global_languages.php
@@ -533,9 +533,29 @@ function __() {
 			$args[0] = __gettext($args[0]);
 		}
 
-		$valid_args = array('%%', '%b', '%c', '%d', '%e', '%E', '%f', '%F', '%g', '%G', '%h', '%H', '%o', '%s', '%u', '%x', '%X');
+		$valid_args = array(
+			'%%', // Escaped percentage
+			'%b', // Binary
+			'%c', // Single Character
+			'%d', // Decimal
+			'%e', // Scientific notation (lowercase)
+			'%E', // Scientific notation (uppercase)
+			'%f', // Floating point (locale aware)
+			'%F', // Floating point (non-locale aware)
+			'%g', // General format (uses E and f styling if precision involved)
+			'%G', // General format (docs say same as g but uses E and f, yet it already does???)
+			'%h', // General format (like g but uses F)
+			'%H', // General format (like g but uses E and F)
+			'%o', // Integer as Octal
+			'%s', // String
+			'%u', // Integer as Unsigned Decimal
+			'%x', // Integer as hex (lowercase)
+			'%X', // Integer as hex (uppercase)
+		);
 
-		if (array_search($args[0], $valid_args, true) !== false) {
+		$valid_regexp = '/(' . implode(')|(', $valid_args) . ')/';
+
+		if (preg_match($valid_regexp, $args[0])) {
 			/* process return string against input arguments */
 
 			return __uf(call_user_func_array('sprintf', $args));