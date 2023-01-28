1ddbdf9cbb10a22a237dfa5627fd122a2b6f3274
TheWitness <thewitness@cacti.net>
2023-01-28 15:20:13 -0500

    QA: Remove some legacy support

--- cli/splice_rrd.php
+++ cli/splice_rrd.php
@@ -23,33 +23,11 @@
  +-------------------------------------------------------------------------+
 */
 
-/* work with both Cacti 1.x and Cacti 0.8.x */
-if (file_exists(__DIR__ . '/../include/cli_check.php')) {
-	require(__DIR__ . '/../include/cli_check.php');
-} elseif (file_exists(__DIR__ . '/../include/global.php')) {
-	/* do NOT run this script through a web browser */
-	if (!isset($_SERVER['argv'][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
-		die('<br>This script is only meant to run at the command line.');
-	}
-
-	$no_http_headers = true;
-
-	require(__DIR__ . '/../include/global.php');
-} else {
-	print "FATAL: Can not initialize the Cacti API" . PHP_EOL;
-	exit(1);
-}
+require(__DIR__ . '/../include/cli_check.php');
 
 if ($config['poller_id'] > 1) {
 	print "FATAL: This utility is designed for the main Data Collector only" . PHP_EOL;
 	exit(1);
-}
-
-// For legacy Cacti behavior
-if (!function_exists('cacti_sizeof')) {
-	function cacti_sizeof($object) {
-		return ($object === false || !is_array($object)) ? 0 : sizeof($object);
-	}
 }
 
 if (!function_exists('get_cacti_cli_version')) {
