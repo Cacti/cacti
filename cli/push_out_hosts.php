6ff364b147b9a471b5a035899b597ca6b6389c07
xmacan <petr.macek@kostax.cz>
2023-05-03 21:28:59 +0200

    log message always

--- cli/push_out_hosts.php
+++ cli/push_out_hosts.php
@@ -41,12 +41,13 @@ $php_binary = read_config_option('path_php_binary');
 
 $parameters = implode(' ', $parms);
 
+cacti_log('WARNING: Deprecated script push_out_hosts.php. Please use rebuild_poller_cache.php.', true, 'PUSHOUT');
+
 if (in_array('-v', $parms) || in_array('-V', $parms) || in_array('--version', $parms)) {
 	// exception for github tests
 	print 'Cacti Push out hosts/repopulate poller cache Tool, Version ' . CACTI_VERSION . ' ' . COPYRIGHT_YEARS . PHP_EOL;
 }
 else {
 	print 'WARNING: Deprecated script push_out_hosts.php. Please use rebuild_poller_cache.php.' . PHP_EOL;
-	cacti_log('WARNING: Deprecated script push_out_hosts.php. Please use rebuild_poller_cache.php.', true, 'PUSHOUT');
 	passthru ($php_binary . ' ' . CACTI_PATH_CLI . '/rebuild_poller_cache.php ' .$parameters);
 }
