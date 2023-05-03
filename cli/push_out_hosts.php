5ac13a535854a65d9bd651ea883766cad33ffe6b
xmacan <petr.macek@kostax.cz>
2023-05-03 21:02:29 +0200

    fix lowercase

--- cli/push_out_hosts.php
+++ cli/push_out_hosts.php
@@ -14,7 +14,7 @@
  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
  | GNU General Public License for more details.                            |
  +-------------------------------------------------------------------------+
- | Cacti: The Complete RRDtool-based Graphing Solution                     |
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
  +-------------------------------------------------------------------------+
  | This code is designed, written, and maintained by the Cacti Group. See  |
  | about.php and/or the AUTHORS file for specific developer information.   |
