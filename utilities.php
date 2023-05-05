ec3b7ea73fdbe32dda63fdabe05b7c7f2cee1d1d
Petr Macek <petr.macek@kostax.cz>
2023-05-05 14:38:31 +0200

    Fix #5323 for 1.2.x (#5326)
    * fix 5323 for 1.2.x
    
    * remove old note

--- utilities.php
+++ utilities.php
@@ -2284,7 +2284,11 @@ function utilities() {
 		__('Rebuild Poller Cache') => array(
 			'link'  => 'utilities.php?action=clear_poller_cache',
 			'mode'  => 'online',
-			'description' => __('The Poller Cache will be re-generated if you select this option. Use this option only in the event of a database crash if you are experiencing issues after the crash and have already run the database repair tools.  Alternatively, if you are having problems with a specific Device, simply re-save that Device to rebuild its Poller Cache.  There is also a command line interface equivalent to this command that is recommended for large systems.  NOTE: On large systems, this command may take several minutes to hours to complete and therefore should not be run from the Cacti UI.  You can simply run \'php -q cli/rebuild_poller_cache.php --help\' at the command line for more information.')
+			'description' => __('The Poller Cache will be re-generated if you select this option. Use this option only in the event of a database crash if you are experiencing issues after the crash and have already run the database repair tools.  Alternatively, if you are having problems with a specific Device, simply re-save that Device to rebuild its Poller Cache.  There is also a command line interface equivalent to this command that is recommended for large systems.'),
+			'note'        => array (
+				'message' => __('NOTE: On large systems, this command may take several minutes to hours to complete and therefore should not be run from the Cacti UI.  You can simply run \'php -q cli/rebuild_poller_cache.php --help\' at the command line for more information.'),
+				'class'   => 'textWarning'
+			)
 		),
 		__('Rebuild Resource Cache') => array(
 			'link'  => 'utilities.php?action=rebuild_resource_cache',
@@ -2367,6 +2371,11 @@ function utilities() {
 				print '</td>';
 				print '<td>';
 				print html_escape($details['description']);
+
+				if(isset($details['note'])) {
+					print '<br/><i class="' . $details['note']['class'] . '">' . html_escape($details['note']['message']) . '</i>';
+				}
+
 				print '</td>';
 				form_end_row();
 
