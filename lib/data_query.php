3801e53b49cbd62be45f60a59ef2886980fd324a
TheWitness <thewitness@cacti.net>
2023-09-22 11:32:28 -0400

    QA: Another attempt to fix #5483
    This should get closer to a solution.

--- lib/data_query.php
+++ lib/data_query.php
@@ -1375,7 +1375,11 @@ function query_snmp_host($host_id, $snmp_query_id) {
 						// Check for an IPv6 Address or Hex String
 						$parts = explode('.', $parse_value);
 						foreach($parts as $idx => $part) {
-							$parts[$idx] = substr(strtoupper('00' . dechex($part)), -2);
+							if (is_numeric($part)) {
+								$parts[$idx] = substr(strtoupper('00' . dechex($part)), -2);
+							} else {
+								$parts[$idx] = substr(strtoupper('00' . $part), -2);
+							}
 
 							if ($idx % 2 == 0 && $idx > 0) {
 								$ip_value .= ':' . $parts[$idx];
