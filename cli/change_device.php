4ebaed2f4f5671fd4aedacbe1a852f9b3a4fefba
BerndP <32120318+bernisys@users.noreply.github.com>
2023-11-09 11:43:53 +0100

    Fixed parameter parsing in change_device.php
    explode parses each parameter into more than just 2 parts, if the string contains more than one "=" character.
    
    Can be fixed by limiting explode to output just two elements, then it automatically stops at the first "=" sign and keeps the rest of the string behind it untouched.

--- cli/change_device.php
+++ cli/change_device.php
@@ -54,7 +54,7 @@ $quietMode            = false;
 $overrides = array();
 foreach($parms as $parameter) {
 	if (strpos($parameter, '=')) {
-		list($arg, $value) = explode('=', $parameter);
+		list($arg, $value) = explode('=', $parameter, 2);
 	} else {
 		$arg = $parameter;
 		$value = '';
