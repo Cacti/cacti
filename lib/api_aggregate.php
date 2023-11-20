bb46052eb100c79873b8039d3e8fbbae88162523
TheWitness <thewitness@cacti.net>
2023-11-20 17:07:45 -0500

    QA: Missed change for #5555

--- lib/api_aggregate.php
+++ lib/api_aggregate.php
@@ -1398,11 +1398,11 @@ function aggregate_handle_ptile_type($member_graphs, $skipped_items, $local_grap
 									if ($_total_type == AGGREGATE_TOTAL_TYPE_ALL) {
 										// All so use sum functions
 										$pparts[3] = str_replace('current', 'aggregate_sum', $pparts[3]);
-										$pparts[3] = str_replace('max',     'aggregate_sum', $pparts[3]);
+										$pparts[3] = str_replace('max',     'aggregate_sum_peak', $pparts[3]);
 									} else {
 										// Similar to separate
 										$pparts[3] = str_replace('current', 'aggregate_current', $pparts[3]);
-										$pparts[3] = str_replace('max',     'aggregate_peak', $pparts[3]);
+										$pparts[3] = str_replace('max',     'aggregate_current_peak', $pparts[3]);
 									}
 
 									switch($pparts[3]) {
@@ -1429,7 +1429,9 @@ function aggregate_handle_ptile_type($member_graphs, $skipped_items, $local_grap
 
 											break;
 										case 'aggregate_sum':
+										case 'aggregate_sum_peak':
 										case 'aggregate_current':
+										case 'aggregate_current_peak':
 										case 'aggregate':
 										case 'aggregate_peak':
 											$new_ppart = $pparts[3];
