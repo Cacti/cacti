a3c6d48492fcabde889ef3e1462268a0aeaa391f
TheWitness <thewitness@cacti.net>
2023-11-20 15:21:19 -0500

    QA: One last fix for #5555 - Aggregate formatting

--- lib/api_aggregate.php
+++ lib/api_aggregate.php
@@ -1385,9 +1385,9 @@ function aggregate_handle_ptile_type($member_graphs, $skipped_items, $local_grap
 		foreach ($comments_hrules as $item) {
 			switch($item['graph_type_id']) {
 				case GRAPH_ITEM_TYPE_COMMENT:
-					if (!isset($special_comments[$item['text_format']])) {
+					if (!isset($special_comments[$item['text_format'] . | . $item['value']])) {
 						if (preg_match('/(:bits:|:bytes:)/', $item['text_format'])) {
-							$special_comments[$item['text_format']] = true;
+							$special_comments[$item['text_format'] . | . $item['value']] = true;
 
 							$parts = explode('|', $item['text_format']);
 
@@ -1459,9 +1459,9 @@ function aggregate_handle_ptile_type($member_graphs, $skipped_items, $local_grap
 
 					break;
 				case GRAPH_ITEM_TYPE_HRULE:
-					if (!isset($special_hrules[$item['value']])) {
+					if (!isset($special_hrules[$item['text_format'] . '|' . $item['value']])) {
 						if (preg_match('/(:bits:|:bytes:)/', $item['value'])) {
-							$special_hrules[$item['value']] = true;
+							$special_hrules[$item['text_format'] . '|' . $item['value']] = true;
 
 							$parts = explode('|', $item['value']);
 
