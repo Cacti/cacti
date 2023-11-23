4b57ca1f053a0a46f15cdcceb534c6c380a715b3
xmacan <petr.macek@kostax.cz>
2023-11-23 16:10:51 +0100

    add template for Aruba OSCX 6x00 switch

--- install/functions.php
+++ install/functions.php
@@ -593,6 +593,7 @@ function install_setup_get_templates() {
 		'Apache_Webserver.xml.gz',
 		'ArubaOS_switch.xml.gz',
 		'Aruba_Instant_AP_Cluster.xml.gz',
+		'Aruba_OSCX_switch_6x00.xml.gz',
 		'Aruba_Wireless_Controller.xml.gz',
 		'BayTech_PDU.xml.gz',
 		'Cacti_Stats.xml.gz',
