60043cb46f051713d6a669421d428d474b122c44
TheWitness <thewitness@cacti.net>
2024-02-03 14:15:09 -0500

    Fixing #5661: Ping6 Not found
    On Linux and *NIX variants Cacti is unable to find ping6 as it's not in the path

--- lib/ping.php
+++ lib/ping.php
@@ -193,7 +193,7 @@ class Net_Ping {
 					$result = shell_exec('ping -t ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 				} elseif (substr_count(strtolower(PHP_OS), 'freebsd')) {
 					if (strpos($this->host['hostname'], ':') !== false) {
-						$result = shell_exec('ping6 -X ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
+						$result = shell_exec('/usr/sbin/ping6 -X ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 					} else {
 						$result = shell_exec('ping -t ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 					}
@@ -211,12 +211,12 @@ class Net_Ping {
 					 * as it now tries to open an ICMP socket and fails
 					 * $result will be empty, then. */
 					if (strpos($host_ip, ':') !== false) {
-						$result = shell_exec('ping6 -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
+						$result = shell_exec('/usr/sbin/ping6 -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
 					} else {
 						$result = shell_exec('ping -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname'] . ' 2>&1');
 
-						if ((strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) && file_exists('/bin/ping6')) {
-							$result = shell_exec('ping6 -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
+						if ((strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) && file_exists('/usr/sbin/ping6')) {
+							$result = shell_exec('/usr/sbin/ping6 -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
 						}
 					}
 				}
