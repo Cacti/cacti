ce2bb87a31dd9fef47558c5c8790cd0873b5e463
Bongjae Chang <bongjae.chang@gmail.com>
2024-02-06 15:27:48 +0900

    Fixed "installer - Undefined array key "name" warning in php8" - https://github.com/Cacti/cacti/issues/5662

--- install/functions.php
+++ install/functions.php
@@ -651,8 +651,8 @@ function install_setup_get_templates() {
 			// Loading Template Information from package
 			$myinfo             = @json_decode(shell_exec(cacti_escapeshellcmd(read_config_option('path_php_binary')) . ' -q ' . cacti_escapeshellarg(CACTI_PATH_CLI . '/import_package.php') . ' --filename=' . cacti_escapeshellarg("/$path/$xmlfile") . ' --info-only'), true);
 			$myinfo['filename'] = $xmlfile;
+            $myinfo['name']     = $xmlfile;
 			$info[]             = $myinfo;
-			$info[]             = array('filename' => $xmlfile, 'name' => $xmlfile);
 		}
 	}
 
