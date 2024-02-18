357ff7ae13fa6a36ae329dea70081bcf0460fb97
TheWitness <thewitness@cacti.net>
2024-02-18 12:03:26 -0500

    Update ping.php
    Minor updates for Windows

--- lib/ping.php
+++ lib/ping.php
@@ -222,7 +222,7 @@ class Net_Ping {
 				}
 			}
 
-			if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+			if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN' || ($fping != '' && file_exists($fping))) {
 				$position = strpos($result, 'min/avg/max');
 
 				if ($position > 0) {
