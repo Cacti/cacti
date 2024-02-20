dea987427ba7f09baaef065dc1871bf70fbe9e86
xmacan <petr.macek@kostax.cz>
2024-02-20 22:21:00 +0100

    fix filter - double get request due to redundant action variable

--- user_log.php
+++ user_log.php
@@ -106,7 +106,7 @@ function view_user_log() {
 	?>
 	<script type='text/javascript'>
 	function clearFilter() {
-		strURL = urlPath+'user_log.php?action=view&clear=1';
+		strURL = urlPath+'user_log.php?clear=1';
 		loadUrl({url:strURL})
 	}
 
@@ -139,7 +139,6 @@ function view_user_log() {
 		strURL += '&result=' + $('#result').val();
 		strURL += '&rows=' + $('#rows').val();
 		strURL += '&filter=' + $('#filter').val();
-		strURL += '&action=view';
 		loadUrl({url:strURL})
 	}
 	</script>
@@ -274,7 +273,7 @@ function view_user_log() {
 
 	$user_log = db_fetch_assoc($user_log_sql);
 
-	$nav = html_nav_bar('user_log.php?action=view&username=' . get_request_var('username') . '&filter=' . get_request_var('filter'), MAX_DISPLAY_PAGES, get_request_var('page'), $rows, $total_rows, 6, __('User Logins'), 'page', 'main');
+	$nav = html_nav_bar('user_log.php?username=' . get_request_var('username') . '&filter=' . get_request_var('filter'), MAX_DISPLAY_PAGES, get_request_var('page'), $rows, $total_rows, 6, __('User Logins'), 'page', 'main');
 
 	print $nav;
 
@@ -289,7 +288,7 @@ function view_user_log() {
 		'ip'        => array(__('IP Address'), 'DESC')
 	);
 
-	html_header_sort($display_text, get_request_var('sort_column'), get_request_var('sort_direction'), 1, 'user_log.php?action=view');
+	html_header_sort($display_text, get_request_var('sort_column'), get_request_var('sort_direction'), 1, 'user_log.php');
 
 	$i = 0;
 
