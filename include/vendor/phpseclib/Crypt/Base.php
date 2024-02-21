02eee38d75766a692fcb3e054e4125e766d6a407
Jing Chen <jingchen328@gmail.com>
2024-02-22 01:37:40 +0800

    Upgrade phpseclib to 2.0.46 (#5675)

--- include/vendor/phpseclib/Crypt/Base.php
+++ include/vendor/phpseclib/Crypt/Base.php
@@ -514,6 +514,8 @@ abstract class Base
             switch (true) {
                 // PHP_OS & "\xDF\xDF\xDF" == strtoupper(substr(PHP_OS, 0, 3)), but a lot faster
                 case (PHP_OS & "\xDF\xDF\xDF") === 'WIN':
+                case !function_exists('php_uname'):
+                case !is_string(php_uname('m')):
                 case (php_uname('m') & "\xDF\xDF\xDF") != 'ARM':
                 case PHP_INT_SIZE == 8:
                     define('CRYPT_BASE_USE_REG_INTVAL', true);
@@ -2892,5 +2894,16 @@ abstract class Base
      */
     function do_nothing()
     {
+    }
+
+    /**
+     * Is the continuous buffer enabled?
+     *
+     * @access public
+     * @return boolean
+     */
+    function continuousBufferEnabled()
+    {
+        return $this->continuousBuffer;
     }
 }
