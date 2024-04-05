3bd0651f1dfe913895179a6fc02e90a4c9a7fe61
TheWitness <thewitness@cacti.net>
2024-04-05 17:12:20 -0400

    QA: Add Missing hook for Thold

--- host.php
+++ host.php
@@ -53,6 +53,8 @@ $device_actions = api_plugin_hook_function('device_action_array', $device_action
 /* set default action */
 set_default_action();
 
+api_plugin_hook('device_top');
+
 switch (get_request_var('action')) {
 	case 'export':
 		host_export();
