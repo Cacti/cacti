e5f84017d3bb3ef45c06ae764cdefe2418d57216
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2024-06-12 23:52:28 -0400

    French support for ping.pl (#5770)
    * Update ping.pl
    
    * Numbers only

--- scripts/ping.pl
+++ scripts/ping.pl
@@ -32,21 +32,21 @@ if ($osname =~ 'solaris') {
 
 # old linux version use 'icmp_seq'
 # newer use 'icmp_req' instead
-open(PROCESS, "$pingcmd 2>&1 | grep -E '(icmp_[s|r]eq.*time|unknown host|Unknown host|not supported|Name or service not known|No address associated with name)' 2>/dev/null |");
+open(PROCESS, "$pingcmd 2>&1 | grep -E '(icmp_[s|r]eq.*t(ime|emps)|unknown host|Unknown host|not supported|Name or service not known|No address associated with name)' 2>/dev/null |");
 $ping = <PROCESS>;
 close(PROCESS);
 chomp($ping);
 
 if (($ping =~ 'unknown host') || ($ping =~ 'Unknown host') || ($ping =~ 'not supported') || ($ping =~ 'Name or service not known') || ($ping =~ 'No address associated with name')) {
 	if ((-f '/bin/ping6') || (-f '/sbin/ping6')) {
-		open(PROCESS, "ping6 -c 1 $host 2>&1 | grep 'icmp_[s|r]eq.*time' 2>/dev/null |");
+		open(PROCESS, "ping6 -c 1 $host 2>&1 | grep 'icmp_[s|r]eq.*t(ime|emps)' 2>/dev/null |");
 		$ping = <PROCESS>;
 		close(PROCESS);
 		chomp($ping);
 	}
 }
 
-$ping =~ m/(.*time=)(.*) (ms|usec)/;
+$ping =~ m/(.*t(?:ime|emps)=)([\w\.]*).*(ms|usec)/;
 
 if ($2 == '') {
 	print 'U'; 		# avoid cacti errors, but do not fake rrdtool stats
