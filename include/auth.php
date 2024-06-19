448b6778f26e40f005f167b5822e443cd5ef5e5b
Petr Macek <petr.macek@kostax.cz>
2024-06-20 01:57:21 +0200

    login log messages unification - add ip address (#5772)
    * log message unification - add ip address
    
    * update

--- include/auth.php
+++ include/auth.php
@@ -110,7 +110,9 @@ if ($auth_method != AUTH_METHOD_NONE) {
 				$_SESSION[SESS_USER_AGENT]  = $_SERVER['HTTP_USER_AGENT'];
 				$_SESSION[SESS_CLIENT_ADDR] = get_client_addr();
 
-				cacti_log("LOGIN: User '" . $current_user['username'] . "' authenticated via Basic Authentication.", false, 'AUTH');
+				$client_addr = get_client_addr();
+
+				cacti_log("LOGIN: User '" . $current_user['username'] . "' authenticated via Basic Authentication from IP Address '" . $client_addr . "'", false, 'AUTH');
 
 				db_execute_prepared('INSERT IGNORE INTO user_log
 					(username, user_id, result, ip, time)
