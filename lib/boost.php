de736ee99c1aad32f9dd8213d226e55859d16b67
TheWitness <thewitness@cacti.net>
2024-09-18 20:40:53 -0400

    QA: Adding more entropy to #5829

--- lib/boost.php
+++ lib/boost.php
@@ -740,7 +740,7 @@ function boost_process_poller_output($local_data_id, $rrdtool_pipe = null) {
 	$temp_table          = false;
 
 	if (cacti_count($archive_tables)) {
-		$temp_table = 'poller_output_boost_temp_' . time();
+		$temp_table = "poller_output_boost_temp_$local_data_id_" . microtime(true);
 
 		db_execute("CREATE TEMPORARY TABLE $temp_table LIKE poller_output_boost");
 
