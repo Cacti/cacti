6972688a92ce3c75bd06b44f53240e12d25eba9e
TheWitness <thewitness@cacti.net>
2024-10-03 11:39:30 -0400

    QA: Fixing #5841 - Remove unused variable

--- lib/boost.php
+++ lib/boost.php
@@ -715,7 +715,6 @@ function boost_process_poller_output($local_data_id, $rrdtool_pipe = '') {
 	}
 
 	$query_string        = '';
-	$query_string_suffix = 'ORDER BY local_data_id ASC, timestamp ASC, rrd_name ASC';
 	$sql_params          = array();
 	$locks               = false;
 	$temp_table          = false;
