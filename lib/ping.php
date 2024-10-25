05e5d1849c5efde6a6c874d75a493da2896b6a7e
xmacan <petr.macek@kostax.cz>
2024-10-25 17:03:35 +0200

    add path check

--- lib/ping.php
+++ lib/ping.php
@@ -192,8 +192,15 @@ class Net_Ping
 					$result = shell_exec('ping -6 -W ' . ceil($this->timeout/1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
 				} else {
 					$result = shell_exec('ping -W ' . ceil($this->timeout/1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname'] . ' 2>&1');
-					if ((strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) && file_exists('/usr/bin/ping6')) {
-						$result = shell_exec('/usr/bin/ping6 -W ' . ceil($this->timeout/1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
+					if ((strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) {
+						if (file_exists('/usr/bin/ping6')) {
+							$ping_path = '/usr/bin/ping6';
+						} elseif (file_exists('/usr/sbin/ping6')) {
+							$ping_path = '/usr/sbin/ping6';
+						} else {
+							$ping_path = '/bin/ping6';
+						}
+						$result = shell_exec($ping_path . ' -W ' . ceil($this->timeout/1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
 					}
 				}
 			}
