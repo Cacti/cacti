69d879e55382dc4a0355dcbdf0805bd41a158fce
Jimmy Conner <jimmy@sqmail.org>
2025-02-21 22:34:10 -0600

    Add Rector

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
+
+return RectorConfig::configure()
+    ->withRootFiles()
+    ->withPaths([
+        __DIR__ . '/include',
+        __DIR__ . '/lib',
+        __DIR__ . '/scripts',
+    ])
+    ->withSkip([
+        __DIR__ . '/include/vendor',
+        __DIR__ . '/myadmin*',
+        LongArrayToShortArrayRector::class
+    ])
+    ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
