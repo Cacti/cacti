217fe3541942ddfe7916522f201d5d41b0ce19c2
Jimmy Conner <jimmy@sqmail.org>
2025-02-24 13:13:37 -0600

    Remove Rector / PHPStan for now (#6107)
    I staged my own vendor directory elsewhere for them

--- include/vendor/composer/autoload_static.php
+++ include/vendor/composer/autoload_static.php
@@ -10,8 +10,6 @@ class ComposerStaticInit4457df976b6743d4cf060b8bfc645118
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        '9b38cf48e83f5d8f60375221cd213eee' => __DIR__ . '/..' . '/phpstan/phpstan/bootstrap.php',
-        '38143a9afc50997d55e4815db8489d1c' => __DIR__ . '/..' . '/rector/rector/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
