e27b40e1b997a25ff42031c881a05d5452f8b616
Linegod <pasnak@gmail.com>
2025-03-07 15:04:10 -0600

    change Graph to Device on Device Policy

--- user_group_admin.php
+++ user_group_admin.php
@@ -937,7 +937,7 @@ function user_group_graph_perms_edit($tab, $header_label) {
 			?>
 			<tr class='even'>
 				<td><table><tr>
-				<td class='nowrap'><?php print __('Default Graph Policy for this User Group');?></td>
+				<td class='nowrap'><?php print __('Default Device Policy for this User Group');?></td>
 				<td>
 					<?php form_dropdown('policy_hosts',$policy_array,'','',$policy['policy_hosts'],'',''); ?>
 				</td>
