be662c8124b579cc92901fbbac2ca8927dc8ae5e
TheWitness <thewitness@cacti.net>
2025-07-28 15:29:19 -0400

    Fixing: #6292 - Fatal Validation Errors due to Plugin Defects
    Fatal validation errors occur if a request variable default value is null

--- lib/html_utility.php
+++ lib/html_utility.php
@@ -871,6 +871,10 @@ function get_filter_request_var(string $name, int $filter = FILTER_VALIDATE_INT,
 			}
 		}
 
+		if ($value === null && isset($options['default']) && $options['default'] === null) {
+			$value = '';
+		}
+
 		if ($value === false) {
 			if ($filter == FILTER_VALIDATE_IS_REGEX) {
 				raise_message('custom', __('The regular expression "%s" is not valid. Error is %s', html_escape(get_nfilter_request_var($name)), html_escape($custom_error)), MESSAGE_LEVEL_ERROR);
