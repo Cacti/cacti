d4f08efb685f41464ce3a46c7ddf002bab4e5b3c
TheWitness <thewitness@cacti.net>
2025-08-11 10:33:49 -0400

    :Fix #6296 Fix the SIGTERM slightly differently

--- lib/functions.php
+++ lib/functions.php
@@ -6378,23 +6378,23 @@ function repair_system_data_input_methods($step = 'import') {
 }
 
 if (isset($config['cacti_server_os']) && $config['cacti_server_os'] == 'win32' && !function_exists('posix_kill')) {
-	function posix_kill($pid, $signal = 15) {
-		if (!defined('SIGTERM')) {
-			define('SIGTERM', 15);
-		}
+	if (!defined('SIGTERM')) {
+		define('SIGTERM', 15);
+	}
 
-		if (!defined('SIGKILL')) {
-			define('SIGKILL', 9);
-		}
+	if (!defined('SIGKILL')) {
+		define('SIGKILL', 9);
+	}
 
-		if (!defined('SIGHUP')) {
-			define('SIGHUP', 1);
-		}
+	if (!defined('SIGHUP')) {
+		define('SIGHUP', 1);
+	}
 
-		if (!defined('SIGINT')) {
-			define('SIGINT', 2);
-		}
+	if (!defined('SIGINT')) {
+		define('SIGINT', 2);
+	}
 
+	function posix_kill($pid, $signal = SIGTERM) {
 		$wmi   = new COM("winmgmts:{impersonationLevel=impersonate}!\\\\.\\root\\cimv2");
 		$procs = $wmi->ExecQuery("SELECT ProcessId FROM Win32_Process WHERE ProcessId='" . $pid . "'");
 
