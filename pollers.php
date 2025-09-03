79743676a6b31d219639624e8b4c6e50d2990da4
TheWitness <thewitness@cacti.net>
2025-09-03 08:45:45 -0400

    QA: Restore 'Delete' of a Remote Data Collector

--- pollers.php
+++ pollers.php
@@ -30,6 +30,7 @@ ini_set('memory_limit', '-1');
 ini_set('max_execution_time', '900');
 
 $actions = [
+	POLLER_DELETE      => __('Delete'),
 	POLLER_DISABLE     => __('Disable'),
 	POLLER_ENABLE      => __('Enable'),
 	POLLER_CLEAR_STATS => __('Clear Statistics'),
