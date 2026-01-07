5a1c43672efff4df5b8ada35e3acf1601a5857ed
TheWitness <thewitness@cacti.net>
2026-01-07 14:58:16 -0500

    QA: Use 0 to retrieve the current umask

--- include/vendor/csrf/csrf-magic.php
+++ include/vendor/csrf/csrf-magic.php
@@ -364,7 +364,7 @@ function csrf_get_secret() {
 		$new_secret = csrf_generate_secret();
 		foreach ($files as $file) {
 			if (csrf_writable($file)) {
-				$old_umask = umask(0027);
+				$old_umask = umask(0);
 				$fh = fopen($file, 'w');
 				fwrite($fh, $new_secret);
 				fclose($fh);
