89d5b1c97dd232e206a582f463a1a9196d5c3ea8
TheWitness <thewitness@cacti.net>
2026-01-08 16:58:02 -0500

    Fix#6519: Realtime generates errors in the Cacti Log after a Graph has been removed

--- lib/html.php
+++ lib/html.php
@@ -402,12 +402,19 @@ function html_graph_thumbnail_area(&$graph_array, $no_graphs_message = '', $extr
 		$start = true;
 		foreach ($graph_array as $graph) {
 			if (!isset($graph['host_id'])) {
-				list($graph['host_id'], $graph['disabled']) = db_fetch_row_prepared('SELECT host_id, disabled
-    					FROM graph_local AS gl
-	 				LEFT JOIN host AS h
-      					ON gl.host_id = h.id
-	   				WHERE gl.id = ?',
-					array($graph['local_graph_id']));
+				$gdata = db_fetch_row_prepared('SELECT host_id, disabled
+                    FROM graph_local AS gl
+                    LEFT JOIN host AS h
+                    ON gl.host_id = h.id
+                    WHERE gl.id = ?',
+                    array($graph['local_graph_id']));
+
+				if (cacti_sizeof($gdata)) {
+					$graph['host_id']  = $gdata['host_id'];
+					$graph['disabled'] = $gdata['disabled'];
+				} else {
+					continue;
+				}
 			}
 
 			if (isset($graph['graph_template_name'])) {
