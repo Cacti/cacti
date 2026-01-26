cc3926f8056b6e5f30f62a618602a38d96119f5b
TheWitness <thewitness@cacti.net>
2026-01-26 09:29:19 -0500

    Add whitespace handling for XML parsing

--- lib/xml.php
+++ lib/xml.php
@@ -42,6 +42,7 @@ function xml2array(string $data) : mixed {
 	xml_parser_set_option($p, XML_OPTION_SKIP_WHITE, 1);
 	xml_parser_set_option($p, XML_OPTION_CASE_FOLDING, 0);
 	xml_parse_into_struct($p, $data, $vals, $index);
+
 	if (version_compare(PHP_VERSION, '8.0', '<')) {
 		xml_parser_free($p);
 	}
@@ -136,6 +137,7 @@ function rrdxport2array(string $data) : array {
 	xml_parser_set_option($p, XML_OPTION_CASE_FOLDING, 0);
 	xml_parser_set_option($p, XML_OPTION_TARGET_ENCODING, 'UTF-8');
 	xml_parse_into_struct($p, $data, $vals, $index);
+
 	if (version_compare(PHP_VERSION, '8.0', '<')) {
 		xml_parser_free($p);
 	}
