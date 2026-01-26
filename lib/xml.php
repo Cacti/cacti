b58244e6bbf349a1de0c40569701d6c78402b86b
Athos Ribeiro <athos@ar.dev.br>
2026-01-26 11:17:55 -0300

    Improve PHP 8.5 support
    * The backtick operator is now deprecated in favor of shell_exec.
    * Terminating case statements with a semicolon instead of a colon has
      been deprecated.
    * xml_parser_free was a noop since PHP 8.0 and is now deprecated.

--- lib/xml.php
+++ lib/xml.php
@@ -42,7 +42,9 @@ function xml2array(string $data) : mixed {
 	xml_parser_set_option($p, XML_OPTION_SKIP_WHITE, 1);
 	xml_parser_set_option($p, XML_OPTION_CASE_FOLDING, 0);
 	xml_parse_into_struct($p, $data, $vals, $index);
-	xml_parser_free($p);
+  if (version_compare(PHP_VERSION, '8.0', '<')) {
+    xml_parser_free($p);
+  }
 
 	$tree = [];
 	$i    = 0;
@@ -134,7 +136,9 @@ function rrdxport2array(string $data) : array {
 	xml_parser_set_option($p, XML_OPTION_CASE_FOLDING, 0);
 	xml_parser_set_option($p, XML_OPTION_TARGET_ENCODING, 'UTF-8');
 	xml_parse_into_struct($p, $data, $vals, $index);
-	xml_parser_free($p);
+  if (version_compare(PHP_VERSION, '8.0', '<')) {
+    xml_parser_free($p);
+  }
 
 	$tree   = [];
 	$i      = 0;
