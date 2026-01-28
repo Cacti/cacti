49b2a370a6e33f161ccc01985050699e44a093b4
Petr Macek <petr.macek@kostax.cz>
2026-01-28 17:27:04 +0100

    add hook (#6574)
    * add hook
    
    * update

--- host.php
+++ host.php
@@ -1230,6 +1230,8 @@ function device_change_javascript() : void {
 		});
 	</script>
 	<?php
+
+	api_plugin_hook('device_change_javascript');
 }
 
 function device_javascript(bool $hasHost = true) : void {
