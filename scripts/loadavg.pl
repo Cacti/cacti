be89c36c5255ebe14e76b2df79cef375d90ea6ca
Sean Mancini <sean@seanmancini.com>
2026-01-31 20:24:10 -0500

    Update loadavg.pl

--- scripts/loadavg.pl
+++ scripts/loadavg.pl
@@ -1,27 +1,29 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
+
 
 delete @ENV{qw(PATH)};
 $ENV{PATH} = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin';
 
-$val1 = $ARGV[0];
-($val1) = $val1 =~ /^([\d]+)$/;
+my $val1 = $ARGV[0] // '';
+($val1) = $val1 =~ /^([\d]+)$/ if $val1 ne '';
 
 #get load avg for 5;15;30 min
-open(PROCESS,"uptime |");
-$avg = <PROCESS>;
+open(PROCESS, '-|', 'uptime');
+my $avg = <PROCESS>;
 $avg =~ s/.*:\s*//;
 close(PROCESS);
 
-if ($val1 eq "5") {
-        $avg = `echo "$avg" | awk '\{print \$1 \}'`;
-}
-
-if ($val1 eq "15") {
-        $avg = `echo "$avg" | awk '\{print \$2 \}'`;
-}
-
-if ($val1 eq "30") {
-        $avg = `echo "$avg" | awk '\{print \$3 \}'`;
+if (($val1 eq "5") || ($val1 eq "15") || ($val1 eq "30")) {
+        my @parts = split(/\s*,\s*/, $avg);
+        if ($val1 eq "5") {
+                $avg = $parts[0];
+        } elsif ($val1 eq "15") {
+                $avg = $parts[1];
+        } elsif ($val1 eq "30") {
+                $avg = $parts[2];
+        }
 }
 
 chomp $avg;
