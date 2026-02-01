2c331b0b551a79f4e67af844c31e71cca7a849fe
Sean Mancini <sean@seanmancini.com>
2026-01-31 20:10:58 -0500

    Add support for ss
    Fixed shell injection risk from piped netstat | grep by removing shell pipelines.
    Added use strict; use warnings; and localized variables.
    Added runtime fallback to ss when netstat is unavailable, with a clear failure when neither exists.

--- scripts/unix_tcp_connections.pl
+++ scripts/unix_tcp_connections.pl
@@ -1,19 +1,61 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 delete @ENV{qw(PATH)};
 $ENV{PATH} = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin';
 
-my $grep_string= $ARGV[0];
-($grep_string) = $grep_string =~ /^([\w]+)$/;
+my $grep_string = $ARGV[0] // '';
+($grep_string) = $grep_string =~ /^([\w]+)$/ if $grep_string ne '';
 
 chomp $grep_string;
 
-if ($grep_string eq '') {
-  open(PROCESS, "netstat -n | grep -c tcp | ");
-}else{
-  open(PROCESS, "netstat -n | grep tcp | grep -c $grep_string |");
+sub find_cmd {
+  my @candidates = @_;
+  for my $cmd (@candidates) {
+    return $cmd if -x $cmd;
+  }
+
+  return undef;
 }
-$output = <PROCESS>;
-close(PROCESS);
-chomp($output);
-print $output;
+
+my @cmd;
+my $mode = '';
+my $netstat = find_cmd('/bin/netstat', '/usr/bin/netstat', '/usr/sbin/netstat');
+if (defined $netstat) {
+  @cmd = ($netstat, '-n');
+  $mode = 'netstat';
+} else {
+  my $ss = find_cmd('/bin/ss', '/usr/bin/ss', '/usr/sbin/ss');
+  if (defined $ss) {
+    @cmd = ($ss, '-tan');
+    $mode = 'ss';
+  } else {
+    print 'U';
+    exit 1;
+  }
+}
+
+open(my $process, '-|', @cmd) or exit 1;
+my $count = 0;
+while (my $line = <$process>) {
+  chomp($line);
+
+  if ($mode eq 'netstat') {
+    next unless $line =~ /tcp/;
+    if ($grep_string ne '') {
+      next unless $line =~ /$grep_string/;
+    }
+    $count++;
+  } else {
+    next if $line =~ /^State\s+/;
+    if ($grep_string ne '') {
+      next unless $line =~ /$grep_string/;
+    }
+    $count++;
+  }
+}
+close($process);
+
+print $count;
