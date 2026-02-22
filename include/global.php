6b62c526cdc16ba80bed71394f0d6c7f76427be4
Thomas Vincent <thomasvincent@gmail.com>
2026-02-22 01:36:26 -0800

    fix: sanitize Host header and remove CRLF in HTTPS redirect
    \$_SERVER['HTTP_HOST'] is user-controlled and was passed directly to
    header('Location: ...'). Appended PHP_EOL . PHP_EOL could enable
    response splitting on non-standard SAPIs. Pre-auth reachable.
    
    Strip invalid hostname characters and remove trailing newlines.
    
    Found via RIPS-style static taint analysis on header() sinks.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- include/global.php
+++ include/global.php
@@ -502,7 +502,8 @@ db_cacti_initialized($config['is_web']);
 if ($config['is_web']) {
 	if (read_config_option('force_https') == 'on') {
 		if (!isset($_SERVER['HTTPS']) && isset($_SERVER['HTTP_HOST']) && isset($_SERVER['REQUEST_URI'])) {
-			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . PHP_EOL . PHP_EOL);
+			$host = preg_replace('/[^a-zA-Z0-9.\-:]/', '', $_SERVER['HTTP_HOST']);
+			header('Location: https://' . $host . $_SERVER['REQUEST_URI']);
 
 			exit;
 		}
