381a79cc352875ced68825c8534201c0ce265663
Thomas Vincent <thomasvincent@gmail.com>
2026-02-21 22:57:36 -0800

    fix: remove redundant setAttribute after constructor flag fix
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/database.php
+++ lib/database.php
@@ -125,7 +125,6 @@ function db_connect_real(string $device, string $user, string $pass, string $db_
 			} else {
 				$cnn_id = new PDO("$db_type:host=$device;port=$port;dbname=$db_name;charset=utf8", $user, $pass, $flags);
 			}
-			$cnn_id->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			if (!empty($config['DEBUG_SQL_CONNECT'])) {
 				error_log(sprintf('NOTE: New connection to %s:%s/%s.', $device, $port, $db_name));
