44ff819e62451e3cca8544d89a3c6dec6d670146
Petr Macek <petr.macek@kostax.cz>
2026-03-11 23:27:32 +0100

    fix 6746 - false down state in gui (#6820)

--- lib/api_device.php
+++ lib/api_device.php
@@ -1633,7 +1633,10 @@ function api_device_ping_device(string|null $device_id, bool $from_remote = fals
 						$snmp_system = str_replace(':', ' ', $snmp_system);
 					}
 
-					if ($snmp_system == '') {
+					// Some devices (Dell iDRAC, Fortigate, etc.) may have an empty system value. This causes a false down status
+					$snmp_uptime = cacti_snmp_session_get($session, '.1.3.6.1.6.3.10.2.1.3.0');
+
+					if ($snmp_system == '' && empty($snmp_uptime)) {
 						print "<span class='hostDown'>" . __('Host') . ' ' . __('SNMP error');
 
 						if ($snmp_error != '') {
