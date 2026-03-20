af44e606befe6f540b5b42dc0213f67a73f65ed3
Thomas Vincent <thomasvincent@gmail.com>
2026-03-19 22:58:03 -0700

    hardening: standardize input handling with request variable wrappers (#6865)

--- include/global_languages.php
+++ include/global_languages.php
@@ -44,19 +44,19 @@ if (!read_config_option('i18n_language_support') && read_config_option('i18n_lan
 
 // Repair legacy language support
 if (!empty($config['i18n_force_language'])) {
-	$_REQUEST['language'] = $config['i18n_force_language'];
+	set_request_var('language', $config['i18n_force_language']);
 }
 
-if (!empty($_REQUEST['language'])) {
-	$_REQUEST['language'] = repair_locale($_REQUEST['language']);
+if (isrv('language')) {
+	set_request_var('language', repair_locale(grv('language')));
 }
 
 // determine whether or not we can support the language
 $user_locale = '';
 
-if (!empty($_REQUEST['language']) && !empty($lang2locale[$_REQUEST['language']])) {
+if (isrv('language') && !empty($lang2locale[grv('language')])) {
 	// user requests another language
-	$user_locale = apply_locale($_REQUEST['language']);
+	$user_locale = apply_locale(grv('language'));
 	unset($_SESSION['sess_current_date1']);
 	unset($_SESSION['sess_current_date2']);
 
