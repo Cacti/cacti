f19fc8acafd9e79996425b03aa6a5029971b5f97
Thomas Vincent <thomasvincent@gmail.com>
2026-03-24 00:34:15 -0700

    revert: remove oauth2.php fix (moved to standalone PR #6886)
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- oauth2.php
+++ oauth2.php
@@ -92,9 +92,7 @@ switch ($providerName) {
 if (!isrv('code')) { // If we don't have an authorization code then get one
 	$authUrl                 = $provider->getAuthorizationUrl($options);
 	$_SESSION['oauth2state'] = $provider->getState();
-	header('Location: ' . $authUrl);
-
-	exit;
+	cacti_redirect($authUrl, false);
 
 	// Check given state against previously stored one to mitigate CSRF attack
 }
