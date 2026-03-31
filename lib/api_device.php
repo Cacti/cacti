b1f649bf7752043eeddd5097d8189183d9020c83
TheWitness <thewitness@cacti.net>
2026-03-31 13:29:35 -0400

    Fix: Remove opcache from nightly heavy as well as rector (#6923)
    * Fix: Remove opcache from nightly heavy as well as rector
    
    * Fix: Adding more php-stan checks
    
    * Fix: Proper syntax for array

--- lib/api_device.php
+++ lib/api_device.php
@@ -892,7 +892,7 @@ function api_device_replicate_out(int $device_id, int $poller_id = 1) : bool {
 		replicate_table_to_poller($rcnn_id, $data, 'data_input_data', $poller_id);
 	}
 
-	api_plugin_hook_function('replicate_out', array('remote_poller_id' => $poller_id, 'rcnn_id' => $rcnn_id, 'class' => 'all'));
+	api_plugin_hook_function('replicate_out', ['remote_poller_id' => $poller_id, 'rcnn_id' => $rcnn_id, 'class' => 'all']);
 
 	$stats = db_fetch_row_prepared('SELECT
 		SUM(CASE WHEN action=0 THEN 1 ELSE 0 END) AS snmp,
