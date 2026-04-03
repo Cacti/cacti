4089a8863d7ad23790f6aca916d046dc907d72e7
Thomas Vincent <thomasvincent@gmail.com>
2026-04-03 14:42:29 -0700

    test(scripts): add GPL header to DatasourceScriptErrorReturnTest
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Unit/DatasourceScriptErrorReturnTest.php
+++ tests/Unit/DatasourceScriptErrorReturnTest.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDtool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+*/
 
 /**
  * Source-scan tests verifying that data source scripts return 'U' on error.
