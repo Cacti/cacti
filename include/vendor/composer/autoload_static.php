191d8a1811fe4eaa0d10afc3ea32ab9b468fe80c
TheWitness <thewitness@cacti.net>
2026-04-25 16:58:56 -0400

    bug: Add HTMLPurify and upgrade phpseclib for CVE remediation

--- include/vendor/composer/autoload_static.php
+++ include/vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519
 {
     public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     );
 
@@ -32,6 +33,16 @@ class ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -41,6 +52,7 @@ class ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitdc6b32dad13fa4041cc9058b131d1519::$classMap;
 
         }, null, ClassLoader::class);
