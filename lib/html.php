7c27b83da02e36e35e7f800bde6eddfa05eeca2e
TheWitness <thewitness@cacti.net>
2026-04-30 14:18:46 -0400

    fix: PHPStan issue

--- lib/html.php
+++ lib/html.php
@@ -2318,7 +2318,7 @@ function html_show_tabs_left() : void {
 			$i++;
 		}
 
-		print "<li><a id='$id' role='tab' class='lefttab" . ($tab['selected'] ? " selected' aria-selected='true'" : "' aria-selected='false'") . " href='" . htmle($tab['url']) . "'><span class='fa glyph_$id'></span><span class='text_$id'>" . htmle($tab['title']) . "</span></a><a id='menu-$id' class='maintabs-submenu' href='#'><i class='ti ti-chevron-down'></i></a></li>";
+		print "<li><a id='$id' role='tab' class='lefttab" . ($tab['selected'] === true ? " selected' aria-selected='true'" : "' aria-selected='false'") . " href='" . htmle($tab['url']) . "'><span class='fa glyph_$id'></span><span class='text_$id'>" . htmle($tab['title']) . "</span></a><a id='menu-$id' class='maintabs-submenu' href='#'><i class='ti ti-chevron-down'></i></a></li>";
 	}
 
 	print "<li class='ellipsis maintabs-submenu-ellipsis'><a id='menu-ellipsis' role='tab' aria-selected='false' class='submenu-ellipsis' href='#'><i class='ti ti-chevron-down'></i></a></li>";
