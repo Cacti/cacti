5931b69aff19eea51ab8e17ce46e1c74c157646c
Thomas Vincent <thomasvincent@gmail.com>
2026-06-23 11:13:52 -0700

    fix: keep realtime graphing alive when a poll fails or returns non-JSON (#7212) (#7215)
    * fix: keep realtime graphing alive when a poll fails or returns non-JSON (#7212)
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>
    
    * fix: make destroy() null-safe
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>
    
    ---------
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- include/realtime.js
+++ include/realtime.js
@@ -272,9 +272,15 @@ function realtimeGrapher() {
 
 					$.get(urlPath+'graph_realtime.php?action=countdown&top='+parseInt(position.top)+'&left='+parseInt(position.left)+(isThumb ? '&graph_nolegend=true':'&graph_nolegend=false')+'&graph_end=0&graph_start=-'+(parseInt(graph_start) > 0 ? graph_start:'60')+'&local_graph_id='+local_graph_id+'&ds_step='+ds_step+'&count='+count+'&size='+size)
 						.done(function(data) {
-							var results = $.parseJSON(data);
+							var results;
 
-							if (realtimeArray[results.local_graph_id] == true) {
+							try {
+								results = $.parseJSON(data);
+							} catch (e) {
+								results = null;
+							}
+
+							if (results != null && realtimeArray[results.local_graph_id] == true) {
 								var image_format = (results.image_format == 'svg+xml') ? 'svg+xml' : 'png';
 								$('#graph_'+results.local_graph_id).attr('src', 'data:image/'+image_format+';base64,'+results.data).change();
 
@@ -288,11 +294,12 @@ function realtimeGrapher() {
 							destroy(data);
 							destroy(results);
 							destroy(position);
-
-							graphsRendered++;
 						})
 						.fail(function(data) {
 							getPresentHTTPError(data);
+						})
+						.always(function() {
+							graphsRendered++;
 						});
 				});
 			}
@@ -318,6 +325,10 @@ function realtimeGrapher() {
 }
 
 function destroy(obj) {
+	if (obj == null) {
+		return;
+	}
+
 	for (var prop in obj){
 		var property = obj[prop];
 
