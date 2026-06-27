32334cc40007a8ef6c6ccdd0085285f5e2c98501
Thomas Vincent <thomasvincent@gmail.com>
2026-06-27 15:29:13 -0700

    fix(deps): include validator runtime packages

---
+++ include/vendor/symfony/validator/Constraints/Cascade.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
+
+/**
+ * @Annotation
+ * @Target({"CLASS"})
+ *
+ * @author Jules Pietri <jules@heahprod.com>
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class Cascade extends Constraint
+{
+    public array $exclude = [];
+
+    public function __construct(array|string|null $exclude = null, ?array $options = null)
+    {
+        if (\is_array($exclude) && !array_is_list($exclude)) {
+            $options = array_merge($exclude, $options ?? []);
+            $options['exclude'] = array_flip((array) ($options['exclude'] ?? []));
+        } else {
+            $this->exclude = array_flip((array) $exclude);
+        }
+
+        if (\is_array($options) && \array_key_exists('groups', $options)) {
+            throw new ConstraintDefinitionException(\sprintf('The option "groups" is not supported by the constraint "%s".', __CLASS__));
+        }
+
+        parent::__construct($options);
+    }
+
+    public function getTargets(): string|array
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
