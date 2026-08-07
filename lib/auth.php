d73fb57b29a94ecb1fdd7aadeb89d639d60dd783
Thomas Vincent <thomasvincent@gmail.com>
2026-08-06 18:05:07 -0700

    Document group token invalidation

--- lib/auth.php
+++ lib/auth.php
@@ -4393,8 +4393,8 @@ function rsa_check_keypair() : void {
 }
 
 /**
- * Sets a flag for all users of a group logged in that their perms
- * need to be reloaded from the database
+ * Expires persistent authentication tokens and sets a flag for all users of a
+ * group logged in that their permissions need to be reloaded from the database.
  *
  * @param int $group_id The ID of the group whose users' permissions need to be reset.
  *
