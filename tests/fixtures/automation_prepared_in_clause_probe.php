35bcb405ea440c8c2b56ca00c3638dec064da7ea
Thomas Vincent <thomasvincent@gmail.com>
2026-08-07 01:46:35 -0700

    docs(tests): add complete license headers

--- tests/fixtures/automation_prepared_in_clause_probe.php
+++ tests/fixtures/automation_prepared_in_clause_probe.php
@@ -2,6 +2,11 @@
 /*
  +-------------------------------------------------------------------------+
  | Copyright (C) 2004-2026 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
  +-------------------------------------------------------------------------+
  | Cacti: The Complete RRDtool-based Graphing Solution                     |
  +-------------------------------------------------------------------------+
