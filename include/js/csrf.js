12811e5d96fc4aed4a98b44e4ab55b22a21f3dba
Thomas Vincent <thomasvincent@gmail.com>
2026-08-15 00:12:27 -0700

    refactor(csrf): move the XHR shim to include/js as Cacti-owned code
    It has been maintained by the Cacti Group since 2020 and is not an upstream package.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- include/js/csrf.js
+++ include/js/csrf.js
@@ -1,3 +1,24 @@
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDtool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | Derived from csrf-magic by Edward Z. Yang, BSD licensed.  Maintained  |
+ | by the Cacti Group since 2020; upstream has been inactive since 2016.   |
+ +-------------------------------------------------------------------------+
+*/
+
 /**
  * @file
  *
