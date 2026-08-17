9e0eacf308940ee601f9ed701d3bc8f45bb97c30
TheWitness <thewitness@cacti.net>
2026-08-17 19:19:09 -0400

    qa: Unwind __() stub for a moment

--- tests/Helpers/UnitStubs.php
+++ tests/Helpers/UnitStubs.php
@@ -28,13 +28,13 @@
 // Only define the translation function stub needed by global.php
 // __() is used but not defined in Cacti core
 // __esc() is defined in include/global_languages.php, so we don't stub it
-if (!function_exists(__NAMESPACE__ . '\\__') && !function_exists('\\__')) {
-	function __($text, ...$args) {
-		// If no args, return the text as-is (no formatting needed)
-		if (empty($args)) {
-			return $text;
-		}
-		// If args provided, use vsprintf for formatting
-		return vsprintf($text, $args);
-	}
-}
+//if (!function_exists(__NAMESPACE__ . '\\__') && !function_exists('\\__')) {
+//	function __($text, ...$args) {
+//		// If no args, return the text as-is (no formatting needed)
+//		if (empty($args)) {
+//			return $text;
+//		}
+//		// If args provided, use vsprintf for formatting
+//		return vsprintf($text, $args);
+//	}
+//}
