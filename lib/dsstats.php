5dafd32b81dcebf60e658d54adacfdee8047159a
Thomas Vincent <thomasvincent@gmail.com>
2026-08-29 21:30:10 -0700

    security: escape path_dsstats_log and the aggregate graphs local_graph_id
    path_dsstats_log was concatenated into the poller_dsstats.php background
    shell redirect without cacti_escapeshellarg, unlike the identical
    path_rrdcheck_log path. Wrap it to match.
    
    aggregate_graphs.php reflected local_graph_id into a hidden input on the
    action-confirmation render, where it is not integer-validated (the validating
    branches exit first), so html_escape it.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- lib/dsstats.php
+++ lib/dsstats.php
@@ -1036,9 +1036,9 @@ function dsstats_poller_bottom () {
 		$command_string = read_config_option('path_php_binary');
 		if (read_config_option('path_dsstats_log') != '') {
 			if ($config['cacti_server_os'] == 'unix') {
-				$extra_args = '-q ' . $config['base_path'] . '/poller_dsstats.php >> ' . read_config_option('path_dsstats_log') . ' 2>&1';
+				$extra_args = '-q ' . $config['base_path'] . '/poller_dsstats.php >> ' . cacti_escapeshellarg(read_config_option('path_dsstats_log')) . ' 2>&1';
 			} else {
-				$extra_args = '-q ' . $config['base_path'] . '/poller_dsstats.php >> ' . read_config_option('path_dsstats_log');
+				$extra_args = '-q ' . $config['base_path'] . '/poller_dsstats.php >> ' . cacti_escapeshellarg(read_config_option('path_dsstats_log'));
 			}
 		} else {
 			$extra_args = '-q ' . $config['base_path'] . '/poller_dsstats.php';
