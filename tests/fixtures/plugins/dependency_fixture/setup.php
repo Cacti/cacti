d8f115cd9ebecaa032766eb40f8bd2c801021301
Thomas Vincent <thomasvincent@gmail.com>
2026-09-04 18:29:43 -0700

    fix: preserve layout after plugin dependency errors
    Redirect failed plugin dependency checks to the full Plugin Management page now that state changes use native POST navigation.
    
    Fixes #7961
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

---
+++ tests/fixtures/plugins/dependency_fixture/setup.php
@@ -0,0 +1,14 @@
+<?php
+
+function plugin_dependency_fixture_install() {
+}
+
+function plugin_dependency_fixture_version() {
+	return array(
+		'name'     => 'dependency_fixture',
+		'version'  => '1.0',
+		'longname' => 'Dependency Redirect Fixture',
+		'author'   => 'The Cacti Group',
+		'homepage' => 'https://www.cacti.net',
+	);
+}
