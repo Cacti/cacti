28ca3a8580dfb5a7f4adad3a6bec62ed5c5251ac
Thomas Vincent <thomasvincent@gmail.com>
2026-09-05 06:29:23 -0700

    test: fix 1.2 CDEF runner isolation

---
+++ tests/Helpers/CdefCoverageBootstrap.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+*/
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$GLOBALS['cdef_test_items']   = [];
+$GLOBALS['cdef_test_lists']   = [];
+$GLOBALS['cdef_test_names']   = [];
+$GLOBALS['cdef_test_queries'] = [];
+$GLOBALS['cdef_functions']    = [];
+$GLOBALS['cdef_operators']    = [];
+
+function db_fetch_row_prepared(string $sql, array $params = []) : array|false {
+	$GLOBALS['cdef_test_queries'][] = [$sql, $params];
+
+	return $GLOBALS['cdef_test_items'][(int) ($params[0] ?? 0)] ?? false;
+}
+
+function db_fetch_assoc_prepared(string $sql, array $params = []) : array|false {
+	$GLOBALS['cdef_test_queries'][] = [$sql, $params];
+
+	return $GLOBALS['cdef_test_lists'][(int) ($params[0] ?? 0)] ?? [];
+}
+
+function db_fetch_cell_prepared(string $sql, array $params = []) : mixed {
+	$GLOBALS['cdef_test_queries'][] = [$sql, $params];
+
+	return $GLOBALS['cdef_test_names'][(int) ($params[0] ?? 0)] ?? false;
+}
+
+function cacti_sizeof(mixed $value) : int {
+	return is_array($value) ? count($value) : 0;
+}
+
+require_once dirname(__DIR__, 2) . '/lib/cdef.php';
