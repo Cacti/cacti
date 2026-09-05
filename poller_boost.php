7932cb0a3c04255c466ccddaab8071ad7e431433
Thomas Vincent <thomasvincent@gmail.com>
2026-09-04 19:45:26 -0700

    fix: recover Boost process columns on installs already stamped 1.2.32
    Git-following 1.2.x installs can have version.cacti = 1.2.32 without
    ever running a 1.2.32 upgrade file. Boost now applies the missing
    run_id/child_id columns at poller start. Graph items without a data
    source now return AVERAGE instead of a previous call's CF.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- poller_boost.php
+++ poller_boost.php
@@ -185,6 +185,8 @@ if ($child == false) {
 		/* Check if processes are running and kill them */
 		boost_kill_running_processes();
 
+		boost_ensure_process_table();
+
 		/* Truncate the rrd_update_counter table */
 		db_execute('TRUNCATE TABLE poller_output_boost_processes');
 
