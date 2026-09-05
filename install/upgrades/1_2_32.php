6e44ac107f627acf3ffd99a6d93d8c49710cced7
Thomas Vincent <thomasvincent@gmail.com>
2026-09-04 19:09:42 -0700

    fix: apply Boost process-table columns on the 1.2.32 upgrade path
    The 1.2.31 upgrade already shipped, so existing 1.2.31 installs never
    received the run_id/child_id columns from #7728.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

---
+++ install/upgrades/1_2_32.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+function upgrade_to_1_2_32() {
+	global $config;
+
+	/* #7728 landed these columns in cacti.sql and in upgrade_to_1_2_31() after
+	 * 1.2.31 had already shipped. Installs sitting on 1.2.31 never re-run that
+	 * file. db_install_add_column() is a no-op when the column exists, so
+	 * 1.2.30 -> 1.2.32 is safe. */
+	db_install_execute('TRUNCATE TABLE poller_output_boost_processes');
+	db_install_add_column('poller_output_boost_processes', array('name' => 'run_id', 'type' => 'char(32)', 'NULL' => false, 'default' => '', 'after' => 'sock_int_value'));
+	db_install_add_column('poller_output_boost_processes', array('name' => 'child_id', 'type' => 'int(10)', 'unsigned' => true, 'NULL' => false, 'default' => '0', 'after' => 'run_id'));
+	db_install_add_key('poller_output_boost_processes', 'UNIQUE', 'run_child', array('run_id', 'child_id'));
+}
