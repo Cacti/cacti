7b632d810b0947b2d5414536968470de2c814a85
Thomas Vincent <thomasvincent@gmail.com>
2026-09-05 17:52:19 -0700

    fix(aggregate): document mutable message checks
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- aggregate_templates.php
+++ aggregate_templates.php
@@ -269,6 +269,8 @@ function aggregate_form_save() : void {
 		}
 	}
 
+	// Save helpers mutate the global message registry; PHPStan cannot observe that side effect.
+	/** @phpstan-ignore booleanNot.alwaysTrue */
 	if (!is_error_message()) {
 		raise_message(1);
 	}
