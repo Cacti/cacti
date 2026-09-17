f804c1617d395ca879dd353ddb2a042d71403b83
TheWitness <thewitness@cacti.net>
2026-09-17 10:19:14 -0700

    fix: Unable to move graph template item, move PHP floot to 8.2 (#8031)
    * fix: Unable to move graph template item
    
    * ci: Remove php 8.1 from testing matrix
    
    * fix: Update baselines for the recent change

--- graph_templates_items.php
+++ graph_templates_items.php
@@ -74,21 +74,21 @@ switch (get_request_var('action')) {
 
 		item_remove();
 
-		header('Location: graph_templates.php?header=false&action=template_edit&id=' . get_request_var('graph_template_id'));
+		header('Location: graph_templates.php?action=template_edit&id=' . get_request_var('graph_template_id'));
 		break;
 	case 'item_movedown':
 		get_filter_request_var('graph_template_id');
 
 		item_movedown();
 
-		header('Location: graph_templates.php?header=false&action=template_edit&id=' . get_request_var('graph_template_id'));
+		header('Location: graph_templates.php?action=template_edit&id=' . get_request_var('graph_template_id'));
 		break;
 	case 'item_moveup':
 		get_filter_request_var('graph_template_id');
 
 		item_moveup();
 
-		header('Location: graph_templates.php?header=false&action=template_edit&id=' . get_request_var('graph_template_id'));
+		header('Location: graph_templates.php?action=template_edit&id=' . get_request_var('graph_template_id'));
 		break;
 	case 'item_edit':
 		top_header();
